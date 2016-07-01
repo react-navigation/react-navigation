5cd2fc3e79893c1b43b76148023cdfa1e53b54f3
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-07-01 16:48:47 +0530

    example: add a listview example

---
+++ packages/react-native-tab-view/example/src/ListViewExample.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { ListView, View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ff4081',
+    padding: 8,
+  },
+  row: {
+    margin: 8,
+    padding: 16,
+    borderRadius: 3,
+    backgroundColor: 'rgba(255, 255, 255, .1)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'rgba(0, 0, 0, .4)',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+});
+
+export default class ListViewExample extends Component {
+  state = {
+    data: [],
+    dataSource: new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    }),
+  };
+
+  componentWillMount() {
+    this._genRows();
+  }
+
+  _genRows = () => {
+    const data = this.state.data.slice(0);
+    const itemsLength = data.length;
+
+    if (itemsLength >= 1000) {
+      return;
+    }
+
+    for (let i = 0; i < 100; i++) {
+      data.push(itemsLength + i);
+    }
+
+    this.setState({
+      data,
+      dataSource: this.state.dataSource.cloneWithRows(data),
+    });
+  };
+
+  _renderRow = (index) => {
+    return (
+      <View style={styles.row}>
+        <Text style={styles.text}>{index}</Text>
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <ListView
+        contentContainerStyle={styles.container}
+        dataSource={this.state.dataSource}
+        renderRow={this._renderRow}
+        onEndReached={this._genRows}
+      />
+    );
+  }
+}
