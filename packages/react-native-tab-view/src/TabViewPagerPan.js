e9cdceb8806f5c3d0ebc31302e982302aeb9584d
Julien Vincent <julienlucvincent@gmail.com>
2016-11-14 21:49:29 +0200

    refactor: inline pan responder functions to TabViewPagerPan (#65)

--- packages/react-native-tab-view/src/TabViewPagerPan.js
+++ packages/react-native-tab-view/src/TabViewPagerPan.js
@@ -4,13 +4,14 @@ import React, { Component, PropTypes } from 'react';
 import {
   Animated,
   PanResponder,
+  Platform,
   StyleSheet,
   View,
 } from 'react-native';
-import TabViewPanResponder from './TabViewPanResponder';
 import TabViewStyleInterpolator from './TabViewStyleInterpolator';
 import { SceneRendererPropType } from './TabViewPropTypes';
 import type { SceneRendererProps } from './TabViewTypeDefinitions';
+import type { GestureEvent, GestureState } from './PanResponderTypes';
 
 const styles = StyleSheet.create({
   container: {
@@ -37,6 +38,8 @@ type Props = SceneRendererProps & {
   children?: any;
 }
 
+const DEAD_ZONE = 12;
+
 export default class TabViewPagerPan extends Component<DefaultProps, Props, void> {
   static propTypes = {
     ...SceneRendererPropType,
@@ -52,77 +55,112 @@ export default class TabViewPagerPan extends Component<DefaultProps, Props, void
   };
 
   componentWillMount() {
-    this._setPanHandlers(this.props);
+    this._normalizeSwipeVelocityThreshold();
 
-    // We need this mess to maintain a single panResponder
-    // We can't update the panResponder mid-gesture
-    // Otherwise it'll never release the InteractionManager handle
-    // Which will cause InteractionManager.runAfterInteractions callbacks to never fire
     this._panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: (...args) => {
-        return this._callResponderMethod('onStartShouldSetPanResponder', false)(...args);
-      },
-      onStartShouldSetPanResponderCapture: (...args) => {
-        return this._callResponderMethod('onStartShouldSetPanResponderCapture', false)(...args);
-      },
-      onMoveShouldSetPanResponder: (...args) => {
-        return this._callResponderMethod('onMoveShouldSetPanResponder', false)(...args);
-      },
-      onMoveShouldSetPanResponderCapture: (...args) => {
-        return this._callResponderMethod('onMoveShouldSetPanResponderCapture', false)(...args);
-      },
-      onPanResponderReject: (...args) => {
-        this._callResponderMethod('onPanResponderReject', null)(...args);
-      },
-      onPanResponderGrant: (...args) => {
-        this._callResponderMethod('onPanResponderGrant', null)(...args);
-      },
-      onPanResponderStart: (...args) => {
-        this._callResponderMethod('onPanResponderStart', null)(...args);
-      },
-      onPanResponderEnd: (...args) => {
-        this._callResponderMethod('onPanResponderEnd', null)(...args);
-      },
-      onPanResponderRelease: (...args) => {
-        this._callResponderMethod('onPanResponderRelease', null)(...args);
-      },
-      onPanResponderMove: (...args) => {
-        this._callResponderMethod('onPanResponderMove', null)(...args);
-      },
-      onPanResponderTerminate: (...args) => {
-        this._callResponderMethod('onPanResponderTerminate', null)(...args);
-      },
-      onPanResponderTerminationRequest: (...args) => {
-        return this._callResponderMethod('onPanResponderTerminationRequest', true)(...args);
-      },
-      onShouldBlockNativeResponder: (...args) => {
-        return this._callResponderMethod('onShouldBlockNativeResponder', false)(...args);
-      },
+      onMoveShouldSetPanResponder: this._canMoveScreen,
+      onMoveShouldSetPanResponderCapture: this._canMoveScreen,
+      onPanResponderGrant: this._startGesture,
+      onPanResponderMove: this._respondToGesture,
+      onPanResponderTerminate: this._finishGesture,
+      onPanResponderRelease: this._finishGesture,
+      onPanResponderTerminationRequest: () => true,
     });
   }
 
-  componentWillReceiveProps(nextProps: Props) {
-    this._setPanHandlers(nextProps);
+  componentDidUpdate() {
+    this._normalizeSwipeVelocityThreshold();
   }
 
-  _setPanHandlers = (props: Props) => {
-    if (this.props.swipeEnabled === false) {
-      this._panHandlers = null;
+  _normalizeSwipeVelocityThreshold = () => {
+    this._swipeVelocityThreshold = this.props.swipeVelocityThreshold;
+    if (Platform.OS === 'android') {
+      // on Android, velocity is way lower due to timestamp being in nanosecond
+      // normalize it to have the same velocity on both iOS and Android
+      this._swipeVelocityThreshold /= 1000000;
+    }
+  };
+
+  _isIndexInRange = (index: number) => {
+    const { routes } = this.props.navigationState;
+    return (index >= 0 && index <= routes.length - 1);
+  };
+
+  _isMovingHorzontally = (evt: GestureEvent, gestureState: GestureState) => {
+    return (
+      (Math.abs(gestureState.dx) > Math.abs(gestureState.dy * 3)) &&
+      (Math.abs(gestureState.vx) > Math.abs(gestureState.vy * 3))
+    );
+  };
+
+  _isReverseDirection = (gestureState: GestureState) => {
+    if (this._startDirection > 0) {
+      return gestureState.vx < 0;
     } else {
-      this._panHandlers = TabViewPanResponder.forHorizontal(props);
+      return gestureState.vx > 0;
     }
   };
 
-  _callResponderMethod = (methodName: string, returnValue: any) => (...args: Array<*>) => {
-    const panHandlers = this._panHandlers;
-    if (panHandlers && panHandlers[methodName]) {
-      return panHandlers[methodName](...args);
+  _getNextIndex = (evt: GestureEvent, gestureState: GestureState) => {
+    const { index } = this.props.navigationState;
+    if (Math.abs(gestureState.dx) > this.props.swipeDistanceThreshold || Math.abs(gestureState.vx) > this._swipeVelocityThreshold) {
+      const nextIndex = index - (gestureState.dx / Math.abs(gestureState.dx));
+      if (this._isIndexInRange(nextIndex)) {
+        return nextIndex;
+      }
     }
-    return returnValue;
+    return index;
   };
 
-  _panHandlers: any;
+  _canMoveScreen = (evt: GestureEvent, gestureState: GestureState) => {
+    const { navigationState: { routes, index } } = this.props;
+    const canMove = this._isMovingHorzontally(evt, gestureState) && (
+        (gestureState.dx >= DEAD_ZONE && index >= 0) ||
+        (gestureState.dx <= -DEAD_ZONE && index <= routes.length - 1)
+      );
+    if (canMove) {
+      this._startDirection = gestureState.dx;
+    }
+    return canMove;
+  };
+
+  _startGesture = () => {
+    this._lastValue = this.props.getLastPosition();
+    this.props.position.stopAnimation();
+  };
+
+  _respondToGesture = (evt: GestureEvent, gestureState: GestureState) => {
+    const { layout: { width } } = this.props;
+    const currentPosition = typeof this._lastValue === 'number' ? this._lastValue : this.props.navigationState.index;
+    const nextPosition = currentPosition - (gestureState.dx / width);
+    if (this._isMoving === null) {
+      this._isMoving = this._isMovingHorzontally(evt, gestureState);
+    }
+    if (this._isMoving && this._isIndexInRange(nextPosition)) {
+      this.props.position.setValue(nextPosition);
+    }
+  };
+
+  _finishGesture = (evt: GestureEvent, gestureState: GestureState) => {
+    const currentIndex = this.props.navigationState.index;
+    const currentValue = this.props.getLastPosition();
+    if (currentValue !== currentIndex) {
+      if (this._isMoving && !this._isReverseDirection(gestureState)) {
+        const nextIndex = this._getNextIndex(evt, gestureState);
+        this.props.jumpToIndex(nextIndex);
+      } else {
+        this.props.jumpToIndex(currentIndex);
+      }
+    }
+    this._lastValue = null;
+    this._isMoving = null;
+  };
+
   _panResponder: any;
+  _lastValue = null;
+  _isMoving = null;
+  _startDirection = 0;
+  _swipeVelocityThreshold = 0.25;
 
   render() {
     const { navigationState, layout } = this.props;
