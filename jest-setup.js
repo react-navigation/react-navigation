ecb504eb81726fc14b6955ebf7b5de43de62c221
ferrannp <fnp.developer@gmail.com>
2017-01-30 19:38:13 +0100

    Mock ScrollView.scrollTo which gives problems when testing components like TabNavigator.

--- jest-setup.js
+++ jest-setup.js
@@ -10,3 +10,12 @@ jest.mock('Linking', () => {
     getInitialURL: jest.fn().mockImplementation((value: string) => Promise.resolve(value)),
   }
 });
+
+// See https://github.com/facebook/react-native/issues/11659
+jest.mock('ScrollView', () => {
+  const RealComponent = require.requireActual('ScrollView');
+  class ScrollView extends RealComponent {
+    scrollTo = () => {}
+  }
+  return ScrollView
+});
