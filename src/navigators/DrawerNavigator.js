e46316248b8561f4d53fb9b5c6bbe6b1ee35221d
Rafael Mathias <rafaelmathias@MacBook-Pro-de-Rafael.local>
2017-02-04 14:55:37 -0200

    override drawer navigator styles

--- src/navigators/DrawerNavigator.js
+++ src/navigators/DrawerNavigator.js
@@ -45,6 +45,7 @@ const DrawerNavigator = (
     contentComponent,
     contentOptions,
     drawerPosition,
+    style,
     ...tabsConfig
   } = mergedConfig;
   const contentRouter = TabRouter(routeConfigs, tabsConfig);
@@ -67,6 +68,7 @@ const DrawerNavigator = (
       contentComponent={contentComponent}
       contentOptions={contentOptions}
       drawerPosition={drawerPosition}
+      style={style}
     />
   ), containerConfig);
 };
