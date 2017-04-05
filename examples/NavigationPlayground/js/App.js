4c761daa257318509934d812bec5416d91ee40b5
Kevin Cooper <cooperka92@gmail.com>
2017-04-05 11:16:22 -0400

    [Playground] Add TabsInDrawer example

--- examples/NavigationPlayground/js/App.js
+++ examples/NavigationPlayground/js/App.js
@@ -15,6 +15,7 @@ import { StackNavigator } from 'react-navigation';
 import Banner from './Banner';
 import CustomTabs from './CustomTabs';
 import Drawer from './Drawer';
+import TabsInDrawer from './TabsInDrawer';
 import ModalStack from './ModalStack';
 import StacksInTabs from './StacksInTabs';
 import SimpleStack from './SimpleStack';
@@ -35,6 +36,11 @@ const ExampleRoutes = {
     name: 'Drawer Example',
     description: 'Android-style drawer navigation',
     screen: Drawer,
+  },
+  TabsInDrawer: {
+    name: 'Drawer + Tabs Example',
+    description: 'A drawer combined with tabs',
+    screen: TabsInDrawer,
   },
   CustomTabs: {
     name: 'Custom Tabs',
---
+++ examples/NavigationPlayground/js/TabsInDrawer.js
@@ -0,0 +1,59 @@
+/**
+ * @flow
+ */
+
+import React from 'react';
+import {
+  Button,
+  Platform,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
+import {
+  TabNavigator,
+  DrawerNavigator,
+} from 'react-navigation';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import SimpleTabs from './SimpleTabs';
+import StacksOverTabs from './StacksOverTabs';
+
+const TabsInDrawer = DrawerNavigator({
+  SimpleTabs: {
+    screen: SimpleTabs,
+    navigationOptions: {
+      drawer: () => ({
+        label: 'Simple Tabs',
+        icon: ({ tintColor }) => (
+          <MaterialIcons
+            name="filter-1"
+            size={24}
+            style={{ color: tintColor }}
+          />
+        ),
+      }),
+    },
+  },
+  StacksOverTabs: {
+    screen: StacksOverTabs,
+    navigationOptions: {
+      drawer: () => ({
+        label: 'Stacks Over Tabs',
+        icon: ({ tintColor }) => (
+          <MaterialIcons
+            name="filter-2"
+            size={24}
+            style={{ color: tintColor }}
+          />
+        ),
+      }),
+    },
+  },
+});
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Platform.OS === 'ios' ? 20 : 0,
+  },
+});
+
+export default TabsInDrawer;
