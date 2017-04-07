6978ceec85889bbe1c27e82df9436a1620a379dc
Kevin Cooper <cooperka92@gmail.com>
2017-04-07 12:06:44 -0400

    Fix the ReduxExample app (2) (#945)
    * [Example] Fix the ReduxExample app
    
    Just a bit of cleanup so it can run.
    
    * [Example] Remove AsyncStorage persistence from ReduxExample
    
    It's a cool feature, but isn't necessary to demonstrate how to use
    react-navigation and may cause more confusion than it's worth.

--- examples/ReduxExample/index.js
+++ examples/ReduxExample/index.js
@@ -3,20 +3,15 @@
  */
 
 import React from 'react';
-import { AppRegistry, AsyncStorage } from 'react-native';
+import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { persistStore, autoRehydrate } from 'redux-persist';
 
-import AppReducer from 'src/reducers';
-import AppWithNavigationState from 'src/navigators/AppNavigator';
+import AppReducer from './src/reducers';
+import AppWithNavigationState from './src/navigators/AppNavigator';
 
 class ReduxExampleApp extends React.Component {
-  store = createStore(AppReducer, undefined, autoRehydrate());
-
-  componentDidMount() {
-    persistStore(this.store, { storage: AsyncStorage });
-  }
+  store = createStore(AppReducer);
 
   render() {
     return (
