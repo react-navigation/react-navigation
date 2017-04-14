e15cc78eec93913d97f29df3f73dd3bc4ba6fd87
Mike Grabowski <grabbou@gmail.com>
2017-04-14 07:45:25 +0200

    Update CardStackTransitioner.js

--- src/views/CardStackTransitioner.js
+++ src/views/CardStackTransitioner.js
@@ -107,6 +107,7 @@ class CardStackTransitioner extends Component<DefaultProps, Props, void> {
       headerMode,
       mode,
       router,
+      style,
       cardStyle,
     } = this.props;
     return (
@@ -115,7 +116,8 @@ class CardStackTransitioner extends Component<DefaultProps, Props, void> {
         headerMode={headerMode}
         mode={mode}
         router={router}
-        style={cardStyle}
+        cardStyle={cardStyle}
+        style={style}
         {...props}
       />
     );
