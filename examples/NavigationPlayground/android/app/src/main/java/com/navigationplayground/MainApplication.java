7204036ed3020f910b2957a3f3b19d13aa508707
Mike Grabowski <grabbou@gmail.com>
2017-04-18 14:17:47 +0200

    Clean up

--- examples/NavigationPlayground/android/app/src/main/java/com/navigationplayground/MainApplication.java
+++ examples/NavigationPlayground/android/app/src/main/java/com/navigationplayground/MainApplication.java
@@ -31,7 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage()
+          new VectorIconsPackage()
       );
     }
   };
