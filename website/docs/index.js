f211d0780719d36e273875a1f4ee734520f9ac26
Sami Jaber <jabersami@gmail.com>
2017-04-22 12:13:27 -0400

    add versioning for documentation

---
+++ website/docs/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  '1.0.0-beta.7': require('./1.0.0-beta.7.json'),
+  '1.0.0-beta.8': require('./1.0.0-beta.8.json'),
+  'edge': require('./edge.json'),
+};
--- website/src/AppFrame.js
+++ website/src/AppFrame.js
@@ -1,12 +1,19 @@
 import React from 'react';
-
-import Link from './Link';
-import Footer from './Footer';
 import {
   addNavigationHelpers,
 } from 'react-navigation';
 
-const NavigationLinks = ({navigation, className, reverse}) => {
+import Link from './Link';
+import Footer from './Footer';
+
+import docs from '../docs';
+
+const NavigationLinks = ({
+  childNavigation,
+  navigation,
+  className,
+  reverse,
+}) => {
   let links = [
     ...navigation.state.routes.map((route, i) => {
       if (route.routeName === 'Home' || route.routeName === 'NotFound') {
@@ -36,26 +43,105 @@ const NavigationLinks = ({navigation, className, reverse}) => {
   );
 }
 
+class VersionSelector extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { menuShown: false };
+
+    this.toggleMenuShown = this.toggleMenuShown.bind(this);
+    this.handleVersionClick = this.handleVersionClick.bind(this);
+  }
+
+  toggleMenuShown() {
+    this.setState({ menuShown: !this.state.menuShown });
+  }
+
+  handleVersionClick(version) {
+    const { setParams } = this.props.childNavigation;
+    return () => {
+      setParams({ version });
+    }
+  }
+
+  render() {
+    const { menuShown } = this.state;
+    const { state } = this.props.childNavigation;
+    
+    const isUndef = (obj) => obj === undefined;
+    const docVersionIsUndefined = (state) => (
+      isUndef(state) || 
+      isUndef(state.params) || 
+      isUndef(state.params.version)
+    );
+
+    const currentVersion = docVersionIsUndefined(state) ? 'edge' : state.params.version;
+    const versionsList = Object.keys(docs).map((version) => (
+      <li
+        key={version}
+        onClick={this.handleVersionClick(version)}
+        style={{
+          color: (currentVersion === version) ? '#6b52ae' : '#555'
+        }}
+      >
+        {version}
+      </li>
+    ));
+
+    return (
+      <div className='versionselector'>
+        <span className="versionbutton" onClick={this.toggleMenuShown}>
+          version: {currentVersion} ▼
+        </span>
+        <ul
+          style={{
+            //display: menuShown ? 'inline' : 'none',
+            visibility: menuShown ? 'visible' : 'hidden',
+            listStyleType: 'none',
+          }}
+        >
+          {versionsList}
+        </ul>
+      </div>
+    );
+  }
+}
+
 class AppFrame extends React.Component {
-  state = {isMobileMenuOpen: false};
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isMobileMenuOpen: false,
+    };
+
+    this.setVersion = this.setVersion.bind(this);
+  }
+  
   componentWillReceiveProps(props) {
     if (this.props.navigation.state !== props.navigation.state) {
       this.setState({isMobileMenuOpen: false});
       window.scrollTo(0,0);
     }
   }
+  
+  setVersion(version) {
+    return () => this.setState({ version });
+  }
+
   render() {
-    const {navigation, router} = this.props;
-    const {isMobileMenuOpen} = this.state;
+    const { navigation, router } = this.props;
+    const { isMobileMenuOpen } = this.state;
     const childNavigation = addNavigationHelpers({
       ...navigation,
       state: navigation.state.routes[navigation.state.index],
     });
-    const {state} = navigation;
+    const { state } = navigation;
     const ScreenView = router.getComponentForRouteName(state.routes[state.index].routeName);
-    const {routes, index} = state;
+    const { routes, index } = state;
     const route = routes[index];
     const hasChildNavigation = !!route.routes;
+
     return (
       <div className={`main-app ${isMobileMenuOpen ? 'mobileMenuActive' : ''}`}>
 
@@ -68,7 +154,16 @@ class AppFrame extends React.Component {
             </h1>
           </Link>
 
-          <NavigationLinks navigation={navigation} className="navbuttons" />
+          <VersionSelector
+            className="versionselector"
+            childNavigation={childNavigation}
+          />
+
+          <NavigationLinks
+            navigation={navigation}
+            className="navbuttons"
+            childNavigation={childNavigation}
+          />
 
           {hasChildNavigation && (
             <span
@@ -82,7 +177,12 @@ class AppFrame extends React.Component {
 
         </div></nav>
 
-        <NavigationLinks navigation={navigation} className="mobile-navbuttons" reverse />
+        <NavigationLinks
+          navigation={navigation}
+          className="mobile-navbuttons"
+          childNavigation={childNavigation}
+          reverse
+        />
 
         <ScreenView navigation={childNavigation} />
 
