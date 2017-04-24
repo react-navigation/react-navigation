48963df7708e943b31dc487d020a7496381c9ed9
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-04-25 04:34:33 +0530

    chore: tweak ESLint config

--- packages/react-native-tab-view/example/rn-cli.config.js
+++ packages/react-native-tab-view/example/rn-cli.config.js
@@ -4,9 +4,16 @@ const path = require('path');
 
 module.exports = {
   getProjectRoots() {
-    return [__dirname, path.resolve(__dirname, '..', 'src')];
+    return [__dirname, path.resolve(__dirname, '..')];
   },
   getProvidesModuleNodeModules() {
     return ['react-native', 'react', 'prop-types'];
+  },
+  getBlacklistRE() {
+    return blacklist([
+      new RegExp(
+        `^${escape(path.resolve(__dirname, '..', 'node_modules'))}\\/.*$` // eslint-disable-line prettier/prettier
+      ),
+    ]);
   },
 };
