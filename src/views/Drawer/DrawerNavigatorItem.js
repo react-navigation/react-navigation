d1b27516853da91e16e23587797a222caa743e51
Kevin Cooper <cooperka92@gmail.com>
2017-04-30 20:15:59 -0400

    Factor out DrawerNavigatorItem into its own component

--- src/views/Drawer/DrawerNavigatorItem.js
+++ src/views/Drawer/DrawerNavigatorItem.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import React from 'react';
-import { View, Text, Platform, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 import TouchableItem from '../TouchableItem';
 
@@ -16,7 +16,7 @@ import type { DrawerScene } from './DrawerView.js';
 
 type Props = {
   navigation: NavigationScreenProp<NavigationState, NavigationAction>,
-  items?: Array<NavigationRoute>,
+  route: NavigationRoute,
   activeTintColor?: string,
   activeBackgroundColor?: string,
   inactiveTintColor?: string,
@@ -24,20 +24,19 @@ type Props = {
   getLabel: (scene: DrawerScene) => ?(React.Element<*> | string),
   renderIcon: (scene: DrawerScene) => ?React.Element<*>,
   getDrawerOnPress: (scene: DrawerScene) => ?() => void,
-  style?: Style,
   labelStyle?: Style,
 };
 
 /**
- * Component that renders the navigation list in the drawer.
+ * Component for each item in the drawer.
  */
-const DrawerNavigatorItems = (
+const DrawerNavigatorItem = (
   {
     navigation: {
       state,
       navigate,
     },
-    items,
+    route,
     activeTintColor,
     activeBackgroundColor,
     inactiveTintColor,
@@ -45,67 +44,57 @@ const DrawerNavigatorItems = (
     getLabel,
     renderIcon,
     getScreenOptions,
-    style,
     labelStyle,
   }: Props,
-) => (
-  <View style={[styles.container, style]}>
-    {(items || state.routes).map((route: NavigationRoute, index: number) => {
-      const { drawerOnPress } = getScreenOptions(route.key);
-      const focused = state.routes[state.index].key === route.key;
-      const color = focused ? activeTintColor : inactiveTintColor;
-      const backgroundColor = focused
-        ? activeBackgroundColor
-        : inactiveBackgroundColor;
-      const scene = { route, index, focused, tintColor: color };
-      const icon = renderIcon(scene);
-      const label = getLabel(scene);
+) => {
+  const { drawerOnPress } = getScreenOptions(route.key);
+  const focused = state.routes[state.index].key === route.key;
+  const color = focused ? activeTintColor : inactiveTintColor;
+  const backgroundColor = focused
+    ? activeBackgroundColor
+    : inactiveBackgroundColor;
+  const scene = { route, focused, tintColor: color };
+  const icon = renderIcon(scene);
+  const label = getLabel(scene);
 
-      const drawerItem = (
-        <View style={[styles.item, { backgroundColor }]}>
-          {icon
-            ? <View style={[styles.icon, focused ? null : styles.inactiveIcon]}>
-              {icon}
-            </View>
-            : null}
-          {typeof label === 'string'
-            ? <Text style={[styles.label, { color }, labelStyle]}>
-              {label}
-            </Text>
-            : label}
-        </View>
-      );
-
-      if (drawerOnPress === null) {
-        return (
-          <View key={route.key}>
-            {drawerItem}
+  const drawerItem = (
+    <View style={[styles.item, { backgroundColor }]}>
+      {icon
+        ? <View style={[styles.icon, focused ? null : styles.inactiveIcon]}>
+            {icon}
           </View>
-        );
-      }
+        : null}
+      {typeof label === 'string'
+        ? <Text style={[styles.label, { color }, labelStyle]}>
+            {label}
+          </Text>
+        : label}
+    </View>
+  );
 
-      return (
-        <TouchableItem
-          key={route.key}
-          onPress={
-            drawerOnPress
-              ? () => drawerOnPress()
-              : () => {
+  if (drawerOnPress === null) {
+    return drawerItem;
+  }
+
+  return (
+    <TouchableItem
+      onPress={
+        drawerOnPress
+          ? () => drawerOnPress()
+          : () => {
               navigate('DrawerClose');
               navigate(route.routeName);
             }
-          }
-          delayPressIn={0}
-        >
-          {drawerItem}
-        </TouchableItem>
-      );
-    })}
-  </View>
-);
+      }
+      delayPressIn={0}
+    >
+      {drawerItem}
+    </TouchableItem>
+  );
+};
 
 /* Material design specs - https://material.io/guidelines/patterns/navigation-drawer.html#navigation-drawer-specs */
-DrawerNavigatorItems.defaultProps = {
+DrawerNavigatorItem.defaultProps = {
   activeTintColor: '#2196f3',
   activeBackgroundColor: 'rgba(0, 0, 0, .04)',
   inactiveTintColor: 'rgba(0, 0, 0, .87)',
@@ -113,10 +102,6 @@ DrawerNavigatorItems.defaultProps = {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: Platform.OS === 'ios' ? 20 : 0,
-    paddingVertical: 4,
-  },
   item: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -139,4 +124,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default DrawerNavigatorItems;
+export default DrawerNavigatorItem;
