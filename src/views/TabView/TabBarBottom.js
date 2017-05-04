d3beeb853ef0740a4ab618f79a066367fe9bb12d
milen-3web <milen@3web.bg>
2017-05-04 08:54:46 +0300

    Formatting with prettier

--- src/views/TabView/TabBarBottom.js
+++ src/views/TabView/TabBarBottom.js
@@ -1,194 +1,189 @@
 /* @flow */
 
 import React, { PureComponent } from 'react';
-import {
-	Animated,
-	View,
-	TouchableWithoutFeedback,
-	StyleSheet,
-} from 'react-native';
+import { Animated, TouchableWithoutFeedback, StyleSheet } from 'react-native';
 import TabBarIcon from './TabBarIcon';
 
 import type {
-	NavigationAction,
-		NavigationRoute,
-		NavigationState,
-		NavigationScreenProp,
-		Style,
+  NavigationAction,
+  NavigationRoute,
+  NavigationState,
+  NavigationScreenProp,
+  Style,
 } from '../../TypeDefinition';
 
 import type { TabScene } from './TabView';
 
 type DefaultProps = {
-	activeTintColor: string,
-	activeBackgroundColor: string,
-	inactiveTintColor: string,
-	inactiveBackgroundColor: string,
-	showLabel: boolean,
+  activeTintColor: string,
+  activeBackgroundColor: string,
+  inactiveTintColor: string,
+  inactiveBackgroundColor: string,
+  showLabel: boolean,
 };
 
 type Props = {
-	activeTintColor: string,
-	activeBackgroundColor: string,
-	inactiveTintColor: string,
-	inactiveBackgroundColor: string,
-	position: Animated.Value,
-	navigation: NavigationScreenProp<NavigationState, NavigationAction>,
-	jumpToIndex: (index: number) => void,
-	getLabel: (scene: TabScene) => ?(React.Element<*> | string),
-	renderIcon: (scene: TabScene) => React.Element<*>,
-		showLabel: boolean,
-			style ?: Style,
-			labelStyle ?: Style,
-			showIcon: boolean,
+  activeTintColor: string,
+  activeBackgroundColor: string,
+  inactiveTintColor: string,
+  inactiveBackgroundColor: string,
+  position: Animated.Value,
+  navigation: NavigationScreenProp<NavigationState, NavigationAction>,
+  jumpToIndex: (index: number) => void,
+  getLabel: (scene: TabScene) => ?(React.Element<*> | string),
+  renderIcon: (scene: TabScene) => React.Element<*>,
+  showLabel: boolean,
+  style?: Style,
+  labelStyle?: Style,
+  showIcon: boolean,
 };
 
 export default class TabBarBottom
-	extends PureComponent<DefaultProps, Props, void> {
-	// See https://developer.apple.com/library/content/documentation/UserExperience/Conceptual/UIKitUICatalog/UITabBar.html
-	static defaultProps = {
-		activeTintColor: '#3478f6', // Default active tint color in iOS 10
-		activeBackgroundColor: 'transparent',
-		inactiveTintColor: '#929292', // Default inactive tint color in iOS 10
-		inactiveBackgroundColor: 'transparent',
-		showLabel: true,
-		showIcon: true,
-	};
+  extends PureComponent<DefaultProps, Props, void> {
+  // See https://developer.apple.com/library/content/documentation/UserExperience/Conceptual/UIKitUICatalog/UITabBar.html
+  static defaultProps = {
+    activeTintColor: '#3478f6', // Default active tint color in iOS 10
+    activeBackgroundColor: 'transparent',
+    inactiveTintColor: '#929292', // Default inactive tint color in iOS 10
+    inactiveBackgroundColor: 'transparent',
+    showLabel: true,
+    showIcon: true,
+  };
 
-	props: Props;
+  props: Props;
 
-	_renderLabel = (scene: TabScene) => {
-		const {
+  _renderLabel = (scene: TabScene) => {
+    const {
       position,
-			navigation,
-			activeTintColor,
-			inactiveTintColor,
-			labelStyle,
-			showLabel,
+      navigation,
+      activeTintColor,
+      inactiveTintColor,
+      labelStyle,
+      showLabel,
     } = this.props;
-		if (showLabel === false) {
-			return null;
-		}
-		const { index } = scene;
-		const { routes } = navigation.state;
-		// Prepend '-1', so there are always at least 2 items in inputRange
-		const inputRange = [-1, ...routes.map((x: *, i: number) => i)];
-		const outputRange = inputRange.map(
-			(inputIndex: number) =>
-				inputIndex === index ? activeTintColor : inactiveTintColor,
-		);
-		const color = position.interpolate({
-			inputRange,
-			outputRange,
-		});
+    if (showLabel === false) {
+      return null;
+    }
+    const { index } = scene;
+    const { routes } = navigation.state;
+    // Prepend '-1', so there are always at least 2 items in inputRange
+    const inputRange = [-1, ...routes.map((x: *, i: number) => i)];
+    const outputRange = inputRange.map(
+      (inputIndex: number) =>
+        inputIndex === index ? activeTintColor : inactiveTintColor,
+    );
+    const color = position.interpolate({
+      inputRange,
+      outputRange,
+    });
 
-		const label = this.props.getLabel(scene);
-		if (typeof label === 'string') {
-			return (
-				<Animated.Text style={[styles.label, { color }, labelStyle]}>
-					{label}
-				</Animated.Text>
-			);
-		}
-		if (typeof label === 'function') {
-			return label(scene);
-		}
+    const label = this.props.getLabel(scene);
+    if (typeof label === 'string') {
+      return (
+        <Animated.Text style={[styles.label, { color }, labelStyle]}>
+          {label}
+        </Animated.Text>
+      );
+    }
+    if (typeof label === 'function') {
+      return label(scene);
+    }
 
-		return label;
-	};
+    return label;
+  };
 
-	_renderIcon = (scene: TabScene) => {
-		const {
+  _renderIcon = (scene: TabScene) => {
+    const {
       position,
-			navigation,
-			activeTintColor,
-			inactiveTintColor,
-			renderIcon,
-			showIcon,
+      navigation,
+      activeTintColor,
+      inactiveTintColor,
+      renderIcon,
+      showIcon,
     } = this.props;
-		if (showIcon === false) {
-			return null;
-		}
-		return (
-			<TabBarIcon
-				position={position}
-				navigation={navigation}
-				activeTintColor={activeTintColor}
-				inactiveTintColor={inactiveTintColor}
-				renderIcon={renderIcon}
-				scene={scene}
-				style={styles.icon}
-			/>
-		);
-	};
+    if (showIcon === false) {
+      return null;
+    }
+    return (
+      <TabBarIcon
+        position={position}
+        navigation={navigation}
+        activeTintColor={activeTintColor}
+        inactiveTintColor={inactiveTintColor}
+        renderIcon={renderIcon}
+        scene={scene}
+        style={styles.icon}
+      />
+    );
+  };
 
-	render() {
-		const {
+  render() {
+    const {
       position,
-			navigation,
-			jumpToIndex,
-			activeBackgroundColor,
-			inactiveBackgroundColor,
-			style,
+      navigation,
+      jumpToIndex,
+      activeBackgroundColor,
+      inactiveBackgroundColor,
+      style,
     } = this.props;
-		const { routes } = navigation.state;
-		// Prepend '-1', so there are always at least 2 items in inputRange
-		const inputRange = [-1, ...routes.map((x: *, i: number) => i)];
-		return (
-			<Animated.View style={[styles.tabBar, style]}>
-				{routes.map((route: NavigationRoute, index: number) => {
-					const focused = index === navigation.state.index;
-					const scene = { route, index, focused };
-					const outputRange = inputRange.map(
-						(inputIndex: number) =>
-							inputIndex === index
-								? activeBackgroundColor
-								: inactiveBackgroundColor,
-					);
-					const backgroundColor = position.interpolate({
-						inputRange,
-						outputRange,
-					});
-					const justifyContent = this.props.showIcon ? 'flex-end' : 'center';
-					return (
-						<TouchableWithoutFeedback
-							key={route.key}
-							onPress={() => jumpToIndex(index)}
-						>
-							<Animated.View
-								style={[styles.tab, { backgroundColor, justifyContent }]}
-							>
-								{this._renderIcon(scene)}
-								{this._renderLabel(scene)}
-							</Animated.View>
-						</TouchableWithoutFeedback>
-					);
-				})}
-			</Animated.View>
-		);
-	}
+    const { routes } = navigation.state;
+    // Prepend '-1', so there are always at least 2 items in inputRange
+    const inputRange = [-1, ...routes.map((x: *, i: number) => i)];
+    return (
+      <Animated.View style={[styles.tabBar, style]}>
+        {routes.map((route: NavigationRoute, index: number) => {
+          const focused = index === navigation.state.index;
+          const scene = { route, index, focused };
+          const outputRange = inputRange.map(
+            (inputIndex: number) =>
+              inputIndex === index
+                ? activeBackgroundColor
+                : inactiveBackgroundColor,
+          );
+          const backgroundColor = position.interpolate({
+            inputRange,
+            outputRange,
+          });
+          const justifyContent = this.props.showIcon ? 'flex-end' : 'center';
+          return (
+            <TouchableWithoutFeedback
+              key={route.key}
+              onPress={() => jumpToIndex(index)}
+            >
+              <Animated.View
+                style={[styles.tab, { backgroundColor, justifyContent }]}
+              >
+                {this._renderIcon(scene)}
+                {this._renderLabel(scene)}
+              </Animated.View>
+            </TouchableWithoutFeedback>
+          );
+        })}
+      </Animated.View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	tabBar: {
-		height: 49, // Default tab bar height in iOS 10
-		flexDirection: 'row',
-		borderTopWidth: StyleSheet.hairlineWidth,
-		borderTopColor: 'rgba(0, 0, 0, .2)',
-		backgroundColor: '#f4f4f4', // Default background color in iOS 10
-	},
-	tab: {
-		flex: 1,
-		alignItems: 'stretch',
-		justifyContent: 'flex-end',
-	},
-	icon: {
-		flexGrow: 1,
-	},
-	label: {
-		textAlign: 'center',
-		fontSize: 10,
-		marginBottom: 1.5,
-		backgroundColor: 'transparent',
-	},
+  tabBar: {
+    height: 49, // Default tab bar height in iOS 10
+    flexDirection: 'row',
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderTopColor: 'rgba(0, 0, 0, .2)',
+    backgroundColor: '#f4f4f4', // Default background color in iOS 10
+  },
+  tab: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'flex-end',
+  },
+  icon: {
+    flexGrow: 1,
+  },
+  label: {
+    textAlign: 'center',
+    fontSize: 10,
+    marginBottom: 1.5,
+    backgroundColor: 'transparent',
+  },
 });
