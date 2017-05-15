3b6eee6367cbcda7c410413d5b026a7043469a16
Adam Miskiewicz <adam@exponentjs.com>
2017-05-14 22:11:03 -0700

    Split test commands into test.sh, respect CIRCLE_NODE_INDEX

---
+++ scripts/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eo pipefail
+
+case $CIRCLE_NODE_INDEX in 
+  0) yarn test ;; 
+  1) cd examples/NavigationPlayground && yarn test ;; 
+  2) cd examples/ReduxExample && yarn test ;; 
+esac
