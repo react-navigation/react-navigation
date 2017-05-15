fdedb6540c12b3f6579e1865435051421bc79876
Adam Miskiewicz <adam@exponentjs.com>
2017-05-14 23:14:22 -0700

    More CI parallelization optimizations

--- packages/react-navigation/scripts/test.sh
+++ packages/react-navigation/scripts/test.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 case $CIRCLE_NODE_INDEX in 
-  0) yarn test && yarn codecov ;; 
-  1) cd examples/NavigationPlayground && yarn test ;; 
-  2) cd examples/ReduxExample && yarn test ;; 
+  0) yarn && yarn test && yarn codecov ;; 
+  1) cd examples/NavigationPlayground && yarn && yarn test ;; 
+  2) cd examples/ReduxExample && yarn && yarn test ;; 
 esac
