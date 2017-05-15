9e2f237859e55693cfda5626f504aca8f882544f
Gregory <gregorynative@gmail.com>
2017-05-15 14:42:10 +0300

    support old versions

--- src/PlatformHelpers.native.js
+++ src/PlatformHelpers.native.js
@@ -1,5 +1,15 @@
 /* @flow */
 
-import { BackHandler, Linking } from 'react-native';
+/* @flow */
 
-export { BackHandler, Linking };
+import {
+  BackAndroid as OldBackAndroid,
+  BackHandler as NewBackAndroid,
+  Linking,
+} from 'react-native';
+
+const fullVersion = require('react-native/package.json').version.split('.')[1];
+
+let BackHandler = fullVersion < 44 ? OldBackAndroid : NewBackAndroid;
+
+export { BackHandler, Linking } from 'react-native';
