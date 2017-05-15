e664038817ddb0a7d081bf9c196feee4f0e0d7a2
Gregory <gregorynative@gmail.com>
2017-05-15 12:38:43 +0300

    Update createNavigationContainer.js

--- src/createNavigationContainer.js
+++ src/createNavigationContainer.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import invariant from 'fbjs/lib/invariant';
-import { BackAndroid, Linking } from './PlatformHelpers';
+import { BackHandler, Linking } from './PlatformHelpers';
 import NavigationActions from './NavigationActions';
 import addNavigationHelpers from './addNavigationHelpers';
 
@@ -153,7 +153,7 @@ export default function createNavigationContainer<T: *>(
         return;
       }
 
-      this.subs = BackAndroid.addEventListener('backPress', () =>
+      this.subs = BackHandler.addEventListener('backPress', () =>
         this.dispatch(NavigationActions.back())
       );
 
