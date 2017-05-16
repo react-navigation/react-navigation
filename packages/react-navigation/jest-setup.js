f816088a4cee90f128df32b6d28ce6b4c1e62ffc
Adam Miskiewicz <adam@exponentjs.com>
2017-05-15 19:02:46 -0700

    Restore jest-setup.js file

---
+++ packages/react-navigation/jest-setup.js
@@ -0,0 +1,28 @@
+/**
+ * @flow
+ * eslint-env jest 
+ */
+
+// See https://github.com/facebook/jest/issues/2208
+jest.mock('Linking', () => ({
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  openURL: jest.fn(),
+  canOpenURL: jest.fn(),
+  getInitialURL: jest
+    .fn()
+    .mockImplementation((value: string) => Promise.resolve(value)),
+}));
+
+// See https://github.com/facebook/react-native/issues/11659
+jest.mock('ScrollView', () => {
+  // $FlowExpectedError
+  const RealComponent = require.requireActual('ScrollView');
+  class ScrollView extends RealComponent {
+    scrollTo = () => {};
+  }
+  return ScrollView;
+});
+
+// $FlowExpectedError
+Date.now = jest.fn(() => 0);
