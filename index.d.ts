d210aa39d322ad23b29857c8b82215ac9528c0da
Gabriel Bull <me@gabrielbull.com>
2017-06-23 15:09:19 -0400

    Added withNavigation to typescript definition

--- index.d.ts
+++ index.d.ts
@@ -660,3 +660,9 @@ export class Transitioner extends React.Component<
 > {
 
 }
+
+type InjectedProps = {
+  navigation: NavigationScreenProp<any, NavigationAction>;
+}
+
+export function withNavigation<P>(component: React.SFC<P & InjectedProps> | React.ComponentClass<P & InjectedProps> | React.ClassType<P & InjectedProps, any, any>): React.ComponentClass<P>;
