b8904cd00f4dd5294dea821da3a24ddfd90d4581
Kevin Cooper <cooperka92@gmail.com>
2017-07-18 12:20:21 -0700

    [Playground] Add some example items to the Drawer demo

--- examples/NavigationPlayground/js/Drawer.js
+++ examples/NavigationPlayground/js/Drawer.js
@@ -3,8 +3,8 @@
  */
 
 import React from 'react';
-import { Button, Platform, ScrollView, StyleSheet } from 'react-native';
-import { DrawerNavigator } from 'react-navigation';
+import { View, Button, Platform, ScrollView, StyleSheet } from 'react-native';
+import { DrawerNavigator, DrawerItems, DrawerItem } from 'react-navigation';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import SampleText from './SampleText';
 
@@ -43,28 +43,132 @@ DraftsScreen.navigationOptions = {
   ),
 };
 
-const DrawerExample = DrawerNavigator(
+const CustomDrawerItem = (props) => {
+  const { item, focused, activeTintColor, inactiveTintColor, onPress: defaultOnPress } = props;
+
+  const tintColor = focused ? activeTintColor : inactiveTintColor;
+
+  const screenOptions = typeof item.screenOptions === 'function'
+    ? item.screenOptions(props)
+    : item.screenOptions;
+  const { drawerIcon, drawerLabel, onPress: customOnPress } = screenOptions;
+
+  return (
+    <DrawerItem
+      {...props}
+      focused={false}
+      icon={drawerIcon ? drawerIcon({ tintColor }) : null}
+      label={drawerLabel}
+      onPress={customOnPress !== undefined ? customOnPress : defaultOnPress}
+    />
+  );
+};
+
+const CustomDrawerItems = (props) => {
+  const { items, onItemPress, ...itemProps } = props;
+
+  return (
+    <View style={[styles.drawerContainer]}>
+      {items.map((item) => {
+        const { routeName, key } = item;
+
+        // Simply render the default component if the item is a route.
+        if (routeName) {
+          return (
+            <DrawerItems
+              {...props}
+              key={key}
+              items={[item]}
+              style={styles.drawerContainerInner}
+            />
+          );
+        }
+
+        return (
+          <CustomDrawerItem
+            {...itemProps}
+            key={key}
+            item={item}
+            onPress={onItemPress}
+          />
+        );
+      })}
+    </View>
+  );
+};
+
+// The routes themselves need to be specified even if you're overriding them with custom `items`;
+// otherwise, React Navigation wouldn't know which components to use.
+const drawerRoutes = {
+  Inbox: {
+    path: '/',
+    screen: InboxScreen,
+  },
+  Drafts: {
+    path: '/sent',
+    screen: DraftsScreen,
+  },
+};
+
+// Add a few custom items along with the regular ones.
+//
+// This array is optional; if you don't provide it,
+// the drawer will automatically show all of your routes that were defined above.
+const customDrawerItems = [
   {
-    Inbox: {
-      path: '/',
-      screen: InboxScreen,
-    },
-    Drafts: {
-      path: '/sent',
-      screen: DraftsScreen,
-    },
+    key: 'RefreshButton',
+    screenOptions: ({ navigation }) => ({
+      drawerLabel: navigation.state.params.isRefreshing ? 'Refreshing...' : 'Refresh',
+      drawerIcon: ({ tintColor }) => (
+        <MaterialIcons
+          name="refresh"
+          size={24}
+          style={{ color: tintColor }}
+        />
+      ),
+      onPress: () => {
+        navigation.setParams({ isRefreshing: true });
+
+        // Finish refreshing after a short delay.
+        setTimeout(() => {
+          navigation.setParams({ isRefreshing: false });
+        }, 500);
+      },
+    })
   },
   {
-    initialRouteName: 'Drafts',
-    contentOptions: {
-      activeTintColor: '#e91e63',
-    },
-  }
-);
+    key: 'MailboxesLabel',
+    screenOptions: {
+      drawerLabel: 'Mailboxes',
+      onPress: null,
+    }
+  },
+  { key: 'Inbox', routeName: 'Inbox' },
+  { key: 'Drafts', routeName: 'Drafts' },
+];
+
+const DrawerExample = DrawerNavigator(drawerRoutes, {
+  initialRouteName: 'Drafts',
+  contentComponent: CustomDrawerItems,
+  contentOptions: {
+    activeTintColor: '#e91e63',
+
+    // Display the custom items defined above, rather than the default ones.
+    items: customDrawerItems,
+  },
+});
 
 const styles = StyleSheet.create({
   container: {
     marginTop: Platform.OS === 'ios' ? 20 : 0,
+  },
+  drawerContainer: {
+    marginTop: Platform.OS === 'ios' ? 20 : 0,
+    paddingVertical: 4,
+  },
+  drawerContainerInner: {
+    marginTop: 0,
+    paddingVertical: 0,
   },
 });
 
