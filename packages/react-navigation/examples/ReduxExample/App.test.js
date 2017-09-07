4adce69e70f5b46128882dcfec77c7df27a41fa8
Kevin Cooper <cooperka92@gmail.com>
2017-09-06 22:02:57 -0400

    Upgrade the ReduxExample app (use CRNA) (#2171)
    * [ReduxExample] Delete android and ios dirs
    
    * [ReduxExample] Upgrade using create-react-native-app
    
    * [ReduxExample] Add postinstall script to clean up files Yarn copies in
    
    Yarn copies them when installing locally, causing lots of @proviesModule collisions.
    
    * [ReduxExample] Upgrade expo and other dependencies
    
    * [ReduxExample] Don't delete .git just in case

---
+++ packages/react-navigation/examples/ReduxExample/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from './App';
+
+import renderer from 'react-test-renderer';
+
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
