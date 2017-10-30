33c97c8239256868a6a4a6333cd8c7a942d98692
ashoat <ashoat@gmail.com>
2017-10-30 10:16:42 -0400

    Silence last two Flow errors regarding withCachedChildNavigation injecting childNavigationProps
    These are the only two errors I haven't been able to figure out at all. If I had more time I'd try and figure out the simplest case and report it to the Flow team, but I've been working on this for a week already and have to move on.
    
    The issue at hand is that `withCachedChildNavigation` injects `childNavigationProps`, but Flow doesn't see this and thinks it needs to be specified by the view component. We're using the HOC pattern suggested by the Flow docs, and I've tried several other patterns to no avail.

--- src/navigators/TabNavigator.js
+++ src/navigators/TabNavigator.js
@@ -50,6 +50,8 @@ const TabNavigator = (
     config,
     NavigatorTypes.TABS
   )((props: *) => (
+    // Flow doesn't realize TabView already has childNavigationProps from
+    // withCachedChildNavigation for some reason. $FlowFixMe
     <TabView
       {...props}
       tabBarComponent={tabBarComponent}
