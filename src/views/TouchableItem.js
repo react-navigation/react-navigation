3d4ca357987101ebc5a82aabccaefe67777ad5ed
Vojtech Novak <vonovak@gmail.com>
2017-11-02 00:08:25 +0100

    Update TouchableItem.js

--- src/views/TouchableItem.js
+++ src/views/TouchableItem.js
@@ -59,9 +59,7 @@ export default class TouchableItem extends React.Component<Props> {
             this.props.borderless || false
           )}
         >
-          <View style={style}>
-            {React.Children.only(this.props.children)}
-          </View>
+          <View style={style}>{React.Children.only(this.props.children)}</View>
         </TouchableNativeFeedback>
       );
     }
