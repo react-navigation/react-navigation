react-navigation/react-navigation

commit 185289ad6658e07783c807ee7ac29b9e1244fa21

Add iPhone X SafeAreaView (#2833)
* Add SafeAreaView - JS only version

* Add SafeAreaView

* Looking pretty good

* Small refactor

* Remove console.log

* Fix merge conflict with Header flow types.

* Fix conflict with itemsContainerStyle.

* Fix merge conflict.

* Fix merge conflict, yarn and package.json from fixflow

* Fix merge conflict, navigation playground yarn.lock and package.json with fixflow

* Now it can work on lower versions of RN

* Snapshots merge conflict.

* Update DrawerNavigator snapshot.

* Fix conflict with iconContainerStyle

* Add support for correct status bar height on iPad.

* Update jest snapshots.

* Update StackNavigator snapshot.

* Use modulo instead of while

* Fix landscape tab bar width on < iOS 11

src/views/Header/Header.js
@@ -6,6 +6,7 @@ import * as React from 'react';
 
 import {
   Animated,
+  Dimensions,
   Platform,
   StyleSheet,
   View,
@@ -15,6 +16,7 @@ import {
 import HeaderTitle from './HeaderTitle';
 import HeaderBackButton from './HeaderBackButton';
 import HeaderStyleInterpolator from './HeaderStyleInterpolator';
+import SafeAreaView from '../SafeAreaView';
 import withOrientation from '../withOrientation';
 
 import type {
@@ -42,13 +44,10 @@ type State = {
 };
 
 const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
-const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : 0;
 const TITLE_OFFSET = Platform.OS === 'ios' ? 70 : 56;
 
 type Props = HeaderProps & { isLandscape: boolean };
 class Header extends React.PureComponent<Props, State> {
-  static HEIGHT = APPBAR_HEIGHT + STATUSBAR_HEIGHT;
-
   state = {
     widths: {},
   };
@@ -304,20 +303,23 @@ class Header extends React.PureComponent<Props, State> {
 
     const { options } = this.props.getScreenDetails(scene);
     const headerStyle = options.headerStyle;
-    const landscapeAwareStatusBarHeight = isLandscape ? 0 : STATUSBAR_HEIGHT;
+    const appBarHeight = Platform.OS === 'ios' ? (isLandscape ? 32 : 44) : 56;
     const containerStyles = [
-      styles.container,
       {
-        paddingTop: landscapeAwareStatusBarHeight,
-        height: APPBAR_HEIGHT + landscapeAwareStatusBarHeight,
+        height: appBarHeight,
       },
       headerStyle,
     ];
 
     return (
-      <Animated.View {...rest} style={containerStyles}>
-        <View style={styles.appBar}>{appBar}</View>
-      </Animated.View>
+      <SafeAreaView
+        style={styles.container}
+        forceInset={{ top: 'always', bottom: 'never' }}
+      >
+        <Animated.View {...rest} style={containerStyles}>
+          <View style={styles.appBar}>{appBar}</View>
+        </Animated.View>
+      </SafeAreaView>
     );
   }
 }

examples/NavigationPlayground/js/App.js
@@ -13,7 +13,7 @@ import {
   Text,
   View,
 } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { SafeAreaView, StackNavigator } from 'react-navigation';
 
 import Banner from './Banner';
 import CustomTabs from './CustomTabs';
@@ -58,12 +58,14 @@ const ExampleRoutes = {
     screen: CustomTransitioner,
   },
   ModalStack: {
-    name: Platform.OS === 'ios'
-      ? 'Modal Stack Example'
-      : 'Stack with Dynamic Header',
-    description: Platform.OS === 'ios'
-      ? 'Stack navigation with modals'
-      : 'Dynamically showing and hiding the header',
+    name:
+      Platform.OS === 'ios'
+        ? 'Modal Stack Example'
+        : 'Stack with Dynamic Header',
+    description:
+      Platform.OS === 'ios'
+        ? 'Stack navigation with modals'
+        : 'Dynamically showing and hiding the header',
     screen: ModalStack,
   },
   StacksInTabs: {
@@ -91,7 +93,7 @@ const ExampleRoutes = {
 };
 
 const MainScreen = ({ navigation }) => (
-  <ScrollView>
+  <ScrollView style={{ flex: 1 }} contentInsetAdjustmentBehavior="automatic">
     <Banner />
     {Object.keys(ExampleRoutes).map((routeName: string) => (
       <TouchableOpacity
@@ -103,12 +105,17 @@ const MainScreen = ({ navigation }) => (
           navigation.navigate(routeName, {}, action);
         }}
       >
-        <View style={styles.item}>
-          <Text style={styles.title}>{ExampleRoutes[routeName].name}</Text>
-          <Text style={styles.description}>
-            {ExampleRoutes[routeName].description}
-          </Text>
-        </View>
+        <SafeAreaView
+          style={styles.itemContainer}
+          forceInset={{ vertical: 'never' }}
+        >
+          <View style={styles.item}>
+            <Text style={styles.title}>{ExampleRoutes[routeName].name}</Text>
+            <Text style={styles.description}>
+              {ExampleRoutes[routeName].description}
+            </Text>
+          </View>
+        </SafeAreaView>
       </TouchableOpacity>
     ))}
   </ScrollView>
@@ -137,9 +144,11 @@ export default () => <AppNavigator />;
 
 const styles = StyleSheet.create({
   item: {
-    backgroundColor: '#fff',
     paddingHorizontal: 16,
     paddingVertical: 12,
+  },
+  itemContainer: {
+    backgroundColor: '#fff',
     borderBottomWidth: StyleSheet.hairlineWidth,
     borderBottomColor: '#ddd',
   },

examples/NavigationPlayground/js/Banner.js
@@ -3,23 +3,30 @@
 import React from 'react';
 
 import { Image, Platform, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
 
 const Banner = () => (
-  <View style={styles.banner}>
-    <Image source={require('./assets/NavLogo.png')} style={styles.image} />
-    <Text style={styles.title}>React Navigation Examples</Text>
-  </View>
+  <SafeAreaView
+    style={styles.bannerContainer}
+    forceInset={{ vertical: 'never' }}
+  >
+    <View style={styles.banner}>
+      <Image source={require('./assets/NavLogo.png')} style={styles.image} />
+      <Text style={styles.title}>React Navigation Examples</Text>
+    </View>
+  </SafeAreaView>
 );
 
 export default Banner;
 
 const styles = StyleSheet.create({
-  banner: {
+  bannerContainer: {
     backgroundColor: '#673ab7',
+  },
+  banner: {
     flexDirection: 'row',
     alignItems: 'center',
     padding: 16,
-    marginTop: Platform.OS === 'ios' ? 20 : 0,
   },
   image: {
     width: 36,

examples/NavigationPlayground/js/CustomTabs.js
@@ -15,6 +15,7 @@ import {
 import {
   createNavigator,
   createNavigationContainer,
+  SafeAreaView,
   TabRouter,
   addNavigationHelpers,
 } from 'react-navigation';
@@ -22,13 +23,15 @@ import SampleText from './SampleText';
 
 const MyNavScreen = ({ navigation, banner }) => (
   <ScrollView>
-    <SampleText>{banner}</SampleText>
-    <Button
-      onPress={() => {
-        navigation.goBack(null);
-      }}
-      title="Go back"
-    />
+    <SafeAreaView forceInset={{ horizontal: 'always' }}>
+      <SampleText>{banner}</SampleText>
+      <Button
+        onPress={() => {
+          navigation.goBack(null);
+        }}
+        title="Go back"
+      />
+    </SafeAreaView>
   </ScrollView>
 );
 
@@ -47,7 +50,7 @@ const MySettingsScreen = ({ navigation }) => (
 const CustomTabBar = ({ navigation }) => {
   const { routes } = navigation.state;
   return (
-    <View style={styles.tabContainer}>
+    <SafeAreaView style={styles.tabContainer}>
       {routes.map(route => (
         <TouchableOpacity
           onPress={() => navigation.navigate(route.routeName)}
@@ -57,7 +60,7 @@ const CustomTabBar = ({ navigation }) => {
           <Text>{route.routeName}</Text>
         </TouchableOpacity>
       ))}
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -65,7 +68,7 @@ const CustomTabView = ({ router, navigation }) => {
   const { routes, index } = navigation.state;
   const ActiveScreen = router.getComponentForRouteName(routes[index].routeName);
   return (
-    <View style={styles.container}>
+    <SafeAreaView forceInset={{ top: 'always' }}>
       <CustomTabBar navigation={navigation} />
       <ActiveScreen
         navigation={addNavigationHelpers({
@@ -73,7 +76,7 @@ const CustomTabView = ({ router, navigation }) => {
           state: routes[index],
         })}
       />
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -103,9 +106,6 @@ const CustomTabs = createNavigationContainer(
 );
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: Platform.OS === 'ios' ? 20 : 0,
-  },
   tabContainer: {
     flexDirection: 'row',
     height: 48,

examples/NavigationPlayground/js/CustomTransitioner.js
@@ -10,28 +10,27 @@ import {
 } from 'react-native';
 import {
   Transitioner,
+  SafeAreaView,
   StackRouter,
   createNavigationContainer,
   addNavigationHelpers,
-  createNavigator
+  createNavigator,
 } from 'react-navigation';
 import SampleText from './SampleText';
 
 const MyNavScreen = ({ navigation, banner }) => (
-  <View>
+  <SafeAreaView forceInset={{ top: 'always' }}>
     <SampleText>{banner}</SampleText>
-    {navigation.state && navigation.state.routeName !== 'Settings' &&
-      <Button
-        onPress={() => navigation.navigate('Settings')}
-        title="Go to a settings screen"
-      />
-    }
-    
-    <Button
-      onPress={() => navigation.goBack(null)}
-      title="Go back"
-    />
-  </View>
+    {navigation.state &&
+      navigation.state.routeName !== 'Settings' && (
+        <Button
+          onPress={() => navigation.navigate('Settings')}
+          title="Go to a settings screen"
+        />
+      )}
+
+    <Button onPress={() => navigation.goBack(null)} title="Go back" />
+  </SafeAreaView>
 );
 
 const MyHomeScreen = ({ navigation }) => (
@@ -63,13 +62,11 @@ class CustomNavigationView extends Component {
   }
 
   _render = (transitionProps, prevTransitionProps) => {
-    const scenes = transitionProps.scenes.map(scene => this._renderScene(transitionProps, scene));
-    return (
-      <View style={{ flex: 1 }}>
-        {scenes}
-      </View>
+    const scenes = transitionProps.scenes.map(scene =>
+      this._renderScene(transitionProps, scene)
     );
-  }
+    return <View style={{ flex: 1 }}>{scenes}</View>;
+  };
 
   _renderScene = (transitionProps, scene) => {
     const { navigation, router } = this.props;
@@ -84,9 +81,7 @@ class CustomNavigationView extends Component {
 
     const animation = {
       opacity: animatedValue,
-      transform: [
-        { scale: animatedValue },
-      ],
+      transform: [{ scale: animatedValue }],
     };
 
     // The prop `router` is populated when we call `createNavigator`.
@@ -100,8 +95,8 @@ class CustomNavigationView extends Component {
           })}
         />
       </Animated.View>
-    )
-  }
+    );
+  };
 }
 
 const CustomRouter = StackRouter({
@@ -116,9 +111,6 @@ const CustomTransitioner = createNavigationContainer(
 export default CustomTransitioner;
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: Platform.OS === 'ios' ? 20 : 0,
-  },
   view: {
     position: 'absolute',
     left: 0,

examples/NavigationPlayground/js/Drawer.js
@@ -3,19 +3,21 @@
  */
 
 import React from 'react';
-import { Button, Platform, ScrollView, StyleSheet } from 'react-native';
-import { DrawerNavigator } from 'react-navigation';
+import { Button, Platform, ScrollView } from 'react-native';
+import { DrawerNavigator, SafeAreaView } from 'react-navigation';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import SampleText from './SampleText';
 
 const MyNavScreen = ({ navigation, banner }) => (
-  <ScrollView style={styles.container}>
-    <SampleText>{banner}</SampleText>
-    <Button
-      onPress={() => navigation.navigate('DrawerOpen')}
-      title="Open drawer"
-    />
-    <Button onPress={() => navigation.goBack(null)} title="Go back" />
+  <ScrollView>
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <SampleText>{banner}</SampleText>
+      <Button
+        onPress={() => navigation.navigate('DrawerOpen')}
+        title="Open drawer"
+      />
+      <Button onPress={() => navigation.goBack(null)} title="Go back" />
+    </SafeAreaView>
   </ScrollView>
 );
 
@@ -61,11 +63,5 @@ const DrawerExample = DrawerNavigator(
     },
   }
 );
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: Platform.OS === 'ios' ? 20 : 0,
-  },
-});
 
 export default DrawerExample;

examples/NavigationPlayground/js/ModalStack.js
@@ -4,30 +4,38 @@
 
 import React from 'react';
 import { Button, ScrollView, Text } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { SafeAreaView, StackNavigator } from 'react-navigation';
 import SampleText from './SampleText';
 
 const MyNavScreen = ({ navigation, banner }) => (
-  <ScrollView>
-    <SampleText>{banner}</SampleText>
-    <Button
-      onPress={() => navigation.navigate('Profile', { name: 'Jane' })}
-      title="Go to a profile screen"
-    />
-    <Button
-      onPress={() => navigation.navigate('HeaderTest')}
-      title="Go to a header toggle screen"
-    />
-    {navigation.state.routeName === 'HeaderTest' &&
+  <ScrollView contentInsetAdjustmentBehavior="automatic">
+    <SafeAreaView
+      forceInset={{
+        top: navigation.state.routeName === 'HeaderTest' ? 'always' : 'never',
+      }}
+    >
+      <SampleText>{banner}</SampleText>
       <Button
-        title="Toggle Header"
-        onPress={() =>
-          navigation.setParams({
-            headerVisible: !navigation.state.params ||
-              !navigation.state.params.headerVisible,
-          })}
-      />}
-    <Button onPress={() => navigation.goBack(null)} title="Go back" />
+        onPress={() => navigation.navigate('Profile', { name: 'Jane' })}
+        title="Go to a profile screen"
+      />
+      <Button
+        onPress={() => navigation.navigate('HeaderTest')}
+        title="Go to a header toggle screen"
+      />
+      {navigation.state.routeName === 'HeaderTest' && (
+        <Button
+          title="Toggle Header"
+          onPress={() =>
+            navigation.setParams({
+              headerVisible:
+                !navigation.state.params ||
+                !navigation.state.params.headerVisible,
+            })}
+        />
+      )}
+      <Button onPress={() => navigation.goBack(null)} title="Go back" />
+    </SafeAreaView>
   </ScrollView>
 );
 

examples/NavigationPlayground/js/SimpleStack.js
@@ -4,11 +4,11 @@
 
 import React from 'react';
 import { Button, ScrollView } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, SafeAreaView } from 'react-navigation';
 import SampleText from './SampleText';
 
 const MyNavScreen = ({ navigation, banner }) => (
-  <ScrollView>
+  <SafeAreaView>
     <SampleText>{banner}</SampleText>
     <Button
       onPress={() => navigation.navigate('Profile', { name: 'Jane' })}
@@ -19,7 +19,7 @@ const MyNavScreen = ({ navigation, banner }) => (
       title="Go to a photos screen"
     />
     <Button onPress={() => navigation.goBack(null)} title="Go back" />
-  </ScrollView>
+  </SafeAreaView>
 );
 
 const MyHomeScreen = ({ navigation }) => (
@@ -41,7 +41,9 @@ MyPhotosScreen.navigationOptions = {
 
 const MyProfileScreen = ({ navigation }) => (
   <MyNavScreen
-    banner={`${navigation.state.params.mode === 'edit' ? 'Now Editing ' : ''}${navigation.state.params.name}'s Profile`}
+    banner={`${navigation.state.params.mode === 'edit'
+      ? 'Now Editing '
+      : ''}${navigation.state.params.name}'s Profile`}
     navigation={navigation}
   />
 );

commit 8552f3351eb797913b6e959886d1f782c8ada131

fix for breakage in 185289ad6658e07783c807ee7ac29b9e1244fa21.
that commit broke the possibility to have any kind of different separator on the bottom of the header.
this commit restores that.

like before, you can have in `headerStyle` something like `borderBottomWidth: 3, borderBottomColor: 'red'` for a custom separator, or `borderBottomWidth: 0` for no separator.

src/views/Header/Header.js
@@ -308,6 +308,7 @@ class Header extends React.PureComponent<Props, State> {
     } = options;
     const appBarHeight = Platform.OS === 'ios' ? (isLandscape ? 32 : 44) : 56;
     const containerStyles = [
+      styles.container,
       {
         height: appBarHeight,
       },
@@ -316,7 +317,7 @@ class Header extends React.PureComponent<Props, State> {
 
     return (
       <SafeAreaView
-        style={[styles.container, { backgroundColor: headerBackgroundColor }]}
+        style={{ backgroundColor: headerBackgroundColor }}
         forceInset={{ top: 'always', bottom: 'never' }}
       >
         <Animated.View {...rest} style={containerStyles}>