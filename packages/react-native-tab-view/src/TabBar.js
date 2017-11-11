2b3d19d07ff9a1d8833099b01a67e7112efe8483
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-11-11 09:17:54 +0530

    fix: change keyboardShouldPersistTaps to handled on tab bar

--- packages/react-native-tab-view/src/TabBar.js
+++ packages/react-native-tab-view/src/TabBar.js
@@ -367,7 +367,7 @@ export default class TabBar<T: Route<*>> extends React.PureComponent<
         <View style={styles.scroll}>
           <ScrollView
             horizontal
-            keyboardShouldPersistTaps="always"
+            keyboardShouldPersistTaps="handled"
             scrollEnabled={scrollEnabled}
             bounces={false}
             alwaysBounceHorizontal={false}
