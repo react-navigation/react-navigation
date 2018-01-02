c2573ada891bbbc3aaf322038ce642c353558b5b
Antonín Simerský <antonin.simersky@live.com>
2018-01-02 10:15:00 -0500

    Overwrite navigation props only if they are not received as a prop

--- src/views/CardStack/CardStack.js
+++ src/views/CardStack/CardStack.js
@@ -64,7 +64,7 @@ type Props = {
 
 /**
  * The max duration of the card animation in milliseconds after released gesture.
- * The actual duration should be always less then that because the rest distance 
+ * The actual duration should be always less then that because the rest distance
  * is always less then the full distance of the layout.
  */
 const ANIMATION_DURATION = 500;
@@ -402,7 +402,8 @@ class CardStack extends React.Component<Props> {
     SceneComponent: NavigationComponent,
     scene: NavigationScene
   ): React.Node {
-    const { navigation } = this._getScreenDetails(scene);
+    const navigation =
+      this.props.navigation || this._getScreenDetails(scene).navigation;
     const { screenProps } = this.props;
     const headerMode = this._getHeaderMode();
     if (headerMode === 'screen') {
