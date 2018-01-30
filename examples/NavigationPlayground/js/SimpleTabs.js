react-navigation/react-navigation

commit dca37627a243b191fda5856aa6ce9eb1ee4957cf

Add events support to redux example (#3368)
* Fixing redux

* Fix tab events and test event nested event subscriptions

* Add event support to redux example

examples/NavigationPlayground/js/SimpleTabs.js
@@ -45,35 +45,69 @@ MyHomeScreen.navigationOptions = {
   ),
 };
 
-const MyPeopleScreen = ({ navigation }) => (
-  <MyNavScreen banner="People Tab" navigation={navigation} />
-);
+class MyPeopleScreen extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'People',
+    tabBarIcon: ({ tintColor, focused }) => (
+      <Ionicons
+        name={focused ? 'ios-people' : 'ios-people-outline'}
+        size={26}
+        style={{ color: tintColor }}
+      />
+    ),
+  };
+  componentDidMount() {
+    this._s0 = this.props.navigation.addListener('willFocus', this._onEvent);
+    this._s1 = this.props.navigation.addListener('didFocus', this._onEvent);
+    this._s2 = this.props.navigation.addListener('willBlur', this._onEvent);
+    this._s3 = this.props.navigation.addListener('didBlur', this._onEvent);
+  }
+  componentWillUnmount() {
+    this._s0.remove();
+    this._s1.remove();
+    this._s2.remove();
+    this._s3.remove();
+  }
+  _onEvent = a => {
+    console.log('EVENT ON PEOPLE TAB', a.type, a);
+  };
+  render() {
+    const { navigation } = this.props;
+    return <MyNavScreen banner="People Tab" navigation={navigation} />;
+  }
+}
 
-MyPeopleScreen.navigationOptions = {
-  tabBarLabel: 'People',
-  tabBarIcon: ({ tintColor, focused }) => (
-    <Ionicons
-      name={focused ? 'ios-people' : 'ios-people-outline'}
-      size={26}
-      style={{ color: tintColor }}
-    />
-  ),
-};
-
-const MyChatScreen = ({ navigation }) => (
-  <MyNavScreen banner="Chat Tab" navigation={navigation} />
-);
-
-MyChatScreen.navigationOptions = {
-  tabBarLabel: 'Chat',
-  tabBarIcon: ({ tintColor, focused }) => (
-    <Ionicons
-      name={focused ? 'ios-chatboxes' : 'ios-chatboxes-outline'}
-      size={26}
-      style={{ color: tintColor }}
-    />
-  ),
-};
+class MyChatScreen extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'Chat',
+    tabBarIcon: ({ tintColor, focused }) => (
+      <Ionicons
+        name={focused ? 'ios-chatboxes' : 'ios-chatboxes-outline'}
+        size={26}
+        style={{ color: tintColor }}
+      />
+    ),
+  };
+  componentDidMount() {
+    this._s0 = this.props.navigation.addListener('willFocus', this._onEvent);
+    this._s1 = this.props.navigation.addListener('didFocus', this._onEvent);
+    this._s2 = this.props.navigation.addListener('willBlur', this._onEvent);
+    this._s3 = this.props.navigation.addListener('didBlur', this._onEvent);
+  }
+  componentWillUnmount() {
+    this._s0.remove();
+    this._s1.remove();
+    this._s2.remove();
+    this._s3.remove();
+  }
+  _onEvent = a => {
+    console.log('EVENT ON CHAT TAB', a.type, a);
+  };
+  render() {
+    const { navigation } = this.props;
+    return <MyNavScreen banner="Chat Tab" navigation={navigation} />;
+  }
+}
 
 const MySettingsScreen = ({ navigation }) => (
   <MyNavScreen banner="Settings Tab" navigation={navigation} />
@@ -128,10 +162,10 @@ class SimpleTabsContainer extends React.Component<SimpleTabsContainerProps> {
   _s3: EventListener;
 
   componentDidMount() {
-    this._s0 = this.props.navigation.addListener('willFocus', this._onWF);
-    this._s1 = this.props.navigation.addListener('didFocus', this._onDF);
-    this._s2 = this.props.navigation.addListener('willBlur', this._onWB);
-    this._s3 = this.props.navigation.addListener('didBlur', this._onDB);
+    this._s0 = this.props.navigation.addListener('willFocus', this._onAction);
+    this._s1 = this.props.navigation.addListener('didFocus', this._onAction);
+    this._s2 = this.props.navigation.addListener('willBlur', this._onAction);
+    this._s3 = this.props.navigation.addListener('didBlur', this._onAction);
   }
   componentWillUnmount() {
     this._s0.remove();
@@ -139,17 +173,8 @@ class SimpleTabsContainer extends React.Component<SimpleTabsContainerProps> {
     this._s2.remove();
     this._s3.remove();
   }
-  _onWF = a => {
-    console.log('_onWillFocus tabsExample ', a);
-  };
-  _onDF = a => {
-    console.log('_onDidFocus tabsExample ', a);
-  };
-  _onWB = a => {
-    console.log('_onWillBlur tabsExample ', a);
-  };
-  _onDB = a => {
-    console.log('_onDidBlur tabsExample ', a);
+  _onAction = a => {
+    console.log('TABS EVENT', a.type, a);
   };
   render() {
     return <SimpleTabs navigation={this.props.navigation} />;

examples/ReduxExample/src/navigators/AppNavigator.js
@@ -13,17 +13,51 @@ export const AppNavigator = StackNavigator({
   Profile: { screen: ProfileScreen },
 });
 
-const AppWithNavigationState = ({ dispatch, nav }) => (
-  <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
-);
+class AppWithNavigationState extends React.Component {
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    nav: PropTypes.object.isRequired,
+  };
 
-AppWithNavigationState.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  nav: PropTypes.object.isRequired,
-};
+  _actionEventSubscribers = new Set();
+
+  _addListener = (eventName, handler) => {
+    eventName === 'action' && this._actionEventSubscribers.add(handler);
+    return {
+      remove: () => {
+        this._actionEventSubscribers.delete(handler);
+      },
+    };
+  };
+
+  componentDidUpdate(lastProps) {
+    const lastState = lastProps.nav;
+    this._actionEventSubscribers.forEach(subscriber => {
+      subscriber({
+        lastState: lastProps.nav,
+        state: this.props.nav,
+        action: this.props.lastAction,
+      });
+    });
+  }
+
+  render() {
+    const { dispatch, nav } = this.props;
+    return (
+      <AppNavigator
+        navigation={addNavigationHelpers({
+          dispatch,
+          state: nav,
+          addListener: this._addListener,
+        })}
+      />
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   nav: state.nav,
+  lastAction: state.lastAction,
 });
 
 export default connect(mapStateToProps)(AppWithNavigationState);

examples/ReduxExample/src/reducers/index.js
@@ -36,6 +36,10 @@ function nav(state = initialNavState, action) {
   return nextState || state;
 }
 
+function lastAction(state = null, action) {
+  return action;
+}
+
 const initialAuthState = { isLoggedIn: false };
 
 function auth(state = initialAuthState, action) {
@@ -50,6 +54,7 @@ function auth(state = initialAuthState, action) {
 }
 
 const AppReducer = combineReducers({
+  lastAction,
   nav,
   auth,
 });

src/__tests__/getChildEventSubscriber-test.js
@@ -1,0 +1,437 @@
+import getChildEventSubscriber from '../getChildEventSubscriber';
+
+test('child action events only flow when focused', () => {
+  const parentSubscriber = jest.fn();
+  const emitParentAction = payload => {
+    parentSubscriber.mock.calls.forEach(subs => {
+      if (subs[0] === payload.type) {
+        subs[1](payload);
+      }
+    });
+  };
+  const subscriptionRemove = () => {};
+  parentSubscriber.mockReturnValueOnce({ remove: subscriptionRemove });
+  const childEventSubscriber = getChildEventSubscriber(
+    parentSubscriber,
+    'key1'
+  );
+  const testState = {
+    key: 'foo',
+    routeName: 'FooRoute',
+    routes: [{ key: 'key0' }, { key: 'key1' }],
+    index: 0,
+    isTransitioning: false,
+  };
+  const focusedTestState = {
+    ...testState,
+    index: 1,
+  };
+  const childActionHandler = jest.fn();
+  const childWillFocusHandler = jest.fn();
+  const childDidFocusHandler = jest.fn();
+  childEventSubscriber('action', childActionHandler);
+  childEventSubscriber('willFocus', childWillFocusHandler);
+  childEventSubscriber('didFocus', childDidFocusHandler);
+  emitParentAction({
+    type: 'action',
+    state: focusedTestState,
+    lastState: testState,
+    action: { type: 'FooAction' },
+  });
+  expect(childActionHandler.mock.calls.length).toBe(0);
+  expect(childWillFocusHandler.mock.calls.length).toBe(1);
+  expect(childDidFocusHandler.mock.calls.length).toBe(1);
+  emitParentAction({
+    type: 'action',
+    state: focusedTestState,
+    lastState: focusedTestState,
+    action: { type: 'FooAction' },
+  });
+  expect(childActionHandler.mock.calls.length).toBe(1);
+  expect(childWillFocusHandler.mock.calls.length).toBe(1);
+  expect(childDidFocusHandler.mock.calls.length).toBe(1);
+});
+
+test('grandchildren subscription', () => {
+  const grandParentSubscriber = jest.fn();
+  const emitGrandParentAction = payload => {
+    grandParentSubscriber.mock.calls.forEach(subs => {
+      if (subs[0] === payload.type) {
+        subs[1](payload);
+      }
+    });
+  };
+  const subscriptionRemove = () => {};
+  grandParentSubscriber.mockReturnValueOnce({ remove: subscriptionRemove });
+  const parentSubscriber = getChildEventSubscriber(
+    grandParentSubscriber,
+    'parent'
+  );
+  const childEventSubscriber = getChildEventSubscriber(
+    parentSubscriber,
+    'key1'
+  );
+  const parentBlurState = {
+    key: 'foo',
+    routeName: 'FooRoute',
+    routes: [
+      { key: 'aunt' },
+      {
+        key: 'parent',
+        routes: [{ key: 'key0' }, { key: 'key1' }],
+        index: 1,
+        isTransitioning: false,
+      },
+    ],
+    index: 0,
+    isTransitioning: false,
+  };
+  const parentTransitionState = {
+    ...parentBlurState,
+    index: 1,
+    isTransitioning: true,
+  };
+  const parentFocusState = {
+    ...parentTransitionState,
+    isTransitioning: false,
+  };
+  const childActionHandler = jest.fn();
+  const childWillFocusHandler = jest.fn();
+  const childDidFocusHandler = jest.fn();
+  childEventSubscriber('action', childActionHandler);
+  childEventSubscriber('willFocus', childWillFocusHandler);
+  childEventSubscriber('didFocus', childDidFocusHandler);
+  emitGrandParentAction({
+    type: 'action',
+    state: parentTransitionState,
+    lastState: parentBlurState,
+    action: { type: 'FooAction' },
+  });
+  expect(childActionHandler.mock.calls.length).toBe(0);
+  expect(childWillFocusHandler.mock.calls.length).toBe(1);
+  expect(childDidFocusHandler.mock.calls.length).toBe(0);
+  emitGrandParentAction({
+    type: 'action',
+    state: parentFocusState,
+    lastState: parentTransitionState,
+    action: { type: 'FooAction' },
+  });
+  expect(childActionHandler.mock.calls.length).toBe(0);
+  expect(childWillFocusHandler.mock.calls.length).toBe(1);
+  expect(childDidFocusHandler.mock.calls.length).toBe(1);
+});
+
+test('grandchildren transitions', () => {
+  const grandParentSubscriber = jest.fn();
+  const emitGrandParentAction = payload => {
+    grandParentSubscriber.mock.calls.forEach(subs => {
+      if (subs[0] === payload.type) {
+        subs[1](payload);
+      }
+    });
+  };
+  const subscriptionRemove = () => {};
+  grandParentSubscriber.mockReturnValueOnce({ remove: subscriptionRemove });
+  const parentSubscriber = getChildEventSubscriber(
+    grandParentSubscriber,
+    'parent'
+  );
+  const childEventSubscriber = getChildEventSubscriber(
+    parentSubscriber,
+    'key1'
+  );
+  const makeFakeState = (childIndex, childIsTransitioning) => ({
+    index: 1,
+    isTransitioning: false,
+    routes: [
+      { key: 'nothing' },
+      {
+        key: 'parent',
+        index: childIndex,
+        isTransitioning: childIsTransitioning,
+        routes: [{ key: 'key0' }, { key: 'key1' }, { key: 'key2' }],
+      },
+    ],
+  });
+  const blurredState = makeFakeState(0, false);
+  const transitionState = makeFakeState(1, true);
+  const focusState = makeFakeState(1, false);
+  const transition2State = makeFakeState(2, true);
+  const blurred2State = makeFakeState(2, false);
+
+  const childActionHandler = jest.fn();
+  const childWillFocusHandler = jest.fn();
+  const childDidFocusHandler = jest.fn();
+  const childWillBlurHandler = jest.fn();
+  const childDidBlurHandler = jest.fn();
+  childEventSubscriber('action', childActionHandler);
+  childEventSubscriber('willFocus', childWillFocusHandler);
+  childEventSubscriber('didFocus', childDidFocusHandler);
+  childEventSubscriber('willBlur', childWillBlurHandler);
+  childEventSubscriber('didBlur', childDidBlurHandler);
+  emitGrandParentAction({
+    type: 'action',
+    state: transitionState,
+    lastState: blurredState,
+    action: { type: 'FooAction' },
+  });
+  expect(childActionHandler.mock.calls.length).toBe(0);
+  expect(childWillFocusHandler.mock.calls.length).toBe(1);
+  expect(childDidFocusHandler.mock.calls.length).toBe(0);
+  emitGrandParentAction({
+    type: 'action',
+    state: focusState,
+    lastState: transitionState,
+    action: { type: 'FooAction' },
+  });
+  expect(childActionHandler.mock.calls.length).toBe(0);
+  expect(childWillFocusHandler.mock.calls.length).toBe(1);
+  expect(childDidFocusHandler.mock.calls.length).toBe(1);
+  emitGrandParentAction({
+    type: 'action',
+    state: focusState,
+    lastState: focusState,
+    action: { type: 'TestAction' },
+  });
+  expect(childWillFocusHandler.mock.calls.length).toBe(1);
+  expect(childDidFocusHandler.mock.calls.length).toBe(1);
+  expect(childActionHandler.mock.calls.length).toBe(1);
+  emitGrandParentAction({
+    type: 'action',
+    state: transition2State,
+    lastState: focusState,
+    action: { type: 'CauseWillBlurAction' },
+  });
+  expect(childWillBlurHandler.mock.calls.length).toBe(1);
+  expect(childDidBlurHandler.mock.calls.length).toBe(0);
+  expect(childActionHandler.mock.calls.length).toBe(2);
+  emitGrandParentAction({
+    type: 'action',
+    state: blurred2State,
+    lastState: transition2State,
+    action: { type: 'CauseDidBlurAction' },
+  });
+  expect(childWillBlurHandler.mock.calls.length).toBe(1);
+  expect(childDidBlurHandler.mock.calls.length).toBe(1);
+  expect(childActionHandler.mock.calls.length).toBe(3);
+});
+
+test('pass through focus', () => {
+  const parentSubscriber = jest.fn();
+  const emitParentAction = payload => {
+    parentSubscriber.mock.calls.forEach(subs => {
+      if (subs[0] === payload.type) {
+        subs[1](payload);
+      }
+    });
+  };
+  const subscriptionRemove = () => {};
+  parentSubscriber.mockReturnValueOnce({ remove: subscriptionRemove });
+  const childEventSubscriber = getChildEventSubscriber(
+    parentSubscriber,
+    'testKey'
+  );
+  const testRoute = {
+    key: 'foo',
+    routeName: 'FooRoute',
+    routes: [{ key: 'key0' }, { key: 'testKey' }],
+    index: 1,
+    isTransitioning: false,
+  };
+  const childWillFocusHandler = jest.fn();
+  const childDidFocusHandler = jest.fn();
+  const childWillBlurHandler = jest.fn();
+  const childDidBlurHandler = jest.fn();
+  childEventSubscriber('willFocus', childWillFocusHandler);
+  childEventSubscriber('didFocus', childDidFocusHandler);
+  childEventSubscriber('willBlur', childWillBlurHandler);
+  childEventSubscriber('didBlur', childDidBlurHandler);
+  emitParentAction({
+    type: 'willFocus',
+    state: testRoute,
+    lastState: testRoute,
+    action: { type: 'FooAction' },
+  });
+  expect(childWillFocusHandler.mock.calls.length).toBe(1);
+  emitParentAction({
+    type: 'didFocus',
+    state: testRoute,
+    lastState: testRoute,
+    action: { type: 'FooAction' },
+  });
+  expect(childDidFocusHandler.mock.calls.length).toBe(1);
+  emitParentAction({
+    type: 'willBlur',
+    state: testRoute,
+    lastState: testRoute,
+    action: { type: 'FooAction' },
+  });
+  expect(childWillBlurHandler.mock.calls.length).toBe(1);
+  emitParentAction({
+    type: 'didBlur',
+    state: testRoute,
+    lastState: testRoute,
+    action: { type: 'FooAction' },
+  });
+  expect(childDidBlurHandler.mock.calls.length).toBe(1);
+});
+
+test('child focus with transition', () => {
+  const parentSubscriber = jest.fn();
+  const emitParentAction = payload => {
+    parentSubscriber.mock.calls.forEach(subs => {
+      if (subs[0] === payload.type) {
+        subs[1](payload);
+      }
+    });
+  };
+  const subscriptionRemove = () => {};
+  parentSubscriber.mockReturnValueOnce({ remove: subscriptionRemove });
+  const childEventSubscriber = getChildEventSubscriber(
+    parentSubscriber,
+    'key1'
+  );
+  const randomAction = { type: 'FooAction' };
+  const testState = {
+    key: 'foo',
+    routeName: 'FooRoute',
+    routes: [{ key: 'key0' }, { key: 'key1' }],
+    index: 0,
+    isTransitioning: false,
+  };
+  const childWillFocusHandler = jest.fn();
+  const childDidFocusHandler = jest.fn();
+  const childWillBlurHandler = jest.fn();
+  const childDidBlurHandler = jest.fn();
+  childEventSubscriber('willFocus', childWillFocusHandler);
+  childEventSubscriber('didFocus', childDidFocusHandler);
+  childEventSubscriber('willBlur', childWillBlurHandler);
+  childEventSubscriber('didBlur', childDidBlurHandler);
+  emitParentAction({
+    type: 'didFocus',
+    action: randomAction,
+    lastState: testState,
+    state: testState,
+  });
+  emitParentAction({
+    type: 'action',
+    action: randomAction,
+    lastState: testState,
+    state: {
+      ...testState,
+      index: 1,
+      isTransitioning: true,
+    },
+  });
+  expect(childWillFocusHandler.mock.calls.length).toBe(1);
+  emitParentAction({
+    type: 'action',
+    action: randomAction,
+    lastState: {
+      ...testState,
+      index: 1,
+      isTransitioning: true,
+    },
+    state: {
+      ...testState,
+      index: 1,
+      isTransitioning: false,
+    },
+  });
+  expect(childDidFocusHandler.mock.calls.length).toBe(1);
+  emitParentAction({
+    type: 'action',
+    action: randomAction,
+    lastState: {
+      ...testState,
+      index: 1,
+      isTransitioning: false,
+    },
+    state: {
+      ...testState,
+      index: 0,
+      isTransitioning: true,
+    },
+  });
+  expect(childWillBlurHandler.mock.calls.length).toBe(1);
+  emitParentAction({
+    type: 'action',
+    action: randomAction,
+    lastState: {
+      ...testState,
+      index: 0,
+      isTransitioning: true,
+    },
+    state: {
+      ...testState,
+      index: 0,
+      isTransitioning: false,
+    },
+  });
+  expect(childDidBlurHandler.mock.calls.length).toBe(1);
+});
+
+test('child focus with immediate transition', () => {
+  const parentSubscriber = jest.fn();
+  const emitParentAction = payload => {
+    parentSubscriber.mock.calls.forEach(subs => {
+      if (subs[0] === payload.type) {
+        subs[1](payload);
+      }
+    });
+  };
+  const subscriptionRemove = () => {};
+  parentSubscriber.mockReturnValueOnce({ remove: subscriptionRemove });
+  const childEventSubscriber = getChildEventSubscriber(
+    parentSubscriber,
+    'key1'
+  );
+  const randomAction = { type: 'FooAction' };
+  const testState = {
+    key: 'foo',
+    routeName: 'FooRoute',
+    routes: [{ key: 'key0' }, { key: 'key1' }],
+    index: 0,
+    isTransitioning: false,
+  };
+  const childWillFocusHandler = jest.fn();
+  const childDidFocusHandler = jest.fn();
+  const childWillBlurHandler = jest.fn();
+  const childDidBlurHandler = jest.fn();
+  childEventSubscriber('willFocus', childWillFocusHandler);
+  childEventSubscriber('didFocus', childDidFocusHandler);
+  childEventSubscriber('willBlur', childWillBlurHandler);
+  childEventSubscriber('didBlur', childDidBlurHandler);
+  emitParentAction({
+    type: 'didFocus',
+    action: randomAction,
+    lastState: testState,
+    state: testState,
+  });
+  emitParentAction({
+    type: 'action',
+    action: randomAction,
+    lastState: testState,
+    state: {
+      ...testState,
+      index: 1,
+    },
+  });
+  expect(childWillFocusHandler.mock.calls.length).toBe(1);
+  expect(childDidFocusHandler.mock.calls.length).toBe(1);
+
+  emitParentAction({
+    type: 'action',
+    action: randomAction,
+    lastState: {
+      ...testState,
+      index: 1,
+    },
+    state: {
+      ...testState,
+      index: 0,
+    },
+  });
+  expect(childWillBlurHandler.mock.calls.length).toBe(1);
+  expect(childDidBlurHandler.mock.calls.length).toBe(1);
+});

src/getChildEventSubscriber.js
@@ -32,13 +32,12 @@ export default function getChildEventSubscriber(addListener, key) {
     const subscribers = getChildSubscribers(payload.type);
     subscribers &&
       subscribers.forEach(subs => {
-        // $FlowFixMe - Payload should probably understand generic state type
         subs(payload);
       });
   };
 
-  let isSelfFocused = false;
-
+  let isParentFocused = true;
+  let isChildFocused = false;
   const cleanup = () => {
     upstreamSubscribers.forEach(subs => subs && subs.remove());
   };
@@ -54,74 +53,112 @@ export default function getChildEventSubscriber(addListener, key) {
   const upstreamSubscribers = upstreamEvents.map(eventName =>
     addListener(eventName, payload => {
       const { state, lastState, action } = payload;
-      const lastFocusKey = lastState && lastState.routes[lastState.index].key;
-      const focusKey = state && state.routes[state.index].key;
+      const lastRoutes = lastState && lastState.routes;
+      const routes = state && state.routes;
+      const lastFocusKey =
+        lastState && lastState.routes && lastState.routes[lastState.index].key;
+      const focusKey = routes && routes[state.index].key;
 
       const isFocused = focusKey === key;
       const wasFocused = lastFocusKey === key;
       const lastRoute =
-        lastState && lastState.routes.find(route => route.key === key);
-      const newRoute = state && state.routes.find(route => route.key === key);
+        lastRoutes && lastRoutes.find(route => route.key === key);
+      const newRoute = routes && routes.find(route => route.key === key);
+      const eventContext = payload.context || 'Root';
       const childPayload = {
+        context: `${key}:${action.type}_${eventContext}`,
         state: newRoute,
         lastState: lastRoute,
         action,
         type: eventName,
       };
 
-      const didNavigate =
-        (lastState && lastState.isTransitioning) !==
-        (state && state.isTransitioning);
-
       const isTransitioning = !!state && state.isTransitioning;
       const wasTransitioning = !!lastState && lastState.isTransitioning;
       const didStartTransitioning = !wasTransitioning && isTransitioning;
       const didFinishTransitioning = wasTransitioning && !isTransitioning;
-
+      const wasChildFocused = isChildFocused;
       if (eventName !== 'action') {
         switch (eventName) {
           case 'didFocus':
-            isSelfFocused = true;
+            isParentFocused = true;
             break;
           case 'didBlur':
-            isSelfFocused = false;
+            isParentFocused = false;
             break;
         }
-        emit(childPayload);
+        if (isFocused && eventName === 'willFocus') {
+          emit(childPayload);
+        }
+        if (isFocused && !isTransitioning && eventName === 'didFocus') {
+          emit(childPayload);
+          isChildFocused = true;
+        }
+        if (isFocused && eventName === 'willBlur') {
+          emit(childPayload);
+        }
+        if (isFocused && !isTransitioning && eventName === 'didBlur') {
+          emit(childPayload);
+        }
         return;
       }
 
       // now we're exclusively handling the "action" event
+      if (!isParentFocused) {
+        return;
+      }
 
-      if (newRoute) {
-        // fire this event to pass navigation events to children subscribers
+      if (isChildFocused && newRoute) {
+        // fire this action event to pass navigation events to children subscribers
         emit(childPayload);
       }
-      if (isFocused && didStartTransitioning && !isSelfFocused) {
+      if (isFocused && didStartTransitioning && !isChildFocused) {
         emit({
           ...childPayload,
           type: 'willFocus',
         });
       }
-      if (isFocused && didFinishTransitioning && !isSelfFocused) {
+      if (isFocused && didFinishTransitioning && !isChildFocused) {
         emit({
           ...childPayload,
           type: 'didFocus',
         });
-        isSelfFocused = true;
+        isChildFocused = true;
       }
-      if (!isFocused && didStartTransitioning && isSelfFocused) {
+      if (isFocused && !isChildFocused && !didStartTransitioning) {
+        emit({
+          ...childPayload,
+          type: 'willFocus',
+        });
+        emit({
+          ...childPayload,
+          type: 'didFocus',
+        });
+        isChildFocused = true;
+      }
+      if (!isFocused && didStartTransitioning && isChildFocused) {
         emit({
           ...childPayload,
           type: 'willBlur',
         });
       }
-      if (!isFocused && didFinishTransitioning && isSelfFocused) {
+      if (!isFocused && didFinishTransitioning && isChildFocused) {
         emit({
           ...childPayload,
           type: 'didBlur',
         });
-        isSelfFocused = false;
+        isChildFocused = false;
+      }
+      if (!isFocused && isChildFocused && !didStartTransitioning) {
+        emit({
+          ...childPayload,
+          type: 'willBlur',
+        });
+        emit({
+          ...childPayload,
+          type: 'didBlur',
+        });
+        isChildFocused = false;
       }
     })
   );

commit 98193ea59c3622a610d914066b7d7da21fa39bc0

Fix SimpleTabs types
dca37627a243b191fda5856aa6ce9eb1ee4957cf broke the types, but it wasn't noticed because Flow wasn't in the CI. This fixes the types; separate PR coming to add Flow to the CI.

examples/NavigationPlayground/js/SimpleTabs.js
@@ -45,7 +45,15 @@ MyHomeScreen.navigationOptions = {
   ),
 };
 
-class MyPeopleScreen extends React.Component {
+type MyPeopleScreenProps = {
+  navigation: NavigationScreenProp<*>,
+};
+class MyPeopleScreen extends React.Component<MyPeopleScreenProps> {
+  _s0: EventListener;
+  _s1: EventListener;
+  _s2: EventListener;
+  _s3: EventListener;
+
   static navigationOptions = {
     tabBarLabel: 'People',
     tabBarIcon: ({ tintColor, focused }) => (
@@ -77,7 +85,15 @@ class MyPeopleScreen extends React.Component {
   }
 }
 
-class MyChatScreen extends React.Component {
+type MyChatScreenProps = {
+  navigation: NavigationScreenProp<*>,
+};
+class MyChatScreen extends React.Component<MyChatScreenProps> {
+  _s0: EventListener;
+  _s1: EventListener;
+  _s2: EventListener;
+  _s3: EventListener;
+
   static navigationOptions = {
     tabBarLabel: 'Chat',
     tabBarIcon: ({ tintColor, focused }) => (