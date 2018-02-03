77a2b41648fd6dc5ce32e718e62180848dc5d449
leon3s <lvernisse@student.42.fr>
2018-02-03 14:21:47 +0100

    [ UPDATE ]

--- dist/PlatformHelpers.web.js
+++ dist/PlatformHelpers.web.js
@@ -1,11 +1,1 @@
-Object.defineProperty(exports, '__esModule', { value: true });
-var Linking = (exports.Linking = {
-  addEventListener: function addEventListener() {},
-  removeEventListener: function removeEventListener() {},
-  getInitialURL: function getInitialURL() {
-    return Promise.reject('Unsupported platform');
-  },
-});
-var BackHandler = (exports.BackHandler = {
-  addEventListener: function addEventListener() {},
-});
+Object.defineProperty(exports,"__esModule",{value:true});var Linking=exports.Linking={addEventListener:function addEventListener(){},removeEventListener:function removeEventListener(){},getInitialURL:function getInitialURL(){return Promise.reject('Unsupported platform');}};var BackHandler=exports.BackHandler={addEventListener:function addEventListener(){}};
