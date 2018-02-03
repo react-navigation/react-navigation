760774453cfb0c5e57aa98b235d2c95c7d01512c
Brent Vatne <brentvatne@gmail.com>
2018-02-03 10:38:43 -0800

    Remove website, docs and related commit. (#3419)
    - This now lives at https://github.com/react-navigation/react-navigation.github.io

--- packages/react-navigation/website/src/DocSearchBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const DocSearchBar = () => (
-  <div className="search-wrapper">
-    <input id="doc-search" type="text" placeholder="Search..." />
-  </div>
-);
-
-export default DocSearchBar;
