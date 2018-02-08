ac741a703b32047dfa85cb33f8d17a1230e7f89a
Brent Vatne <brentvatne@gmail.com>
2018-02-08 12:32:09 -0800

    Remove extra scene in floating header if it hasn't been evicted due to transition yet

--- src/views/Header/Header.js
+++ src/views/Header/Header.js
@@ -262,7 +262,11 @@ class Header extends React.PureComponent {
     let appBar;
 
     if (this.props.mode === 'float') {
-      const scenesProps = this.props.scenes.map(scene => ({
+      const scenesByIndex = {};
+      this.props.scenes.forEach(scene => {
+        scenesByIndex[scene.index] = scene;
+      });
+      const scenesProps = Object.values(scenesByIndex).map(scene => ({
         position: this.props.position,
         progress: this.props.progress,
         scene,
