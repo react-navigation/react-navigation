02515a9c83ffbbae7cadd09927178c1cc63ae575
slorber <lorber.sebastien@gmail.com>
2018-02-13 15:13:29 +0100

    add withNavigationFocus HOC
    See:
    - https://github.com/react-navigation/react-navigation/issues/51#issuecomment-276003658
    - https://github.com/react-navigation/react-navigation/issues/51#issuecomment-278761705
    - https://github.com/react-navigation/react-navigation/pull/3345#issuecomment-360260067

---
+++ src/views/withIsFocused.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import hoistStatics from 'hoist-non-react-statics';
+
+export default function withNavigationFocus(Component) {
+  class ComponentWithNavigationFocus extends React.Component {
+    static displayName = `withNavigationFocus(${Component.displayName ||
+      Component.name})`;
+
+    state = {
+      isFocused: false,
+    };
+
+    componentDidMount() {
+      if (this.props.navigation) {
+        this.subs = [
+          this.props.navigation.addListener('didFocus', () =>
+            this.setState({ isFocused: true })
+          ),
+          this.props.navigation.addListener('willBlur', () =>
+            this.setState({ isFocused: false })
+          ),
+        ];
+      } else {
+        console.warn(
+          'withIsFocused wrapped component did not receive navigation from props, so isFocused prop swill remain false'
+        );
+      }
+    }
+
+    componentWillUnmount() {
+      this.subs && this.subs.forEach(sub => sub.remove());
+    }
+
+    render() {
+      return (
+        <Component
+          {...this.props}
+          isFocused={this.state.isFocused}
+          ref={this.props.onRef}
+        />
+      );
+    }
+  }
+
+  return hoistStatics(ComponentWithNavigationFocus, Component);
+}
