9a1fc41ef065baf5b6ee23c1cce790e516a42f83
slorber <lorber.sebastien@gmail.com>
2018-02-13 16:43:42 +0100

    add example TabsWithNavigationFocus

--- examples/NavigationPlayground/js/App.js
+++ examples/NavigationPlayground/js/App.js
@@ -30,6 +30,7 @@ import StacksWithKeys from './StacksWithKeys';
 import SimpleStack from './SimpleStack';
 import SimpleTabs from './SimpleTabs';
 import TabAnimations from './TabAnimations';
+import TabsWithNavigationFocus from './TabsWithNavigationFocus';
 
 const ExampleInfo = {
   SimpleStack: {
@@ -94,7 +95,12 @@ const ExampleInfo = {
     name: 'Animated Tabs Example',
     description: 'Tab transitions have custom animations',
   },
+  TabsWithNavigationFocus: {
+    name: 'Tabs using withNavigationFocus HOC',
+    description: 'Tab views will receive isFocused prop from HOC',
+  },
 };
+
 const ExampleRoutes = {
   SimpleStack: SimpleStack,
   SimpleTabs: SimpleTabs,
@@ -118,6 +124,7 @@ const ExampleRoutes = {
     path: 'settings',
   },
   TabAnimations: TabAnimations,
+  TabsWithNavigationFocus: TabsWithNavigationFocus,
 };
 
 type State = {
