1fd55905a12871e87fcfd85b62fa83556d52dc82
Brent Vatne <brentvatne@gmail.com>
2018-02-14 18:21:59 -0800

    Further tweak the interpolators for standard-ios preset to be a bit more pleasant

--- src/views/Header/HeaderStyleInterpolator.js
+++ src/views/Header/HeaderStyleInterpolator.js
@@ -95,6 +95,15 @@ function forLeftButton(props) {
   };
 }
 
+/* 
+ * NOTE: this offset calculation is a an approximation that gives us
+ * decent results in many cases, but it is ultimately a poor substitute
+ * for text measurement. See the comment on title for more information.
+ * 
+ * - 70 is the width of the left button area.
+ * - 25 is the width of the left button icon (to account for label offset)
+ */
+const LEFT_LABEL_OFFSET = Dimensions.get('window').width / 2 - 70 - 25;
 function forLeftLabel(props) {
   const { position, scene, scenes } = props;
   const interpolate = getSceneIndicesForInterpolationInputRange(props);
@@ -104,12 +113,15 @@ function forLeftLabel(props) {
   const { first, last } = interpolate;
   const index = scene.index;
 
-  const offset = 100;
+  const offset = LEFT_LABEL_OFFSET;
 
   return {
+    // For now we fade out the label before fading in the title, so the
+    // differences between the label and title position can be hopefully not so
+    // noticable to the user
     opacity: position.interpolate({
-      inputRange: [first, first + Math.abs(index - first) / 2, index, last],
-      outputRange: [0, 0, 1, 0],
+      inputRange: [first, index - 0.35, index, index + 0.5, last],
+      outputRange: [0, 0, 1, 0, 0],
     }),
     transform: [
       {
@@ -131,8 +143,11 @@ function forLeftLabel(props) {
  * smoothly into the title text and to do this we need to understand
  * where the title is positioned within the title container (since it is
  * centered).
+ * 
+ * - 70 is the width of the left button area.
+ * - 25 is the width of the left button icon (to account for label offset)
  */
-const TITLE_OFFSET_IOS = Dimensions.get('window').width - 50;
+const TITLE_OFFSET_IOS = Dimensions.get('window').width / 2 - 70 + 25;
 function forCenterFromLeft(props) {
   const { position, scene } = props;
   const interpolate = getSceneIndicesForInterpolationInputRange(props);
@@ -154,8 +169,8 @@ function forCenterFromLeft(props) {
         translateX: position.interpolate({
           inputRange: [first, index, last],
           outputRange: I18nManager.isRTL
-            ? [-offset / 2, 0, offset * 1.5]
-            : [offset / 2, 0, -offset / 2],
+            ? [-offset, 0, offset]
+            : [offset, 0, -offset],
         }),
       },
     ],
