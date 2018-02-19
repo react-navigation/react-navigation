ba0737b09587790671eada5a29b2376c90193c58
Ron Arts <ron.arts@novantum.com>
2018-02-19 12:53:46 +0100

    MaskedViewIOS use broke react-native-web support, this fixes it.

--- src/PlatformHelpers.web.js
+++ src/PlatformHelpers.web.js
@@ -1,3 +1,8 @@
-import { BackHandler } from 'react-native';
+import React from 'react';
+import { BackHandler, Text } from 'react-native';
 
-export { BackHandler };
+const MaskedViewIOS = () => {
+  <Text>MaskedViewIOS not available on Reacht Native Web</Text>;
+};
+
+export { BackHandler, MaskedViewIOS };
