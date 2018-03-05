afecaaed7f449571b96c2f9f45660eb07d519f8c
Leon Miller-Out <leon@singlebrook.com>
2018-03-05 15:02:34 -0500

    Call react-navigation-redux-helpers's initializeListeners() when mounting app. (#3666)
    This makes the first navigation click work correctly, per
    https://github.com/react-navigation/react-navigation-redux-helpers/issues/7

--- examples/ReduxExample/src/navigators/AppNavigator.js
+++ examples/ReduxExample/src/navigators/AppNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addNavigationHelpers, StackNavigator } from 'react-navigation';
+import { initializeListeners } from 'react-navigation-redux-helpers';
 
 import LoginScreen from '../components/LoginScreen';
 import MainScreen from '../components/MainScreen';
@@ -19,6 +20,10 @@ class AppWithNavigationState extends React.Component {
     dispatch: PropTypes.func.isRequired,
     nav: PropTypes.object.isRequired,
   };
+
+  componentDidMount() {
+    initializeListeners('root', this.props.nav);
+  }
 
   render() {
     const { dispatch, nav } = this.props;
