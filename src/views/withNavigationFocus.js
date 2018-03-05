4839edccc16413ba6f44125d2e93b166d8e0f737
Michał Pierzchała <thymikee@gmail.com>
2018-03-05 22:25:59 +0100

    adjust src/views/withNavigationFocus.js

--- src/views/withNavigationFocus.js
+++ src/views/withNavigationFocus.js
@@ -9,24 +9,26 @@ export default function withNavigationFocus(Component) {
     static displayName = `withNavigationFocus(${Component.displayName ||
       Component.name})`;
 
-    static contextTypes = {
-      navigation: propTypes.object.isRequired,
-    };
-
     constructor(props, context) {
       super();
 
       this.state = {
-        isFocused: this.getNavigation(props, context).isFocused(),
+        isFocused: props.navigation ? props.navigation.isFocused() : false,
       };
     }
 
     componentDidMount() {
+      const navigation = this.getNavigation();
+
+      this.setState({
+        isFocused: navigation.isFocused(),
+      });
+
       this.subscriptions = [
-        this.navigation.addListener('didFocus', () =>
+        navigation.addListener('didFocus', () =>
           this.setState({ isFocused: true })
         ),
-        this.navigation.addListener('willBlur', () =>
+        navigation.addListener('willBlur', () =>
           this.setState({ isFocused: false })
         ),
       ];
@@ -36,8 +38,8 @@ export default function withNavigationFocus(Component) {
       this.subscriptions.forEach(sub => sub.remove());
     }
 
-    getNavigation = (props = this.props, context = this.context) => {
-      const navigation = props.navigation || context.navigation;
+    getNavigation = (props = this.props) => {
+      const navigation = props.navigation || this.navigation;
       invariant(
         !!navigation,
         'withNavigationFocus can only be used on a view hierarchy of a navigator. The wrapped component is unable to get access to navigation from props or context.'
