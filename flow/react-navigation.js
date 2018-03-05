57e37a878352aabda60911affc11196bf4f1a73d
Yordis Prieto <yordis.prieto@gmail.com>
2018-03-05 11:10:39 -0800

    Fix typespec of back action creator (#3659)

--- flow/react-navigation.js
+++ flow/react-navigation.js
@@ -686,7 +686,7 @@ declare module 'react-navigation' {
     SET_PARAMS: 'Navigation/SET_PARAMS',
     URI: 'Navigation/URI',
     back: {
-      (payload: { key?: ?string }): NavigationBackAction,
+      (payload?: { key?: ?string }): NavigationBackAction,
       toString: () => string,
     },
     init: {
