97640dd5060358b57f28f2db3548a0fe911ecabe
Daniel Cochran <dannycochran@google.com>
2018-04-05 10:36:49 -0700

    use format command to fix linting errors

--- src/getChildEventSubscriber.js
+++ src/getChildEventSubscriber.js
@@ -70,9 +70,10 @@ export default function getChildEventSubscriber(addListener, key) {
       const routes = state && state.routes;
 
       const lastFocusKey =
-            lastState && lastState.routes &&
-            lastState.routes.length &&
-            lastState.routes[lastState.index].key;
+        lastState &&
+        lastState.routes &&
+        lastState.routes.length &&
+        lastState.routes[lastState.index].key;
       const focusKey = routes && routes.length && routes[state.index].key;
 
       const isChildFocused = focusKey === key;
