4fd282e4015383b186769d4e869ed18f2a636400
Jed Mao <jedmao@outlook.com>
2018-04-26 15:58:51 -0500

    Explain need for assetsTransformer in comment

--- assetsTransformer.js
+++ assetsTransformer.js
@@ -1,3 +1,8 @@
+/**
+ * This file is needed to hijack asset imports so that test files don't attempt
+ * to import them as JavaScript modules.
+ * See https://github.com/facebook/jest/issues/2663#issuecomment-317109798
+ */
 const path = require('path');
 
 module.exports = {
