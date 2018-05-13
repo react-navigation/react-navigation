97c36c1ef1ea1b0ac7a00c1bc38a1232db13b05d
Eric Vicenti <eric@aven.io>
2018-05-12 20:19:29 -0700

    Isolate modules for uncontainerized navigators

--- src/navigators/createStackNavigator.js
+++ src/navigators/createStackNavigator.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import createNavigationContainer from '../createNavigationContainer';
 import createKeyboardAwareNavigator from './createKeyboardAwareNavigator';
 import createNavigator from './createNavigator';
 import StackView from '../views/StackView/StackView';
@@ -33,8 +32,7 @@ function createStackNavigator(routeConfigMap, stackConfig = {}) {
     Navigator = createKeyboardAwareNavigator(Navigator);
   }
 
-  // HOC to provide the navigation prop for the top-level navigator (when the prop is missing)
-  return createNavigationContainer(Navigator);
+  return Navigator;
 }
 
 export default createStackNavigator;
