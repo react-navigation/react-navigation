f776f87131e14926f7ef18cdd587f0e31c580cec
Louis Lagrange <lagrange.louis@gmail.com>
2018-05-24 18:22:18 +0200

    Fix SwitchNavigator test

--- src/navigators/__tests__/SwitchNavigator-test.js
+++ src/navigators/__tests__/SwitchNavigator-test.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import renderer from 'react-test-renderer';
+import TestRenderer from 'react-test-renderer';
 
 import SwitchNavigator from '../createSwitchNavigator';
+import flushPromises from '../../utils/flushPromises';
 
 const A = () => <View />;
 const B = () => <View />;
 const routeConfig = { A, B };
 
 describe('SwitchNavigator', () => {
-  it('renders successfully', () => {
+  it('renders successfully', async () => {
     const MySwitchNavigator = SwitchNavigator(routeConfig);
-    const rendered = renderer.create(<MySwitchNavigator />).toJSON();
+    const testRenderer = TestRenderer.create(<MySwitchNavigator />);
 
-    expect(rendered).toMatchSnapshot();
+    // the state only actually gets set asynchronously on componentDidMount
+    // thus on the first render the component returns null (or the result of renderLoadingExperimental)
+    expect(testRenderer.toJSON()).toEqual(null);
+    // wait for the state to be set
+    await flushPromises();
+
+    expect(testRenderer.toJSON()).toMatchSnapshot();
   });
 });
