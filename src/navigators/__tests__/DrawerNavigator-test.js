e945c6f1d5eb38b3195be221cfded25e2141d582
Louis Lagrange <lagrange.louis@gmail.com>
2018-05-24 18:20:29 +0200

    Fix DrawerNavigator test

--- src/navigators/__tests__/DrawerNavigator-test.js
+++ src/navigators/__tests__/DrawerNavigator-test.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import renderer from 'react-test-renderer';
+import TestRenderer from 'react-test-renderer';
 
 import DrawerNavigator from '../createDrawerNavigator';
+import flushPromises from '../../utils/flushPromises';
 
 class HomeScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -24,10 +25,16 @@ const routeConfig = {
 };
 
 describe('DrawerNavigator', () => {
-  it('renders successfully', () => {
+  it('renders successfully', async () => {
     const MyDrawerNavigator = DrawerNavigator(routeConfig);
-    const rendered = renderer.create(<MyDrawerNavigator />).toJSON();
+    const testRenderer = TestRenderer.create(<MyDrawerNavigator />);
 
-    expect(rendered).toMatchSnapshot();
+    // the state only actually gets set asynchronously on componentDidMount
+    // thus on the first render the component returns null (or the result of renderLoadingExperimental)
+    expect(testRenderer.toJSON()).toEqual(null);
+    // wait for the state to be set
+    await flushPromises();
+
+    expect(testRenderer.toJSON()).toMatchSnapshot();
   });
 });
