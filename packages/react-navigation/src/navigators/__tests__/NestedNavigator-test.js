74669c76b5a9d7007c7a26b435d3786f5d6768af
Tom Klaver <tomklav@gmail.com>
2018-06-08 18:31:05 +0200

    throw readable error when wrong navigation prop is passed (#4455)
    * add warning when passing wrong navigation prop: https://github.com/react-navigation/react-navigation/issues/3598
    
    * add test for throwing when passing wrong navigation prop
    
    * wrong nav prop error: update snapshots, throw => throw new, update docs reference url

---
+++ packages/react-navigation/src/navigators/__tests__/NestedNavigator-test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import StackNavigator from '../createStackNavigator';
+
+const SubNavigator = StackNavigator({
+  Home: {
+    screen: () => null,
+  },
+});
+
+const NavNestedDirect = StackNavigator({
+  Sub: {
+    screen: SubNavigator,
+  },
+});
+
+const NavNestedIndirect = StackNavigator({
+  Sub: {
+    // eslint-disable-next-line react/display-name
+    screen: props => <SubNavigator {...props} />,
+  },
+});
+
+/* Prevent React error boundaries from swallowing the errors */
+NavNestedIndirect.prototype.componentDidCatch = null;
+SubNavigator.prototype.componentDidCatch = null;
+
+describe('Nested navigators', () => {
+  it('renders succesfully as direct child', () => {
+    const rendered = renderer.create(<NavNestedDirect />).toJSON();
+    expect(rendered).toMatchSnapshot();
+  });
+
+  it('throw when trying to pass navigation prop', () => {
+    const tryRender = () => {
+      renderer.create(<NavNestedIndirect />);
+    };
+    expect(tryRender).toThrowErrorMatchingSnapshot();
+  });
+});
