348e68f338ca87481ca486dd49ca513a3359c3b0
Brent Vatne <brentvatne@gmail.com>
2018-06-11 15:39:40 -0700

    Fix tests

--- packages/drawer/jest-setup.js
+++ packages/drawer/jest-setup.js
@@ -6,21 +6,13 @@
 
 import React from 'react';
 
-[
-  'react-native-drawer-layout-polyfill',
-
-  // tests don't test the native bits, so just substitute the same
-  // fake polyfill for now
-  'react-native-gesture-handler/DrawerLayout',
-].forEach(module => {
-  jest.mock(module, () => {
-    const View = require.requireActual('View');
-    class DrawerLayout extends View {
-      static positions = {
-        Left: 'left',
-        Right: 'right',
-      };
-    }
-    return DrawerLayout;
-  });
-})
+jest.mock('./src/views/DrawerLayout', () => {
+  const View = require.requireActual('View');
+  class DrawerLayout extends View {
+    static positions = {
+      Left: 'left',
+      Right: 'right',
+    };
+  }
+  return DrawerLayout;
+});
