896b62c60b2a86d6da1bf6734ab3a7e7683d02e0
Eric Vicenti <ericvicenti@users.noreply.github.com>
2018-06-22 10:20:27 -0700

    @ericvicenti/universe (#4493)
    * Isolate modules for uncontainerized navigators
    
    * Clean up prop-types
    
    * Fix warnings and web import friendlyness
    
    * strip a flow
    
    * Standalone provider/consumer navigation context
    
    * export shallowEqual as module
    
    * address various lint
    
    # Conflicts:
    #	src/navigators/createStackNavigator.js
    
    * Get tests to pass

--- src/navigators/createStackNavigator.js
+++ src/navigators/createStackNavigator.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import createNavigationContainer from '../createNavigationContainer';
 import createKeyboardAwareNavigator from './createKeyboardAwareNavigator';
 import createNavigator from './createNavigator';
 import StackView from '../views/StackView/StackView';
@@ -33,8 +31,7 @@ function createStackNavigator(routeConfigMap, stackConfig = {}) {
     Navigator = createKeyboardAwareNavigator(Navigator);
   }
 
-  // HOC to provide the navigation prop for the top-level navigator (when the prop is missing)
-  return createNavigationContainer(Navigator);
+  return Navigator;
 }
 
 export default createStackNavigator;
