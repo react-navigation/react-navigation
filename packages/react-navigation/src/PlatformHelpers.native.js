1159573e83d812e94715bd2cb245245d8ac42dc7
Eric Vicenti <ericvicenti@users.noreply.github.com>
2018-06-28 10:02:52 -0700

    clean up PlatformHelpers (#4586)
    this had previously been required for old versions of react native and react-native-web

--- packages/react-navigation/src/PlatformHelpers.native.js
+++
@@ -1,9 +0,0 @@
-import {
-  BackAndroid as DeprecatedBackAndroid,
-  BackHandler as ModernBackHandler,
-  MaskedViewIOS,
-} from 'react-native';
-
-const BackHandler = ModernBackHandler || DeprecatedBackAndroid;
-
-export { BackHandler, MaskedViewIOS };
--- packages/react-navigation/src/PlatformHelpers.web.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { BackHandler, View } from 'react-native';
-
-const MaskedViewIOS = () => <View>{this.props.children}</View>;
-
-export { BackHandler, MaskedViewIOS };
