984f51dbe2751fc7636a2e17aebb63b330e4acb9
Eric Vicenti <eric@aven.io>
2018-07-19 12:41:14 -0700

    Fix createNavigator leak of old descriptors

--- packages/react-navigation/src/navigators/createNavigator.js
+++ packages/react-navigation/src/navigators/createNavigator.js
@@ -24,7 +24,7 @@ function createNavigator(NavigatorView, router, navigationConfig) {
         );
       }
 
-      const descriptors = { ...prevState.descriptors };
+      const descriptors = {};
 
       routes.forEach(route => {
         if (
