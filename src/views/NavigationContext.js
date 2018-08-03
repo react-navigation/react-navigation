943e3cba9fad3b2f7b4e1b6514ec40c03bb1ab88
Brent Vatne <brentvatne@gmail.com>
2018-08-03 14:47:43 -0700

    Fix a couple of exports and export a few more modules

--- src/views/NavigationContext.js
+++ src/views/NavigationContext.js
@@ -5,3 +5,8 @@ const NavigationContext = createReactContext();
 
 export const NavigationProvider = NavigationContext.Provider;
 export const NavigationConsumer = NavigationContext.Consumer;
+
+export default {
+  NavigationProvider,
+  NavigationConsumer,
+};
