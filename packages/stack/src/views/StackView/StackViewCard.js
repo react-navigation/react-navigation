0cb3eb47d599f0aab3d4c8d4f26c26c6ef1be3c3
Brent Vatne <brentvatne@gmail.com>
2018-08-24 10:06:37 -0700

    Filter out active prop on dummy screen component

--- packages/stack/src/views/StackView/StackViewCard.js
+++ packages/stack/src/views/StackView/StackViewCard.js
@@ -3,7 +3,6 @@ import { StyleSheet, Platform } from 'react-native';
 import { Screen } from './screens';
 import createPointerEventsContainer from './createPointerEventsContainer';
 
-/* eslint-disable no-unused-vars */
 const EPS = 1e-5;
 
 function getAccessibilityProps(isActive) {
@@ -30,28 +29,22 @@ class Card extends React.Component {
       children,
       pointerEvents,
       style,
-      // position,
-      scene: { /* index, */ isActive },
+      position,
+      scene: { index, isActive },
     } = this.props;
 
-    // If we use react-native <= 0.55, we can't call position.__makeNative()
-    // before binding this value to the view. If we use >= 0.56, then we have
-    // to call position.__makeNative(). Unclear to me what is happening here
-    // so temporarily commented this out.
-    //
-    // const active = position.interpolate({
-    //   inputRange: [index, index + 1 - EPS, index + 1],
-    //   outputRange: [1, 1, 0],
-    //   extrapolate: 'clamp',
-    // });
+    const active = position.interpolate({
+      inputRange: [index, index + 1 - EPS, index + 1],
+      outputRange: [1, 1, 0],
+      extrapolate: 'clamp',
+    });
 
     return (
       <Screen
         pointerEvents={pointerEvents}
-        ref={this.props.onComponentRef}
+        onComponentRef={this.props.onComponentRef}
         style={[styles.main, style]}
-        // active={active}
-        active={isActive ? 1 : 0}
+        active={active}
         {...getAccessibilityProps(isActive)}
       >
         {children}
