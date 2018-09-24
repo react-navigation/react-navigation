fdb3e220b7ec2f6e9dc2644fb6f87d583a4c49c5
Brent Vatne <brentvatne@gmail.com>
2018-09-24 09:46:11 -0700

    Merge pull request #21 from backmeupplz/master
    Make sure header back button is accessible

--- packages/stack/src/views/Header/HeaderBackButton.js
+++ packages/stack/src/views/Header/HeaderBackButton.js
@@ -108,7 +108,7 @@ class HeaderBackButton extends React.PureComponent {
 
     let button = (
       <TouchableItem
-        accessible={false}
+        accessible={true}
         accessibilityComponentType="button"
         accessibilityLabel={title}
         accessibilityTraits="button"
