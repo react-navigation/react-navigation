9824e90b9fcc5db60e061c37ea89460ec1fb1166
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2018-10-03 13:11:34 -0400

    Add disableRouteNamePaths option to router configs (#4824)
    * Add routeNameAsPathDisabled prop to router config
    
    * Rename option to disableRouteNamePaths
    
    * Update PathHandling-test.js

--- src/routers/pathUtils.js
+++ src/routers/pathUtils.js
@@ -67,7 +67,7 @@ export const urlToPathAndParams = (url, uriPrefix) => {
 export const createPathParser = (
   childRouters,
   routeConfigs,
-  pathConfigs = {}
+  { paths: pathConfigs = {}, disableRouteNamePaths }
 ) => {
   const pathsByRouteNames = {};
   let paths = [];
@@ -84,8 +84,8 @@ export const createPathParser = (
     }
 
     if (pathPattern === undefined) {
-      // If the user hasn't specified a path at all, then we assume the routeName is an appropriate path
-      pathPattern = routeName;
+      // If the user hasn't specified a path at all nor disableRouteNamePaths, then we assume the routeName is an appropriate path
+      pathPattern = disableRouteNamePaths ? null : routeName;
     }
 
     invariant(
