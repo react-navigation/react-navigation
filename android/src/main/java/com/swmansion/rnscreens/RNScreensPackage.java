0daa7e2ef7a316fbc237e85d7067b85dd4ccd96d
Thibault Malbranche <malbranche.thibault@gmail.com>
2018-10-08 09:06:12 +0200

    Rename RNScreen to RNScreens to stay consistent (#27)

--- android/src/main/java/com/swmansion/rnscreens/RNScreensPackage.java
+++ android/src/main/java/com/swmansion/rnscreens/RNScreensPackage.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class RNScreenPackage implements ReactPackage {
+public class RNScreensPackage implements ReactPackage {
   @Override
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
     return Collections.emptyList();
