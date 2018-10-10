5b01eaa6be601e184312f96271859734052ff0a1
Brent Vatne <brentvatne@gmail.com>
2018-10-10 14:00:04 -0700

    Merge pull request #34 from pzatorski/feature/back-button-accessibility
    change accessibilityLabel for HeaderBackButton - Fix iOS voiceover

--- packages/stack/src/views/Header/HeaderBackButton.js
+++ packages/stack/src/views/Header/HeaderBackButton.js
@@ -111,7 +111,7 @@ class HeaderBackButton extends React.PureComponent {
       <TouchableItem
         accessible
         accessibilityComponentType="button"
-        accessibilityLabel={title}
+        accessibilityLabel={title ? `${title}, back` : 'Go back'}
         accessibilityTraits="button"
         testID="header-back"
         delayPressIn={0}
