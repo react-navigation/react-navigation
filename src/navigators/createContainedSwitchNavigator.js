84de94d96130b1879fa22dfb29382254cf5d109a
Brent Vatne <brentvatne@gmail.com>
2018-10-15 14:19:32 -0700

    Fix tests

--- src/navigators/createContainedSwitchNavigator.js
+++
@@ -1,9 +0,0 @@
-import { createNavigationContainer } from '@react-navigation/native';
-import { createSwitchNavigator } from '@react-navigation/core';
-
-const SwitchNavigator = (routeConfigs, config = {}) => {
-  const navigator = createSwitchNavigator(routeConfigs, config);
-  return createNavigationContainer(navigator);
-};
-
-export default SwitchNavigator;
