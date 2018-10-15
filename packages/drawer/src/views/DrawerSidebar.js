53bc4a554b32bb72e6561c679ea96b6269b00dc6
Eric Vicenti <eric@aven.io>
2018-10-15 12:30:43 -0700

    move to react-nav core and native deps

--- packages/drawer/src/views/DrawerSidebar.js
+++ packages/drawer/src/views/DrawerSidebar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import { NavigationActions, StackActions } from 'react-navigation';
+import { NavigationActions, StackActions } from '@react-navigation/core';
 import invariant from '../utils/invariant';
 
 /**
