9d00c5ced10a74e776104e03cc58ab020b3b64ae
Brent Vatne <brentvatne@gmail.com>
2018-10-22 19:45:08 -0700

    Use proper cache object reference and bump version

--- packages/core/src/getChildrenNavigationCache.js
+++ packages/core/src/getChildrenNavigationCache.js
@@ -3,9 +3,8 @@ export default function getChildrenNavigationCache(navigation) {
     return {};
   }
 
-  let childrenNavigationCache = navigation._childrenNavigation
-    ? navigation._childrenNavigation
-    : {};
+  let childrenNavigationCache =
+    navigation._childrenNavigation || (navigation._childrenNavigation = {});
   let childKeys = navigation.state.routes.map(route => route.key);
   Object.keys(childrenNavigationCache).forEach(cacheKey => {
     if (!childKeys.includes(cacheKey)) {
@@ -13,5 +12,5 @@ export default function getChildrenNavigationCache(navigation) {
     }
   });
 
-  return childrenNavigationCache;
+  return navigation._childrenNavigation;
 }
