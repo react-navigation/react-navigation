5ce4cb3ae13ad5bef7a1e730bd88d07d69232c99
Xianglin He <xianglinhe@gmail.com>
2018-10-30 14:27:34 +0800

    added contentContainerStyle.

--- packages/drawer/src/views/DrawerView.js
+++ packages/drawer/src/views/DrawerView.js
@@ -181,6 +181,7 @@ export default class DrawerView extends React.PureComponent {
         statusBarAnimation={this.props.navigationConfig.statusBarAnimation}
         minSwipeDistance={this.props.navigationConfig.minSwipeDistance}
         overlayColor={this.props.navigationConfig.overlayColor}
+        contentContainerStyle={this.props.navigationConfig.contentContainerStyle}
       >
         <DrawerGestureContext.Provider value={this.drawerGestureRef}>
           <ScreenContainer style={styles.pages}>
