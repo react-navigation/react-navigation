f8a0fd1cb3c6ae004919bba15391a66f198c87b2
Yu Watanabe <fight.ippatu5648@gmail.com>
2018-11-27 03:07:51 +0900

    ./src/createNavigationContainer -> @react-navigation/native/src/createAppContainer on NavigationTestUtils.js (#5266)
    * ./src/createNavigationContainer deprecated , so change.
    
    * changelog

--- packages/react-navigation/NavigationTestUtils.js
+++ packages/react-navigation/NavigationTestUtils.js
@@ -1,4 +1,4 @@
-import { _TESTING_ONLY_reset_container_count } from './src/createNavigationContainer';
+import { _TESTING_ONLY_reset_container_count } from '@react-navigation/native/src/createAppContainer';
 
 export default {
   resetInternalState: () => {
