ddf7dddd96206b9b858b7a66d6e0f95f95217266
Brent Vatne <brentvatne@gmail.com>
2018-11-30 13:00:25 -0800

    Merge pull request #24 from bySabi/master
    call createReactContext with at explicit non-value

--- packages/core/src/views/NavigationContext.js
+++ packages/core/src/views/NavigationContext.js
@@ -1,5 +1,5 @@
 import createReactContext from 'create-react-context';
 
-const NavigationContext = createReactContext();
+const NavigationContext = createReactContext(undefined);
 
 export default NavigationContext;
