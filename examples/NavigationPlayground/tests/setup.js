e117bb3cde10d29f28dbe2da5c919d2034708d69
Brent Vatne <brentvatne@gmail.com>
2019-03-13 21:24:15 -0700

    Get Jest running in NavigationPlayground

--- examples/NavigationPlayground/tests/setup.js
+++ examples/NavigationPlayground/tests/setup.js
@@ -3,3 +3,44 @@ import { NativeModules } from 'react-native';
 NativeModules.ReactLocalization = {
   language: 'en',
 };
+
+// TODO: remove these mocks!
+jest.mock('react-native-gesture-handler', () => {
+  const View = require('react-native/Libraries/Components/View/View');
+  return {
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    /* Buttons */
+    RawButton: View,
+    BaseButton: View,
+    RectButton: View,
+    BorderlessButton: View,
+    /* Other */
+    FlatList: View,
+    gestureHandlerRootHOC: jest.fn(),
+    Directions: {},
+  };
+});
+
+jest.mock('react-native-gesture-handler/DrawerLayout', () => {
+  return {
+    Directions: null,
+  };
+});
