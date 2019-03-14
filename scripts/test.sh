a8c0f83df9b6650fe955a3cd039920f6a54ecf7c
Brent Vatne <brentvatne@gmail.com>
2019-03-13 21:27:15 -0700

    Enable tests on CI again (currently will fail because of TS error in RNGH)

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,5 @@ set -eo pipefail
 
 case $CIRCLE_NODE_INDEX in 
   0) yarn test && yarn codecov ;; 
-  #1) cd examples/NavigationPlayground && yarn && yarn test ;; 
-  #2) cd examples/ReduxExample && yarn && yarn test ;; 
+  1) cd examples/NavigationPlayground && yarn && yarn test ;; 
 esac
