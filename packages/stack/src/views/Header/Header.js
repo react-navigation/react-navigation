092f87035922d73e30a80461fa405c283a83632d
Eric Vicenti <ericvicenti@users.noreply.github.com>
2019-03-27 00:25:31 -0700

    Merge pull request #99 from expo/@evanbacon/expo-web/header-shadow
    [web] added: header shadow

--- packages/stack/src/views/Header/Header.js
+++ packages/stack/src/views/Header/Header.js
@@ -649,7 +649,11 @@ const platformContainerStyles = Platform.select({
     borderBottomWidth: StyleSheet.hairlineWidth,
     borderBottomColor: '#A7A7AA',
   },
-  default: {},
+  default: {
+    // https://github.com/necolas/react-native-web/issues/44
+    // Material Design
+    boxShadow: `0 2px 4px -1px rgba(0,0,0,0.2), 0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12)`,
+  },
 });
 
 const DEFAULT_BACKGROUND_COLOR = '#FFF';
