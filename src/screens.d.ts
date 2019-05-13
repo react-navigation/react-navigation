acf80e640c584bf4019cfaf9356ee44b09e7dc99
Satyajit Sahoo <satyajit.happy@gmail.com>
2019-05-13 14:40:47 +0200

    Fix type of active prop in typescript definitions (#97)
    The TS definitions specify the prop as a boolean, but passing a boolean causes a crash.

--- src/screens.d.ts
+++ src/screens.d.ts
@@ -3,13 +3,13 @@
 
 declare module 'react-native-screens' {
   import { ComponentClass } from 'react';
-  import { ViewProps } from 'react-native';
+  import { ViewProps, Animated } from 'react-native';
 
   export function useScreens(shouldUseScreens?: boolean): void;
   export function screensEnabled(): boolean;
 
   export interface ScreenProps extends ViewProps {
-    active?: boolean;
+    active?: 0 | 1 | Animated.AnimatedInterpolation;
     onComponentRef?: (view: any) => void;
   }
   export const Screen: ComponentClass<ScreenProps>;
