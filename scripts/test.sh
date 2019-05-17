579f572e4e69a5e57fb4ee46557321afb730d431
Bruno <bruno.castro@codeminer42.com>
2019-05-17 09:50:18 -0300

    tests: remove tests from example app

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,6 @@
 
 set -eo pipefail
 
-case $CIRCLE_NODE_INDEX in 
-  0) yarn test && yarn codecov ;; 
-  1) yarn && cd examples/NavigationPlayground && yarn && yarn test ;; 
+case $CIRCLE_NODE_INDEX in
+  0) yarn test && yarn codecov ;;
 esac
