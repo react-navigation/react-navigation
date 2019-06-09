0514ca7fcad5bc04b98201588a1f9e0f9fecae44
satyajit.happy <satyajit.happy@gmail.com>
2019-06-09 20:19:50 +0200

    feat: add basic nesting example

--- example/index.tsx
+++ example/index.tsx
@@ -3,12 +3,16 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { NavigationContainer, Screen } from '../src';
 import StackNavigator, { StackNavigationProp } from './StackNavigator';
+import TabNavigator, { TabNavigationProp } from './TabNavigator';
 
 const First = ({ navigation }: { navigation: StackNavigationProp }) => (
   <div>
     <h1>First route</h1>
     <button type="button" onClick={() => navigation.push('second')}>
       Push second
+    </button>
+    <button type="button" onClick={() => navigation.push('third')}>
+      Push third
     </button>
     <button type="button" onClick={() => navigation.pop()}>
       Go back
@@ -21,6 +25,44 @@ const Second = ({ navigation }: { navigation: StackNavigationProp }) => (
     <h1>Second route</h1>
     <button type="button" onClick={() => navigation.push('first')}>
       Push first
+    </button>
+    <button type="button" onClick={() => navigation.pop()}>
+      Go back
+    </button>
+  </div>
+);
+
+const Fourth = ({
+  navigation,
+}: {
+  navigation: TabNavigationProp & StackNavigationProp;
+}) => (
+  <div>
+    <h1>Fourth route</h1>
+    <button type="button" onClick={() => navigation.jumpTo('fifth')}>
+      Jump to fifth
+    </button>
+    <button type="button" onClick={() => navigation.push('first')}>
+      Push first
+    </button>
+    <button type="button" onClick={() => navigation.pop()}>
+      Go back
+    </button>
+  </div>
+);
+
+const Fifth = ({
+  navigation,
+}: {
+  navigation: TabNavigationProp & StackNavigationProp;
+}) => (
+  <div>
+    <h1>Fifth route</h1>
+    <button type="button" onClick={() => navigation.jumpTo('fourth')}>
+      Jump to fourth
+    </button>
+    <button type="button" onClick={() => navigation.push('second')}>
+      Push second
     </button>
     <button type="button" onClick={() => navigation.pop()}>
       Go back
@@ -49,6 +91,14 @@ function App() {
       <StackNavigator>
         <Screen name="first" component={First} />
         <Screen name="second" component={Second} />
+        <Screen name="third">
+          {props => (
+            <TabNavigator {...props}>
+              <Screen name="fourth" component={Fourth} />
+              <Screen name="fifth" component={Fifth} />
+            </TabNavigator>
+          )}
+        </Screen>
       </StackNavigator>
     </NavigationContainer>
   );
