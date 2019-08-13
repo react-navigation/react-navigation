597b5dadc4cf3533d4e98bc5ce3524f29ea37243
Satyajit Sahoo <satyajit.happy@gmail.com>
2019-08-14 01:41:39 +0530

    feat: add a useIsFocused hook to get focus state (#52)

---
+++ packages/core/src/__tests__/useIsFocused.test.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { render, act } from 'react-native-testing-library';
+import useNavigationBuilder from '../useNavigationBuilder';
+import useIsFocused from '../useIsFocused';
+import NavigationContainer from '../NavigationContainer';
+import Screen from '../Screen';
+import MockRouter from './__fixtures__/MockRouter';
+
+it('renders correct focus state', () => {
+  const TestNavigator = (props: any): any => {
+    const { state, descriptors } = useNavigationBuilder(MockRouter, props);
+
+    return state.routes.map(route => descriptors[route.key].render());
+  };
+
+  const Test = () => {
+    const isFocused = useIsFocused();
+
+    return (
+      <React.Fragment>{isFocused ? 'focused' : 'unfocused'}</React.Fragment>
+    );
+  };
+
+  const navigation = React.createRef<any>();
+
+  const root = render(
+    <NavigationContainer ref={navigation}>
+      <TestNavigator>
+        <Screen name="first">{() => null}</Screen>
+        <Screen name="second" component={Test} />
+        <Screen name="third">{() => null}</Screen>
+      </TestNavigator>
+    </NavigationContainer>
+  );
+
+  expect(root).toMatchInlineSnapshot(`"unfocused"`);
+
+  act(() => navigation.current.navigate('second'));
+
+  expect(root).toMatchInlineSnapshot(`"focused"`);
+
+  act(() => navigation.current.navigate('third'));
+
+  expect(root).toMatchInlineSnapshot(`"unfocused"`);
+
+  act(() => navigation.current.navigate('second'));
+
+  expect(root).toMatchInlineSnapshot(`"focused"`);
+});
---
+++ packages/core/src/useIsFocused.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { useSubscription } from 'use-subscription';
+import useNavigation from './useNavigation';
+
+/**
+ * Hook to get the current focus state of the screen. Returns a `true` if screen is focused, otherwise `false`.
+ * This can be used if a component needs to render something based on the focus state.
+ * It uses `use-subscription` under the hood for safer use in concurrent mode.
+ */
+export default function useIsFocused(): boolean {
+  const navigation = useNavigation();
+  const getCurrentValue = React.useCallback(navigation.isFocused, [navigation]);
+  const subscribe = React.useCallback(
+    (callback: (value: boolean) => void) => {
+      const unsubscribeFocus = navigation.addListener('focus', () =>
+        callback(true)
+      );
+
+      const unsubscribeBlur = navigation.addListener('blur', () =>
+        callback(false)
+      );
+
+      return () => {
+        unsubscribeFocus();
+        unsubscribeBlur();
+      };
+    },
+    [navigation]
+  );
+
+  return useSubscription({
+    getCurrentValue,
+    subscribe,
+  });
+}
