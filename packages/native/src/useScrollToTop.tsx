5bb5b5a1ef82a4d860aef393f9a4493fc6925946
satyajit.happy <satyajit.happy@gmail.com>
2019-08-28 12:22:15 +0200

    fix: fix stack nested in tab always getting reset

--- packages/native/src/useScrollToTop.tsx
+++ packages/native/src/useScrollToTop.tsx
@@ -14,10 +14,12 @@ export default function useScrollToTop(ref: React.RefObject<ScrollableView>) {
       // We don't wanna import tab types here to avoid extra deps
       // in addition, there are multiple tab implementations
       navigation.addListener('tabPress', (e: EventArg<'tabPress'>) => {
+        const isFocused = navigation.isFocused();
+
         // Run the operation in the next frame so we're sure all listeners have been run
         // This is necessary to know if preventDefault() has been called
         requestAnimationFrame(() => {
-          if (navigation.isFocused() && !e.defaultPrevented && ref.current) {
+          if (isFocused && !e.defaultPrevented && ref.current) {
             // When user taps on already focused tab, scroll to top
             ref.current.scrollTo({ y: 0 });
           }
--- packages/stack/src/navigators/createStackNavigator.tsx
+++ packages/stack/src/navigators/createStackNavigator.tsx
@@ -45,14 +45,12 @@ function StackNavigator({
     () =>
       navigation.addListener &&
       navigation.addListener('tabPress', (e: EventArg<'tabPress'>) => {
+        const isFocused = navigation.isFocused();
+
         // Run the operation in the next frame so we're sure all listeners have been run
         // This is necessary to know if preventDefault() has been called
         requestAnimationFrame(() => {
-          if (
-            state.index > 0 &&
-            navigation.isFocused() &&
-            !e.defaultPrevented
-          ) {
+          if (state.index > 0 && isFocused && !e.defaultPrevented) {
             // When user taps on already focused tab and we're inside the tab,
             // reset the stack to replicate native behaviour
             navigation.dispatch({
