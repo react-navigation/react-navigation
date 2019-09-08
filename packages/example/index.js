eb8375c915100ce41cf0c58840479d0902495f54
Brent Vatne <brentvatne@gmail.com>
2019-09-08 09:10:44 -0700

    feat: make example run as bare react-native project as well (#85)

---
+++ packages/example/index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+
+// Need to add extension or eslint confuses it with app.json
+import App from './App.tsx';
+
+AppRegistry.registerComponent('ReactNavigationExample', () => App);
