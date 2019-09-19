51cf75f97097221c85c7a116797a66886b55f582
satyajit.happy <satyajit.happy@gmail.com>
2019-09-19 16:30:32 +0200

    refactor: don't use export * to reexport deprecations

--- packages/react-navigation/src/react-navigation.js
+++ packages/react-navigation/src/react-navigation.js
@@ -1,4 +1,29 @@
 export * from '@react-navigation/core';
 export * from '@react-navigation/native';
 
-export * from './deprecations';
+// Export each item individually so that they can be evaluated lazily
+// https://babeljs.io/docs/en/babel-plugin-transform-modules-commonjs#lazy
+export {
+  createNavigationContainer,
+  createStackNavigator,
+  createBottomTabNavigator,
+  createMaterialTopTabNavigator,
+  createDrawerNavigator,
+  StackGestureContext,
+  DrawerGestureContext,
+  DrawerRouter,
+  DrawerActions,
+  Transitioner,
+  StackView,
+  StackViewCard,
+  StackViewTransitionConfigs,
+  Header,
+  HeaderTitle,
+  HeaderBackButton,
+  HeaderStyleInterpolator,
+  DrawerView,
+  DrawerItems,
+  DrawerSidebar,
+  BottomTabBar,
+  MaterialTopTabBar,
+} from './deprecations';
