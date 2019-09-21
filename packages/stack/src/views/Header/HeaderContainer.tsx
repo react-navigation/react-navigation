b95c4321dddd9bdb48562a416b43768d4b9bc8a8
satyajit.happy <satyajit.happy@gmail.com>
2019-09-22 01:05:17 +0200

    fix: provide navigation prop in header

--- packages/stack/src/views/Header/HeaderContainer.tsx
+++ packages/stack/src/views/Header/HeaderContainer.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { View, StyleSheet, StyleProp, ViewStyle } from 'react-native';
-import { NavigationRoute } from 'react-navigation';
+import { NavigationContext, NavigationRoute } from 'react-navigation';
 import {
   Layout,
   HeaderScene,
@@ -88,36 +88,40 @@ export default function HeaderContainer({
         };
 
         return (
-          <View
+          <NavigationContext.Provider
             key={scene.route.key}
-            onLayout={
-              onContentHeightChange
-                ? e =>
-                    onContentHeightChange({
-                      route: scene.route,
-                      height: e.nativeEvent.layout.height,
-                    })
-                : undefined
-            }
-            pointerEvents="box-none"
-            accessibilityElementsHidden={!isFocused}
-            importantForAccessibility={
-              isFocused ? 'auto' : 'no-hide-descendants'
-            }
-            style={
-              mode === 'float' || options.headerTransparent
-                ? styles.header
-                : null
-            }
+            value={scene.descriptor.navigation}
           >
-            {options.header !== undefined ? (
-              options.header === null ? null : (
-                options.header(props)
-              )
-            ) : (
-              <Header {...props} />
-            )}
-          </View>
+            <View
+              onLayout={
+                onContentHeightChange
+                  ? e =>
+                      onContentHeightChange({
+                        route: scene.route,
+                        height: e.nativeEvent.layout.height,
+                      })
+                  : undefined
+              }
+              pointerEvents="box-none"
+              accessibilityElementsHidden={!isFocused}
+              importantForAccessibility={
+                isFocused ? 'auto' : 'no-hide-descendants'
+              }
+              style={
+                mode === 'float' || options.headerTransparent
+                  ? styles.header
+                  : null
+              }
+            >
+              {options.header !== undefined ? (
+                options.header === null ? null : (
+                  options.header(props)
+                )
+              ) : (
+                <Header {...props} />
+              )}
+            </View>
+          </NavigationContext.Provider>
         );
       })}
     </View>
