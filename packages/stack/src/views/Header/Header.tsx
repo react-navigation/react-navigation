d09c9ae643d05b2b918c3ce86dfd7b8404901245
Dulmandakh <dulmandakh@gmail.com>
2019-09-21 23:53:44 +0800

    refactor(stack): convert Header component to function (#104)

--- packages/stack/src/views/Header/Header.tsx
+++ packages/stack/src/views/Header/Header.tsx
@@ -4,64 +4,56 @@ import HeaderSegment from './HeaderSegment';
 import { HeaderProps, HeaderTitleProps } from '../../types';
 import HeaderTitle from './HeaderTitle';
 
-export default class Header extends React.PureComponent<HeaderProps> {
-  render() {
-    const {
-      scene,
-      previous,
-      layout,
-      navigation,
-      styleInterpolator,
-    } = this.props;
-    const { options } = scene.descriptor;
-    const title =
-      typeof options.headerTitle !== 'function' &&
-      options.headerTitle !== undefined
-        ? options.headerTitle
-        : options.title !== undefined
-        ? options.title
-        : scene.route.name;
+export default React.memo(function Header(props: HeaderProps) {
+  const { scene, previous, layout, navigation, styleInterpolator } = props;
+  const { options } = scene.descriptor;
+  const title =
+    typeof options.headerTitle !== 'function' &&
+    options.headerTitle !== undefined
+      ? options.headerTitle
+      : options.title !== undefined
+      ? options.title
+      : scene.route.name;
 
-    let leftLabel;
+  let leftLabel;
 
-    // The label for the left back button shows the title of the previous screen
-    // If a custom label is specified, we use it, otherwise use previous screen's title
-    if (options.headerBackTitle !== undefined) {
-      leftLabel = options.headerBackTitle;
-    } else if (previous) {
-      const o = previous.descriptor.options;
+  // The label for the left back button shows the title of the previous screen
+  // If a custom label is specified, we use it, otherwise use previous screen's title
+  if (options.headerBackTitle !== undefined) {
+    leftLabel = options.headerBackTitle;
+  } else if (previous) {
+    const o = previous.descriptor.options;
 
-      leftLabel =
-        typeof o.headerTitle !== 'function' && o.headerTitle !== undefined
-          ? o.headerTitle
-          : o.title !== undefined
-          ? o.title
-          : previous.route.name;
-    }
-
-    return (
-      <HeaderSegment
-        {...options}
-        layout={layout}
-        scene={scene}
-        title={title}
-        leftLabel={leftLabel}
-        headerTitle={
-          typeof options.headerTitle !== 'function'
-            ? (props: HeaderTitleProps) => <HeaderTitle {...props} />
-            : options.headerTitle
-        }
-        onGoBack={
-          previous
-            ? () =>
-                navigation.dispatch({
-                  ...StackActions.pop(),
-                  source: scene.route.key,
-                })
-            : undefined
-        }
-        styleInterpolator={styleInterpolator}
-      />
-    );
+    leftLabel =
+      typeof o.headerTitle !== 'function' && o.headerTitle !== undefined
+        ? o.headerTitle
+        : o.title !== undefined
+        ? o.title
+        : previous.route.name;
   }
-}
+
+  return (
+    <HeaderSegment
+      {...options}
+      layout={layout}
+      scene={scene}
+      title={title}
+      leftLabel={leftLabel}
+      headerTitle={
+        typeof options.headerTitle !== 'function'
+          ? (props: HeaderTitleProps) => <HeaderTitle {...props} />
+          : options.headerTitle
+      }
+      onGoBack={
+        previous
+          ? () =>
+              navigation.dispatch({
+                ...StackActions.pop(),
+                source: scene.route.key,
+              })
+          : undefined
+      }
+      styleInterpolator={styleInterpolator}
+    />
+  );
+});
