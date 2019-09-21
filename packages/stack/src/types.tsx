25d3e96d0ed4bc342180c92b62682c72730b531e
satyajit.happy <satyajit.happy@gmail.com>
2019-09-22 00:58:01 +0200

    fix: set proper type for screenProps in ScreenComponent

--- packages/stack/src/types.tsx
+++ packages/stack/src/types.tsx
@@ -143,7 +143,8 @@ export type NavigationStackScreenComponent<
 > = React.ComponentType<NavigationStackScreenProps<Params, ScreenProps>> & {
   navigationOptions?: NavigationScreenConfig<
     NavigationStackOptions,
-    NavigationStackProp<NavigationRoute, Params>
+    NavigationStackProp<NavigationRoute, Params>,
+    ScreenProps
   >;
 };
 
