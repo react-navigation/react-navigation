2d54b76cb6b7cf6ad0d8a104e7b8e00289a75cf7
Krzysztof Magiera <krzys@swmansion.com>
2019-10-09 11:05:37 +0200

    Update Example app to RN 0.61 (#177)

--- Example/android/app/src/main/java/com/swmansion/rnscreens/example/MainActivity.java
+++ Example/android/app/src/main/java/com/swmansion/rnscreens/example/MainActivity.java
@@ -3,6 +3,9 @@ package com.swmansion.rnscreens.example;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -18,5 +21,15 @@ public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
