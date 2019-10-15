b1da2387b8ad100c76c5f62a9bf5f3dd238a3655
Michał Osadnik <micosa97@gmail.com>
2019-10-15 14:28:39 +0200

    fix: block GH interactions in Native Stack example (#126)

--- packages/example/src/Screens/NativeStack.tsx
+++ packages/example/src/Screens/NativeStack.tsx
@@ -3,7 +3,12 @@ import { View, StyleSheet } from 'react-native';
 import { Button } from 'react-native-paper';
 // eslint-disable-next-line import/no-unresolved
 import { useScreens } from 'react-native-screens';
-import { RouteProp, ParamListBase } from '@react-navigation/core';
+import {
+  RouteProp,
+  ParamListBase,
+  useFocusEffect,
+} from '@react-navigation/core';
+import { DrawerNavigationProp } from '@react-navigation/drawer';
 import { StackNavigationProp } from '@react-navigation/stack';
 import {
   createNativeStackNavigator,
@@ -83,6 +88,22 @@ export default function NativeStackScreen({ navigation }: Props) {
   navigation.setOptions({
     headerShown: false,
   });
+
+  useFocusEffect(
+    React.useCallback(() => {
+      const drawer = navigation.dangerouslyGetParent() as DrawerNavigationProp<
+        ParamListBase
+      >;
+
+      navigation.setOptions({ gestureEnabled: false });
+      drawer.setOptions({ drawerLockMode: 'locked-closed' });
+
+      return () => {
+        navigation.setOptions({ gestureEnabled: true });
+        drawer.setOptions({ drawerLockMode: 'unlocked' });
+      };
+    }, [navigation])
+  );
 
   return (
     <NativeStack.Navigator>
