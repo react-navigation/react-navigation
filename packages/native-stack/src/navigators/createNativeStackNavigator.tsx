301c35ec328d28aabbc9e7f76c0ad03af44c45ac
satyajit.happy <satyajit.happy@gmail.com>
2019-11-04 17:09:01 +0100

    fix: popToTop on tab press in native stack

--- packages/native-stack/src/navigators/createNativeStackNavigator.tsx
+++ packages/native-stack/src/navigators/createNativeStackNavigator.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { createNavigator, useNavigationBuilder } from '@react-navigation/core';
+import {
+  createNavigator,
+  useNavigationBuilder,
+  EventArg,
+} from '@react-navigation/core';
 
 import {
   StackRouter,
   StackNavigationState,
   StackRouterOptions,
+  StackActions,
 } from '@react-navigation/routers';
 
 import {
@@ -35,6 +40,28 @@ function NativeStackNavigator(props: NativeStackNavigatorProps) {
     children,
     screenOptions,
   });
+
+  React.useEffect(
+    () =>
+      navigation.addListener &&
+      navigation.addListener('tabPress', (e: EventArg<'tabPress'>) => {
+        const isFocused = navigation.isFocused();
+
+        // Run the operation in the next frame so we're sure all listeners have been run
+        // This is necessary to know if preventDefault() has been called
+        requestAnimationFrame(() => {
+          if (state.index > 0 && isFocused && !e.defaultPrevented) {
+            // When user taps on already focused tab and we're inside the tab,
+            // reset the stack to replicate native behaviour
+            navigation.dispatch({
+              ...StackActions.popToTop(),
+              target: state.key,
+            });
+          }
+        });
+      }),
+    [navigation, state.index, state.key]
+  );
 
   return (
     <StackView
