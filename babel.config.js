b805ba111a82f8c440d90ecd48d7d46ca867d332
Satyajit Sahoo <satyajit.happy@gmail.com>
2019-11-29 17:12:05 +0100

    chore: upgrade react-native and expo

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,9 @@ module.exports = {
       {
         useBuiltIns: 'usage',
         corejs: 3,
+        targets: {
+          node: 'current',
+        },
       },
     ],
     '@babel/preset-react',
