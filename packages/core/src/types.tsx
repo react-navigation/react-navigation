499f66dba4dcfd59b5f6bfb5511cabb23c1a36e8
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-01-01 13:32:50 +0100

    chore: fix type error

--- packages/core/src/types.tsx
+++ packages/core/src/types.tsx
@@ -83,10 +83,6 @@ export type NavigationAction = {
    * Key of the navigator which should handle this action.
    */
   target?: string;
-  /**
-   * Additional data for the action.
-   */
-  payload?: object;
 };
 
 export type ActionCreators<Action extends NavigationAction> = {
