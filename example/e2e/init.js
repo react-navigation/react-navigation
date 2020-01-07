7d77464831fef35404d133cc47f02b4e3c6af02d
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-01-07 03:57:10 +0100

    chore: setup detox for iOS

---
+++ example/e2e/init.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-commonjs, jest/no-jasmine-globals */
+/* eslint-env jest, jasmine */
+
+const detox = require('detox');
+const config = require('../package.json').detox;
+const adapter = require('detox/runners/jest/adapter');
+const specReporter = require('detox/runners/jest/specReporter');
+
+// Set the default timeout
+jest.setTimeout(120000);
+
+jasmine.getEnv().addReporter(adapter);
+
+// This takes care of generating status logs on a per-spec basis. By default, jest only reports at file-level.
+// This is strictly optional.
+jasmine.getEnv().addReporter(specReporter);
+
+beforeAll(async () => {
+  await detox.init(config);
+}, 300000);
+
+beforeEach(async () => {
+  await adapter.beforeEach();
+});
+
+afterAll(async () => {
+  await adapter.afterAll();
+  await detox.cleanup();
+});
