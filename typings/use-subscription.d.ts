102f07edc8f047a63e0d74a1d7602f1b4870b027
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-01-13 18:31:41 +0100

    fix: make sure paths aren't aliased when building definitions
    closes #265

--- typings/use-subscription.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'use-subscription' {
-  export function useSubscription<T>(options: {
-    getCurrentValue: () => T;
-    subscribe: (callback: (value: T) => void) => () => void;
-  }): T;
-}
