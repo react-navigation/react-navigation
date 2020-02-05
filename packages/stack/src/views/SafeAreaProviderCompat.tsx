a12184414837351753b2710a57f02fbee2b612d4
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-02-05 14:47:36 +0100

    chore: prepare for stable release (#354)

--- packages/stack/src/views/SafeAreaProviderCompat.tsx
+++ packages/stack/src/views/SafeAreaProviderCompat.tsx
@@ -26,7 +26,7 @@ export default function SafeAreaProviderCompat({ children }: Props) {
         if (insets) {
           // If we already have insets, don't wrap the stack in another safe area provider
           // This avoids an issue with updates at the cost of potentially incorrect values
-          // https://github.com/react-navigation/navigation-ex/issues/174
+          // https://github.com/react-navigation/react-navigation/issues/174
           return children;
         }
 
