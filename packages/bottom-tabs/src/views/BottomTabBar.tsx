b6e7e08b9a05be6c04ed21e938b9580876239116
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-02-11 15:42:00 +0100

    fix: provide route context to header and bottom tabs

--- packages/bottom-tabs/src/views/BottomTabBar.tsx
+++ packages/bottom-tabs/src/views/BottomTabBar.tsx
@@ -11,6 +11,7 @@ import {
 } from 'react-native';
 import {
   NavigationContext,
+  NavigationRouteContext,
   CommonActions,
   useTheme,
 } from '@react-navigation/native';
@@ -237,27 +238,29 @@ export default function BottomTabBar({
                   key={route.key}
                   value={descriptors[route.key].navigation}
                 >
-                  <BottomTabItem
-                    route={route}
-                    focused={focused}
-                    horizontal={shouldUseHorizontalLabels()}
-                    onPress={onPress}
-                    onLongPress={onLongPress}
-                    accessibilityLabel={accessibilityLabel}
-                    testID={options.tabBarTestID}
-                    allowFontScaling={allowFontScaling}
-                    activeTintColor={activeTintColor}
-                    inactiveTintColor={inactiveTintColor}
-                    activeBackgroundColor={activeBackgroundColor}
-                    inactiveBackgroundColor={inactiveBackgroundColor}
-                    button={options.tabBarButton}
-                    icon={options.tabBarIcon}
-                    label={label}
-                    showIcon={showIcon}
-                    showLabel={showLabel}
-                    labelStyle={labelStyle}
-                    style={tabStyle}
-                  />
+                  <NavigationRouteContext.Provider value={route}>
+                    <BottomTabItem
+                      route={route}
+                      focused={focused}
+                      horizontal={shouldUseHorizontalLabels()}
+                      onPress={onPress}
+                      onLongPress={onLongPress}
+                      accessibilityLabel={accessibilityLabel}
+                      testID={options.tabBarTestID}
+                      allowFontScaling={allowFontScaling}
+                      activeTintColor={activeTintColor}
+                      inactiveTintColor={inactiveTintColor}
+                      activeBackgroundColor={activeBackgroundColor}
+                      inactiveBackgroundColor={inactiveBackgroundColor}
+                      button={options.tabBarButton}
+                      icon={options.tabBarIcon}
+                      label={label}
+                      showIcon={showIcon}
+                      showLabel={showLabel}
+                      labelStyle={labelStyle}
+                      style={tabStyle}
+                    />
+                  </NavigationRouteContext.Provider>
                 </NavigationContext.Provider>
               );
             })}
