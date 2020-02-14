b7acd73fc7a4d303a5ed0fb588b0643e6750d87b
Chris <chris@altruisticsoftware.com>
2020-02-14 03:43:53 -0600

    docs: Update types.tsx (#6849)
    Typo :)

--- packages/bottom-tabs/src/types.tsx
+++ packages/bottom-tabs/src/types.tsx
@@ -176,8 +176,8 @@ export type BottomTabBarOptions = {
    */
   tabStyle?: StyleProp<ViewStyle>;
   /**
-   * Whether the label is renderd below the icon or beside the icon.
-   * By default, in `vertical` orinetation, label is rendered below and in `horizontal` orientation, it's renderd beside.
+   * Whether the label is rendered below the icon or beside the icon.
+   * By default, in `vertical` orinetation, label is rendered below and in `horizontal` orientation, it's rendered beside.
    */
   labelPosition?: LabelPosition;
   /**
