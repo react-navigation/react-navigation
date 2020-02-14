ac242fd28110132182d4e315b100601bf779712b
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-02-14 23:04:09 +0100

    refactor: discard all routes but last when getting action from state

--- packages/core/src/getActionFromState.tsx
+++ packages/core/src/getActionFromState.tsx
@@ -5,63 +5,50 @@ type NavigateParams = {
   params?: NavigateParams;
 };
 
-type Action =
-  | {
-      type: 'NAVIGATE';
-      payload: { name: string; params: NavigateParams };
-    }
-  | {
-      type: 'RESET_ROOT';
-      payload: PartialState<NavigationState>;
-    };
+type NavigateAction = {
+  type: 'NAVIGATE';
+  payload: { name: string; params: NavigateParams };
+};
 
 export default function getActionFromState(
   state: PartialState<NavigationState>
-): Action {
-  let payload: { name: string; params: NavigateParams } | undefined;
-
-  if (state.routes.length === 1) {
-    // Try to construct payload for a `NAVIGATE` action from the state
-    // This lets us preserve the navigation state and not lose it
-    let route = state.routes[0];
-
-    payload = {
-      name: route.name,
-      params: { ...route.params },
-    };
-
-    let current = state.routes[0].state;
-    let params = payload.params;
-
-    while (current) {
-      if (current.routes.length === 1) {
-        route = current.routes[0];
-        params.screen = route.name;
-
-        if (route.state) {
-          params.params = { ...route.params };
-          params = params.params;
-        } else {
-          params.params = route.params;
-        }
-
-        current = route.state;
-      } else {
-        payload = undefined;
-        break;
-      }
-    }
+): NavigateAction | undefined {
+  if (state.routes.length === 0) {
+    return undefined;
   }
 
-  if (payload) {
-    return {
-      type: 'NAVIGATE',
-      payload,
-    };
+  // Try to construct payload for a `NAVIGATE` action from the state
+  // This lets us preserve the navigation state and not lose it
+  let route = state.routes[state.routes.length - 1];
+
+  let payload: { name: string; params: NavigateParams } = {
+    name: route.name,
+    params: { ...route.params },
+  };
+
+  let current = route.state;
+  let params = payload.params;
+
+  while (current) {
+    if (current.routes.length === 0) {
+      return undefined;
+    }
+
+    route = current.routes[current.routes.length - 1];
+    params.screen = route.name;
+
+    if (route.state) {
+      params.params = { ...route.params };
+      params = params.params;
+    } else {
+      params.params = route.params;
+    }
+
+    current = route.state;
   }
 
   return {
-    type: 'RESET_ROOT',
-    payload: state,
+    type: 'NAVIGATE',
+    payload,
   };
 }
