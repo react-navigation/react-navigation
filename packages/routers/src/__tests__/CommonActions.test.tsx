70d8b1254dab3b4708266ebf5f0f954017f74fbd
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-02-21 19:13:11 +0100

    fix: tweak error message for navigate

--- packages/routers/src/__tests__/CommonActions.test.tsx
+++ packages/routers/src/__tests__/CommonActions.test.tsx
@@ -3,6 +3,6 @@ import * as CommonActions from '../CommonActions';
 it('throws if NAVIGATE is called without key or name', () => {
   // @ts-ignore
   expect(() => CommonActions.navigate({})).toThrowError(
-    'While calling navigate with an object as the argument, you need to specify name or key'
+    'You need to specify name or key when calling navigate with an object as the argument.'
   );
 });
