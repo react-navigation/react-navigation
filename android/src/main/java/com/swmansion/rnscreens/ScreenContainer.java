4e8d13dc72a04cfbd40a71adf80dd56f549b61cb
Krzysztof Magiera <krzys@swmansion.com>
2020-02-24 18:03:46 +0100

    Verify integrity of fragment manager before executePendingTransa… (#377)

--- android/src/main/java/com/swmansion/rnscreens/ScreenContainer.java
+++ android/src/main/java/com/swmansion/rnscreens/ScreenContainer.java
@@ -221,7 +221,9 @@ public class ScreenContainer<T extends ScreenFragment> extends ViewGroup {
     // if there are pending transactions and this view is about to get detached we need to perform
     // them here as otherwise fragment manager will crash because it won't be able to find container
     // view.
-    mFragmentManager.executePendingTransactions();
+    if (mFragmentManager != null && !mFragmentManager.isDestroyed()) {
+      mFragmentManager.executePendingTransactions();
+    }
     super.onDetachedFromWindow();
     mIsAttached = false;
   }
