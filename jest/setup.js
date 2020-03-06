ae64b7b6169568eab3251a0390399e405b93e274
Mike Rogers <mike.rogem002@gmail.com>
2020-03-06 10:24:43 +0000

    Correcting spelling 'Supress' > 'Suppress' (#7731)

--- jest/setup.js
+++ jest/setup.js
@@ -1,7 +1,7 @@
 const error = console.error;
 
 console.error = (...args) =>
-  // Supress error messages regarding error boundary in tests
+  // Suppress error messages regarding error boundary in tests
   /(Consider adding an error boundary to your tree to customize error handling behavior|React will try to recreate this component tree from scratch using the error boundary you provided|Error boundaries should implement getDerivedStateFromError)/m.test(
     args[0]
   )
