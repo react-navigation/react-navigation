ac7f972e922a82cd32d943356941d100b68bd8b0
Rajendran Nadar <raajnadar@gmail.com>
2020-03-30 17:21:32 +0530

    feat: add swipeEnabled option to disable swipe gesture in drawer (#7834)

--- packages/drawer/src/views/DrawerView.tsx
+++ packages/drawer/src/views/DrawerView.tsx
@@ -200,7 +200,7 @@ export default function DrawerView({
   };
 
   const activeKey = state.routes[state.index].key;
-  const { gestureEnabled } = descriptors[activeKey].options;
+  const { gestureEnabled, swipeEnabled } = descriptors[activeKey].options;
 
   return (
     <GestureHandlerWrapper style={styles.content}>
@@ -210,6 +210,7 @@ export default function DrawerView({
             <Drawer
               open={isDrawerOpen}
               gestureEnabled={gestureEnabled}
+              swipeEnabled={swipeEnabled}
               onOpen={handleDrawerOpen}
               onClose={handleDrawerClose}
               onGestureRef={(ref) => {
