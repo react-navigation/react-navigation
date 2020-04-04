19fc2237445fb2e67d4086ef5f440cd5a60194f3
raajnadar <raajnadar@gmail.com>
2020-04-05 00:27:46 +0530

    fix: separate out type import and export

--- packages/routers/src/index.tsx
+++ packages/routers/src/index.tsx
@@ -1,34 +1,49 @@
 import * as CommonActions from './CommonActions';
 
-export { CommonActions };
-
-export { default as BaseRouter } from './BaseRouter';
-
-export {
-  default as StackRouter,
-  StackActions,
+import type {
   StackActionHelpers,
   StackActionType,
   StackRouterOptions,
   StackNavigationState,
 } from './StackRouter';
 
-export {
-  default as TabRouter,
-  TabActions,
+import type {
   TabActionHelpers,
   TabActionType,
   TabRouterOptions,
   TabNavigationState,
 } from './TabRouter';
 
-export {
-  default as DrawerRouter,
-  DrawerActions,
+import type {
   DrawerActionHelpers,
   DrawerActionType,
   DrawerRouterOptions,
   DrawerNavigationState,
 } from './DrawerRouter';
+
+export type {
+  StackActionHelpers,
+  StackActionType,
+  StackRouterOptions,
+  StackNavigationState,
+  TabActionHelpers,
+  TabActionType,
+  TabRouterOptions,
+  TabNavigationState,
+  DrawerActionHelpers,
+  DrawerActionType,
+  DrawerRouterOptions,
+  DrawerNavigationState,
+};
+
+export { CommonActions };
+
+export { default as BaseRouter } from './BaseRouter';
+
+export { default as StackRouter, StackActions } from './StackRouter';
+
+export { default as TabRouter, TabActions } from './TabRouter';
+
+export { default as DrawerRouter, DrawerActions } from './DrawerRouter';
 
 export * from './types';
