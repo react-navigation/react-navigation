92b6a0c8a40aca446af05718f9782d013511413c
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-04-07 12:38:14 +0200

    fix: separate normal and type exports

--- packages/react-native-tab-view/src/index.tsx
+++ packages/react-native-tab-view/src/index.tsx
@@ -1,18 +1,13 @@
-import * as tv from './TabView';
-import * as tb from './TabBar';
-import * as tbi from './TabBarIndicator';
-import * as types from './types';
-
-// help babel to transpile the types correctly
-export type TabViewProps<T extends types.Route> = tv.Props<T>;
-export type TabBarProps<T extends types.Route> = tb.Props<T>;
-export type TabBarIndicatorProps<T extends types.Route> = tbi.Props<T>;
-export type Route = types.Route;
-export type NavigationState<T extends types.Route> = types.NavigationState<T>;
-export type SceneRendererProps = types.SceneRendererProps;
+export { default as TabBar } from './TabBar';
+export type { Props as TabBarProps } from './TabBar';
 
 export { default as TabView } from './TabView';
-export { default as TabBar } from './TabBar';
+export type { Props as TabViewProps } from './TabView';
+
 export { default as TabBarIndicator } from './TabBarIndicator';
+export type { Props as TabBarIndicatorProps } from './TabBarIndicator';
+
 export { default as SceneMap } from './SceneMap';
 export { default as ScrollPager } from './ScrollPager';
+
+export type { Route, NavigationState, SceneRendererProps } from './types';
