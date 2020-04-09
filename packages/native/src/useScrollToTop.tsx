73f72c98b3d53716a52a8b63be3ad78f3b9fae33
AntoineDoubovetzky <antoined@bam.tech>
2020-04-09 23:40:07 +0200

    refactor(usescrolltotop): reuse useTabPress

--- packages/native/src/useScrollToTop.tsx
+++ packages/native/src/useScrollToTop.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { useNavigation, useRoute, EventArg } from '@react-navigation/core';
+import { useNavigation, useRoute } from '@react-navigation/core';
+import useTabPress from './useTabPress';
 
 type ScrollOptions = { y?: number; animated?: boolean };
 
@@ -48,54 +49,28 @@ export default function useScrollToTop(
   const navigation = useNavigation();
   const route = useRoute();
 
-  React.useEffect(() => {
-    let current = navigation;
+  useTabPress(({ tabPressEvent, currentNavigation }) => {
+    // We should scroll to top only when the screen is focused
+    const isFocused = navigation.isFocused();
 
-    // The screen might be inside another navigator such as stack nested in tabs
-    // We need to find the closest tab navigator and add the listener there
-    while (current && current.dangerouslyGetState().type !== 'tab') {
-      current = current.dangerouslyGetParent();
-    }
+    // In a nested stack navigator, tab press resets the stack to first screen
+    // So we should scroll to top only when we are on first screen
+    const isFirst =
+      navigation === currentNavigation ||
+      navigation.dangerouslyGetState().routes[0].key === route.key;
 
-    if (!current) {
-      return;
-    }
+    const scrollable = getScrollableNode(ref) as ScrollableWrapper;
 
-    const unsubscribe = current.addListener(
-      // We don't wanna import tab types here to avoid extra deps
-      // in addition, there are multiple tab implementations
-      // @ts-ignore
-      'tabPress',
-      (e: EventArg<'tabPress', true>) => {
-        // We should scroll to top only when the screen is focused
-        const isFocused = navigation.isFocused();
-
-        // In a nested stack navigator, tab press resets the stack to first screen
-        // So we should scroll to top only when we are on first screen
-        const isFirst =
-          navigation === current ||
-          navigation.dangerouslyGetState().routes[0].key === route.key;
-
-        // Run the operation in the next frame so we're sure all listeners have been run
-        // This is necessary to know if preventDefault() has been called
-        requestAnimationFrame(() => {
-          const scrollable = getScrollableNode(ref) as ScrollableWrapper;
-
-          if (isFocused && isFirst && scrollable && !e.defaultPrevented) {
-            if ('scrollToTop' in scrollable) {
-              scrollable.scrollToTop();
-            } else if ('scrollTo' in scrollable) {
-              scrollable.scrollTo({ y: 0, animated: true });
-            } else if ('scrollToOffset' in scrollable) {
-              scrollable.scrollToOffset({ offset: 0, animated: true });
-            } else if ('scrollResponderScrollTo' in scrollable) {
-              scrollable.scrollResponderScrollTo({ y: 0, animated: true });
-            }
-          }
-        });
+    if (isFocused && isFirst && scrollable && !tabPressEvent.defaultPrevented) {
+      if ('scrollToTop' in scrollable) {
+        scrollable.scrollToTop();
+      } else if ('scrollTo' in scrollable) {
+        scrollable.scrollTo({ y: 0, animated: true });
+      } else if ('scrollToOffset' in scrollable) {
+        scrollable.scrollToOffset({ offset: 0, animated: true });
+      } else if ('scrollResponderScrollTo' in scrollable) {
+        scrollable.scrollResponderScrollTo({ y: 0, animated: true });
       }
-    );
-
-    return unsubscribe;
-  }, [navigation, ref, route.key]);
+    }
+  });
 }
