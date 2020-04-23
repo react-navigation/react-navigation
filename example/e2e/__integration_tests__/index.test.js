83692b5fda0ecc6b8e32e16a4fc3401f1636e923
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-04-23 22:46:28 +0200

    test: configure playwright for e2e tests

--- example/e2e/__integration_tests__/index.test.js
+++
@@ -1,9 +0,0 @@
-import { by, element, expect, device } from 'detox';
-
-beforeEach(async () => {
-  await device.reloadReactNative();
-});
-
-it('has dark theme toggle', async () => {
-  await expect(element(by.text('Dark theme'))).toBeVisible();
-});
