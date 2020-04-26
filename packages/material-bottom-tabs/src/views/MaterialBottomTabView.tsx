38020de80bbedaadae390d85e235d19563c8791a
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-04-27 00:54:35 +0200

    refactor: simplify API for useLinkBuilder

--- packages/material-bottom-tabs/src/views/MaterialBottomTabView.tsx
+++ packages/material-bottom-tabs/src/views/MaterialBottomTabView.tsx
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 import { BottomNavigation, DefaultTheme, DarkTheme } from 'react-native-paper';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {
+  NavigationHelpersContext,
   Route,
   TabNavigationState,
   TabActions,
@@ -45,66 +46,68 @@ export default function MaterialBottomTabView({
   }, [colors, dark]);
 
   return (
-    <BottomNavigation
-      {...rest}
-      theme={theme}
-      navigationState={state}
-      onIndexChange={(index: number) =>
-        navigation.dispatch({
-          ...TabActions.jumpTo(state.routes[index].name),
-          target: state.key,
-        })
-      }
-      renderScene={({ route }) => descriptors[route.key].render()}
-      renderIcon={({ route, focused, color }) => {
-        const { options } = descriptors[route.key];
-
-        if (typeof options.tabBarIcon === 'string') {
-          return (
-            <MaterialCommunityIcons
-              name={options.tabBarIcon}
-              color={color}
-              size={24}
-              style={styles.icon}
-              importantForAccessibility="no-hide-descendants"
-              accessibilityElementsHidden
-            />
-          );
+    <NavigationHelpersContext.Provider value={navigation}>
+      <BottomNavigation
+        {...rest}
+        theme={theme}
+        navigationState={state}
+        onIndexChange={(index: number) =>
+          navigation.dispatch({
+            ...TabActions.jumpTo(state.routes[index].name),
+            target: state.key,
+          })
         }
+        renderScene={({ route }) => descriptors[route.key].render()}
+        renderIcon={({ route, focused, color }) => {
+          const { options } = descriptors[route.key];
 
-        if (typeof options.tabBarIcon === 'function') {
-          return options.tabBarIcon({ focused, color });
+          if (typeof options.tabBarIcon === 'string') {
+            return (
+              <MaterialCommunityIcons
+                name={options.tabBarIcon}
+                color={color}
+                size={24}
+                style={styles.icon}
+                importantForAccessibility="no-hide-descendants"
+                accessibilityElementsHidden
+              />
+            );
+          }
+
+          if (typeof options.tabBarIcon === 'function') {
+            return options.tabBarIcon({ focused, color });
+          }
+
+          return null;
+        }}
+        getLabelText={({ route }: Scene) => {
+          const { options } = descriptors[route.key];
+
+          return options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : (route as Route<string>).name;
+        }}
+        getColor={({ route }) => descriptors[route.key].options.tabBarColor}
+        getBadge={({ route }) => descriptors[route.key].options.tabBarBadge}
+        getAccessibilityLabel={({ route }) =>
+          descriptors[route.key].options.tabBarAccessibilityLabel
         }
+        getTestID={({ route }) => descriptors[route.key].options.tabBarTestID}
+        onTabPress={({ route, preventDefault }) => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
 
-        return null;
-      }}
-      getLabelText={({ route }: Scene) => {
-        const { options } = descriptors[route.key];
-
-        return options.tabBarLabel !== undefined
-          ? options.tabBarLabel
-          : options.title !== undefined
-          ? options.title
-          : (route as Route<string>).name;
-      }}
-      getColor={({ route }) => descriptors[route.key].options.tabBarColor}
-      getBadge={({ route }) => descriptors[route.key].options.tabBarBadge}
-      getAccessibilityLabel={({ route }) =>
-        descriptors[route.key].options.tabBarAccessibilityLabel
-      }
-      getTestID={({ route }) => descriptors[route.key].options.tabBarTestID}
-      onTabPress={({ route, preventDefault }) => {
-        const event = navigation.emit({
-          type: 'tabPress',
-          target: route.key,
-          canPreventDefault: true,
-        });
-
-        if (event.defaultPrevented) {
-          preventDefault();
-        }
-      }}
-    />
+          if (event.defaultPrevented) {
+            preventDefault();
+          }
+        }}
+      />
+    </NavigationHelpersContext.Provider>
   );
 }
 
