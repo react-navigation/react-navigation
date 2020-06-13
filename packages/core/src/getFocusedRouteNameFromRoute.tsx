064c27d79267374608f4879f7f7484d8a2c2c9d2
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-06-13 15:03:20 +0200

    feat: add helper to get focused route name from nested state

---
+++ packages/core/src/getFocusedRouteNameFromRoute.tsx
@@ -0,0 +1,29 @@
+import {
+  Route,
+  PartialState,
+  NavigationState,
+} from '@react-navigation/routers';
+
+export default function getFocusedRouteNameFromRoute(
+  route: Partial<Route<string>> & { state?: PartialState<NavigationState> }
+): string | undefined {
+  const state = route.state;
+  const params = route.params as { screen?: unknown } | undefined;
+
+  const routeName = state
+    ? // Get the currently active route name in the nested navigator
+      state.routes[
+        // If we have a partial state without index, for tab/drawer, first screen will be focused one, and last for stack
+        // The type property will only exist for rehydrated state and not for state from deep link
+        state.index ??
+          (typeof state.type === 'string' && state.type !== 'stack'
+            ? 0
+            : state.routes.length - 1)
+      ].name
+    : // If state doesn't exist, we need to default to `screen` param if available
+    typeof params?.screen === 'string'
+    ? params.screen
+    : undefined;
+
+  return routeName;
+}
