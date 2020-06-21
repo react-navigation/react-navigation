e89d853dffa083f9b7584f35954cbd3fd6a76bfb
michaljarnot <m.jarnot@yahoo.com>
2020-06-22 01:26:32 +0200

    fix: linting issues

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,6 @@
-const banner = 'https://github.com/react-navigation/react-navigation \u001b[33mwas transformed into monorepo. ' +
+const banner =
+  'https://github.com/react-navigation/react-navigation \u001b[33mwas transformed into monorepo. ' +
   'If you want to use the latest version install @react-navigation/native. \n\n You can find more information in our ' +
   'getting started guide: https://reactnavigation.org/docs/getting-started';
 
-console.log(banner)
+console.log(banner);
