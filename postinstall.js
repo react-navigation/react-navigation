ca8c33ff7dbacffa6db4dd32a29ff7e3360e9c33
michaljarnot <m.jarnot@yahoo.com>
2020-06-22 01:06:38 +0200

    chore: inform users about @react-navigation/native

---
+++ postinstall.js
@@ -0,0 +1,5 @@
+const banner = 'https://github.com/react-navigation/react-navigation \u001b[33mwas transformed into monorepo. ' +
+  'If you want to use the latest version install @react-navigation/native. \n\n You can find more information in our ' +
+  'getting started guide: https://reactnavigation.org/docs/getting-started';
+
+console.log(banner)
