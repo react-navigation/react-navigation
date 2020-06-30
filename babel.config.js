f10abeb7b9144ce084be27ca85be5ae1701b969a
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-06-30 16:14:52 +0200

    test: add basic unit tests for all navigators

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: 'usage',
-        corejs: 3,
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/transform-flow-strip-types',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
 };
---
+++ packages/drawer/src/__tests__/index.test.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { render, fireEvent } from 'react-native-testing-library';
+import { NavigationContainer, ParamListBase } from '@react-navigation/native';
+import { createDrawerNavigator, DrawerScreenProps } from '../index';
+
+it('renders a drawer navigator with screens', async () => {
+  const Test = ({ route, navigation }: DrawerScreenProps<ParamListBase>) => (
+    <View>
+      <Text>Screen {route.name}</Text>
+      <Button onPress={() => navigation.navigate('A')} title="Go to A" />
+      <Button onPress={() => navigation.navigate('B')} title="Go to B" />
+    </View>
+  );
+
+  const Drawer = createDrawerNavigator();
+
+  const { findByText, queryByText } = render(
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="A" component={Test} />
+        <Drawer.Screen name="B" component={Test} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+
+  expect(queryByText('Screen A')).not.toBeNull();
+  expect(queryByText('Screen B')).toBeNull();
+
+  fireEvent(await findByText('Go to B'), 'press');
+
+  expect(queryByText('Screen B')).not.toBeNull();
+});
