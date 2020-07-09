a255e350f9a54c6d8e410167c9c8661e70b23779
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-07-09 15:47:27 +0200

    fix: fix options event being emitted incorrectly (#8559)

--- packages/core/src/__tests__/BaseNavigationContainer.test.tsx
+++ packages/core/src/__tests__/BaseNavigationContainer.test.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { act, render } from 'react-native-testing-library';
-import type {
+import {
   DefaultRouterOptions,
   NavigationState,
   Router,
+  StackRouter,
+  TabRouter,
 } from '@react-navigation/routers';
 import BaseNavigationContainer from '../BaseNavigationContainer';
 import NavigationStateContext from '../NavigationStateContext';
@@ -430,9 +432,9 @@ it('emits state events when the state changes', () => {
   });
 });
 
-it('emits state events when options change', () => {
+it('emits option events when options change with tab router', () => {
   const TestNavigator = (props: any) => {
-    const { state, descriptors } = useNavigationBuilder(MockRouter, props);
+    const { state, descriptors } = useNavigationBuilder(TabRouter, props);
 
     return (
       <React.Fragment>
@@ -455,7 +457,10 @@ it('emits state events when options change', () => {
         <Screen name="baz" options={{ v: 3 }}>
           {() => (
             <TestNavigator>
-              <Screen name="foo" options={{ g: 5 }}>
+              <Screen name="qux" options={{ g: 5 }}>
+                {() => null}
+              </Screen>
+              <Screen name="quxx" options={{ h: 9 }}>
                 {() => null}
               </Screen>
             </TestNavigator>
@@ -474,19 +479,105 @@ it('emits state events when options change', () => {
     ref.current?.navigate('bar');
   });
 
-  expect(listener.mock.calls[0][0].data.options).toEqual({
-    y: 2,
-  });
+  expect(listener).toBeCalledTimes(1);
+  expect(listener.mock.calls[0][0].data.options).toEqual({ y: 2 });
+  expect(ref.current?.getCurrentOptions()).toEqual({ y: 2 });
 
   ref.current?.removeListener('options', listener);
+
   const listener2 = jest.fn();
+
   ref.current?.addListener('options', listener2);
 
   act(() => {
     ref.current?.navigate('baz');
   });
 
+  expect(listener2).toBeCalledTimes(1);
   expect(listener2.mock.calls[0][0].data.options).toEqual({ g: 5 });
+  expect(ref.current?.getCurrentOptions()).toEqual({ g: 5 });
+
+  act(() => {
+    ref.current?.navigate('quxx');
+  });
+
+  expect(listener2).toBeCalledTimes(2);
+  expect(listener2.mock.calls[1][0].data.options).toEqual({ h: 9 });
+  expect(ref.current?.getCurrentOptions()).toEqual({ h: 9 });
+});
+
+it('emits option events when options change with stack router', () => {
+  const TestNavigator = (props: any) => {
+    const { state, descriptors } = useNavigationBuilder(StackRouter, props);
+
+    return (
+      <React.Fragment>
+        {state.routes.map((route) => descriptors[route.key].render())}
+      </React.Fragment>
+    );
+  };
+
+  const ref = React.createRef<NavigationContainerRef>();
+
+  const element = (
+    <BaseNavigationContainer ref={ref}>
+      <TestNavigator>
+        <Screen name="foo" options={{ x: 1 }}>
+          {() => null}
+        </Screen>
+        <Screen name="bar" options={{ y: 2 }}>
+          {() => null}
+        </Screen>
+        <Screen name="baz" options={{ v: 3 }}>
+          {() => (
+            <TestNavigator>
+              <Screen name="qux" options={{ g: 5 }}>
+                {() => null}
+              </Screen>
+              <Screen name="quxx" options={{ h: 9 }}>
+                {() => null}
+              </Screen>
+            </TestNavigator>
+          )}
+        </Screen>
+      </TestNavigator>
+    </BaseNavigationContainer>
+  );
+
+  const listener = jest.fn();
+
+  render(element).update(element);
+  ref.current?.addListener('options', listener);
+
+  act(() => {
+    ref.current?.navigate('bar');
+  });
+
+  expect(listener).toBeCalledTimes(1);
+  expect(listener.mock.calls[0][0].data.options).toEqual({ y: 2 });
+  expect(ref.current?.getCurrentOptions()).toEqual({ y: 2 });
+
+  ref.current?.removeListener('options', listener);
+
+  const listener2 = jest.fn();
+
+  ref.current?.addListener('options', listener2);
+
+  act(() => {
+    ref.current?.navigate('baz');
+  });
+
+  expect(listener2).toBeCalledTimes(1);
+  expect(listener2.mock.calls[0][0].data.options).toEqual({ g: 5 });
+  expect(ref.current?.getCurrentOptions()).toEqual({ g: 5 });
+
+  act(() => {
+    ref.current?.navigate('quxx');
+  });
+
+  expect(listener2).toBeCalledTimes(2);
+  expect(listener2.mock.calls[1][0].data.options).toEqual({ h: 9 });
+  expect(ref.current?.getCurrentOptions()).toEqual({ h: 9 });
 });
 
 it('throws if there is no navigator rendered', () => {
