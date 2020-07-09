78f915e803e49e4cbbce07571d9fdfd2238fe5fa
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-07-09 14:34:51 +0200

    fix: refactor emitting options

--- packages/core/src/__tests__/BaseNavigationContainer.test.tsx
+++ packages/core/src/__tests__/BaseNavigationContainer.test.tsx
@@ -480,9 +480,8 @@ it('emits option events when options change with tab router', () => {
   });
 
   expect(listener).toBeCalledTimes(1);
-  expect(listener.mock.calls[0][0].data.options).toEqual({
-    y: 2,
-  });
+  expect(listener.mock.calls[0][0].data.options).toEqual({ y: 2 });
+  expect(ref.current?.getCurrentOptions()).toEqual({ y: 2 });
 
   ref.current?.removeListener('options', listener);
 
@@ -496,6 +495,7 @@ it('emits option events when options change with tab router', () => {
 
   expect(listener2).toBeCalledTimes(1);
   expect(listener2.mock.calls[0][0].data.options).toEqual({ g: 5 });
+  expect(ref.current?.getCurrentOptions()).toEqual({ g: 5 });
 
   act(() => {
     ref.current?.navigate('quxx');
@@ -503,6 +503,7 @@ it('emits option events when options change with tab router', () => {
 
   expect(listener2).toBeCalledTimes(2);
   expect(listener2.mock.calls[1][0].data.options).toEqual({ h: 9 });
+  expect(ref.current?.getCurrentOptions()).toEqual({ h: 9 });
 });
 
 it('emits option events when options change with stack router', () => {
@@ -553,9 +554,8 @@ it('emits option events when options change with stack router', () => {
   });
 
   expect(listener).toBeCalledTimes(1);
-  expect(listener.mock.calls[0][0].data.options).toEqual({
-    y: 2,
-  });
+  expect(listener.mock.calls[0][0].data.options).toEqual({ y: 2 });
+  expect(ref.current?.getCurrentOptions()).toEqual({ y: 2 });
 
   ref.current?.removeListener('options', listener);
 
@@ -569,6 +569,7 @@ it('emits option events when options change with stack router', () => {
 
   expect(listener2).toBeCalledTimes(1);
   expect(listener2.mock.calls[0][0].data.options).toEqual({ g: 5 });
+  expect(ref.current?.getCurrentOptions()).toEqual({ g: 5 });
 
   act(() => {
     ref.current?.navigate('quxx');
@@ -576,6 +577,7 @@ it('emits option events when options change with stack router', () => {
 
   expect(listener2).toBeCalledTimes(2);
   expect(listener2.mock.calls[1][0].data.options).toEqual({ h: 9 });
+  expect(ref.current?.getCurrentOptions()).toEqual({ h: 9 });
 });
 
 it('throws if there is no navigator rendered', () => {
