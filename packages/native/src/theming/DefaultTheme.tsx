5fc88354e7bf07ee5053708fd3b60e1f091b6a43
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-07-10 21:50:04 +0200

    fix: tweak border color to match iOS default

--- packages/native/src/theming/DefaultTheme.tsx
+++ packages/native/src/theming/DefaultTheme.tsx
@@ -7,7 +7,7 @@ const DefaultTheme: Theme = {
     background: 'rgb(242, 242, 242)',
     card: 'rgb(255, 255, 255)',
     text: 'rgb(28, 28, 30)',
-    border: 'rgb(224, 224, 224)',
+    border: 'rgb(216, 216, 216)',
     notification: 'rgb(255, 59, 48)',
   },
 };
