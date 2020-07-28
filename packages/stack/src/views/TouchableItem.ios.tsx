30929e4128e4c6d5fc02bed10e1ba489986181e2
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-07-29 00:33:40 +0200

    fix: fix TouchableItem opacity on press on iOS

--- packages/stack/src/views/TouchableItem.ios.tsx
+++ packages/stack/src/views/TouchableItem.ios.tsx
@@ -10,27 +10,26 @@ type Props = React.ComponentProps<typeof BaseButton> & {
 
 const useNativeDriver = Platform.OS !== 'web';
 
-export default class BorderlessButton extends React.Component<Props> {
+export default class TouchableItem extends React.Component<Props> {
   static defaultProps = {
     activeOpacity: 0.3,
     borderless: true,
+    enabled: true,
   };
 
   private opacity = new Animated.Value(1);
 
   private handleActiveStateChange = (active: boolean) => {
-    if (Platform.OS !== 'android') {
-      Animated.spring(this.opacity, {
-        stiffness: 1000,
-        damping: 500,
-        mass: 3,
-        overshootClamping: true,
-        restDisplacementThreshold: 0.01,
-        restSpeedThreshold: 0.01,
-        toValue: active ? this.props.activeOpacity : 1,
-        useNativeDriver,
-      }).start();
-    }
+    Animated.spring(this.opacity, {
+      stiffness: 1000,
+      damping: 500,
+      mass: 3,
+      overshootClamping: true,
+      restDisplacementThreshold: 0.01,
+      restSpeedThreshold: 0.01,
+      toValue: active ? this.props.activeOpacity : 1,
+      useNativeDriver,
+    }).start();
 
     this.props.onActiveStateChange?.(active);
   };
@@ -43,10 +42,7 @@ export default class BorderlessButton extends React.Component<Props> {
       <AnimatedBaseButton
         {...rest}
         onActiveStateChange={this.handleActiveStateChange}
-        style={[
-          style,
-          Platform.OS === 'ios' && enabled && { opacity: this.opacity },
-        ]}
+        style={[style, enabled && { opacity: this.opacity }]}
       >
         {children}
       </AnimatedBaseButton>
