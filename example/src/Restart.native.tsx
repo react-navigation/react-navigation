1cb71c6c9a46be53de46a2b880484e9baa5c5aab
Brent Vatne <brentvatne@gmail.com>
2020-07-30 17:00:55 -0700

    chore: update example to SDK 38 and bump related dependencies (#8654)

--- example/src/Restart.native.tsx
+++ example/src/Restart.native.tsx
@@ -1,11 +1,5 @@
-import { Updates } from 'expo';
-import RNRestart from 'react-native-restart';
+import * as Updates from 'expo-updates';
 
 export function restartApp() {
-  // @ts-expect-error: Expo doesn't exist in global definitions
-  if (global.Expo) {
-    Updates.reloadFromCache();
-  } else {
-    RNRestart.Restart();
-  }
+  Updates.reloadAsync();
 }
