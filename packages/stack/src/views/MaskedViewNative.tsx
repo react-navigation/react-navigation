8773dcb80f4815d63c8a63cbf996c6a1ff7c5a78
Jesse Katsumata <niconico.clarinet@gmail.com>
2020-11-12 14:21:11 +0900

    chore: replace community maskedview (#9058)
    replaced `@react-native-community/masked-view` with `@react-native-masked-view/masked-view` due to namespace change.

--- packages/stack/src/views/MaskedViewNative.tsx
+++ packages/stack/src/views/MaskedViewNative.tsx
@@ -4,7 +4,7 @@
 import * as React from 'react';
 import { UIManager } from 'react-native';
 
-type MaskedViewType = typeof import('@react-native-community/masked-view').default;
+type MaskedViewType = typeof import('@react-native-masked-view/masked-view').default;
 
 type Props = React.ComponentProps<MaskedViewType> & {
   children: React.ReactElement;
@@ -15,7 +15,7 @@ let RNCMaskedView: MaskedViewType | undefined;
 try {
   // Add try/catch to support usage even if it's not installed, since it's optional.
   // Newer versions of Metro will handle it properly.
-  RNCMaskedView = require('@react-native-community/masked-view').default;
+  RNCMaskedView = require('@react-native-masked-view/masked-view').default;
 } catch (e) {
   // Ignore
 }
