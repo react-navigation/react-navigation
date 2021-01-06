65b3cc4fe63dae85534ea676e7b1f94bc2d56071
Dulmandakh <dulmandakh@gmail.com>
2021-01-06 17:28:32 +0800

    set displayName for LinkingContext

--- packages/native/src/LinkingContext.tsx
+++ packages/native/src/LinkingContext.tsx
@@ -4,5 +4,6 @@ import type { LinkingOptions } from './types';
 const LinkingContext = React.createContext<{
   options: LinkingOptions | undefined;
 }>({ options: undefined });
+LinkingContext.displayName = 'LinkingContext';
 
 export default LinkingContext;
