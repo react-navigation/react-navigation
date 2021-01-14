09c6960d6f1261bd5a70529cfaefd28c5c1eb981
Satyajit Sahoo <satyajit.happy@gmail.com>
2021-01-14 11:57:54 +0100

    Update packages/native/src/theming/ThemeContext.tsx

--- packages/native/src/theming/ThemeContext.tsx
+++ packages/native/src/theming/ThemeContext.tsx
@@ -3,6 +3,7 @@ import DefaultTheme from './DefaultTheme';
 import type { Theme } from '../types';
 
 const ThemeContext = React.createContext<Theme>(DefaultTheme);
+
 ThemeContext.displayName = 'ThemeContext';
 
 export default ThemeContext;
