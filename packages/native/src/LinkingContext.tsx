7199a90392d3561fef6610e91364d269b551e1b3
Satyajit Sahoo <satyajit.happy@gmail.com>
2021-01-14 11:57:19 +0100

    Update packages/native/src/LinkingContext.tsx

--- packages/native/src/LinkingContext.tsx
+++ packages/native/src/LinkingContext.tsx
@@ -4,6 +4,7 @@ import type { LinkingOptions } from './types';
 const LinkingContext = React.createContext<{
   options: LinkingOptions | undefined;
 }>({ options: undefined });
+
 LinkingContext.displayName = 'LinkingContext';
 
 export default LinkingContext;
