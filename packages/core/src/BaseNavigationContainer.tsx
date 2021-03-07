7a36d8a44334bfd7b96e3709b43a07d658132b6a
Max Eisen <44880676+maxeisen@users.noreply.github.com>
2021-03-07 16:37:37 -0500

    Fix "explicitly" spelling error

--- packages/core/src/BaseNavigationContainer.tsx
+++ packages/core/src/BaseNavigationContainer.tsx
@@ -106,7 +106,7 @@ const BaseNavigationContainer = React.forwardRef(
 
     if (!parent.isDefault && !independent) {
       throw new Error(
-        "Looks like you have nested a 'NavigationContainer' inside another. Normally you need only one container at the root of the app, so this was probably an error. If this was intentional, pass 'independent={true}' explicitely. Note that this will make the child navigators disconnected from the parent and you won't be able to navigate between them."
+        "Looks like you have nested a 'NavigationContainer' inside another. Normally you need only one container at the root of the app, so this was probably an error. If this was intentional, pass 'independent={true}' explicitly. Note that this will make the child navigators disconnected from the parent and you won't be able to navigate between them."
       );
     }
 
