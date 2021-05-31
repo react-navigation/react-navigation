4615abbb806b3bcde5a9a8814955a5f0f3d2277c
Satyajit Sahoo <satyajit.happy@gmail.com>
2021-05-31 20:03:29 +0200

    chore: fix running the example app on web

--- packages/native-stack/src/views/FontProcessor.tsx
+++ packages/native-stack/src/views/FontProcessor.tsx
@@ -1,13 +1,5 @@
-// @ts-ignore: No declaration available
-import ReactNativeStyleAttributes from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
-
 export function processFonts(
-  fontFamilies: (string | undefined)[]
+  _: (string | undefined)[]
 ): (string | undefined)[] {
-  // @ts-ignore: React Native types are incorrect here and don't consider fontFamily a style value
-  const fontFamilyProcessor = ReactNativeStyleAttributes.fontFamily?.process;
-  if (typeof fontFamilyProcessor === 'function') {
-    return fontFamilies.map(fontFamilyProcessor);
-  }
-  return fontFamilies;
+  throw new Error('Not supported on Web');
 }
