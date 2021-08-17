df0d6e2f9f182e3e4f704b63741f36eeac4e3f68
Emad Aldeen Hajjar <emadaldeen.hajjar@gmail.com>
2021-08-17 10:24:25 -0300

    feat(elements): add style to SafeAreaProviderCompat props (#9793)

--- packages/elements/src/SafeAreaProviderCompat.tsx
+++ packages/elements/src/SafeAreaProviderCompat.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
-import { Dimensions, Platform } from 'react-native';
+import {
+  Dimensions,
+  Platform,
+  StyleProp,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from 'react-native';
 import {
   initialWindowMetrics,
   SafeAreaInsetsContext,
@@ -8,6 +15,7 @@ import {
 
 type Props = {
   children: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
 };
 
 const { width = 0, height = 0 } = Dimensions.get('window');
@@ -23,7 +31,7 @@ const initialMetrics =
       }
     : initialWindowMetrics;
 
-export default function SafeAreaProviderCompat({ children }: Props) {
+export default function SafeAreaProviderCompat({ children, style }: Props) {
   return (
     <SafeAreaInsetsContext.Consumer>
       {(insets) => {
@@ -31,11 +39,11 @@ export default function SafeAreaProviderCompat({ children }: Props) {
           // If we already have insets, don't wrap the stack in another safe area provider
           // This avoids an issue with updates at the cost of potentially incorrect values
           // https://github.com/react-navigation/react-navigation/issues/174
-          return children;
+          return <View style={[styles.container, style]}>{children}</View>;
         }
 
         return (
-          <SafeAreaProvider initialMetrics={initialMetrics}>
+          <SafeAreaProvider initialMetrics={initialMetrics} style={style}>
             {children}
           </SafeAreaProvider>
         );
@@ -45,3 +53,9 @@ export default function SafeAreaProviderCompat({ children }: Props) {
 }
 
 SafeAreaProviderCompat.initialMetrics = initialMetrics;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
