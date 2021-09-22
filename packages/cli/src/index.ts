35604fe70fd7171d2b9930cc153f242284e5f1cb
react-one <dev@reactone.digital>
2021-09-22 19:29:11 +0100

    feat: add options to command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,6 @@
 import program from 'commander';
-import installPackage from '@Commands/addPackage';
-import getLogger from '@Utils/logger';
+import installPackage from './commands/addPackage';
+import getLogger from './utils/logger';
 
 const logger = getLogger();
 
@@ -13,6 +13,18 @@ program
   .command('add <package>')
   .alias('a')
   .description('install your required package')
-  .action(installPackage);
+  .action(installPackage)
+  .option('-i, --installer <installer>', 'Choose the installer to use manually')
+  .option('-V, --verbose', 'output in verbose mode')
+  .option('-d, --debug', 'output extra debugging')
+  .on('option:installer', function () {
+    process.env.installer = program.opts().installer;
+  })
+  .on('option:verbose', function () {
+    logger.setIsVerbose(program.opts().verbose);
+  })
+  .on('option:debug', function () {
+    logger.setIsDebug(program.opts().debug);
+  });
 
 program.parse(process.argv);
