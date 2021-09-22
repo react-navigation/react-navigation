aa9ab6577ff7ec8f55e19754fee9ea759f5bb74d
react-one <dev@reactone.digital>
2021-09-22 18:41:55 +0100

    refactor: isExpoADependency

---
+++ packages/cli/src/utils/isExpoADependency.ts
@@ -0,0 +1,14 @@
+/**
+ * Check for expo in package.json
+ */
+function isExpoADependency(packageJsonText: string): boolean {
+  const parsedContent = JSON.parse(packageJsonText);
+
+  if (parsedContent.dependencies?.hasOwnProperty('expo')) {
+    return true;
+  }
+
+  return false;
+}
+
+export default isExpoADependency;
