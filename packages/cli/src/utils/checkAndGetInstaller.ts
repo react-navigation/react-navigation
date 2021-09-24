4d889143b4800f064b4f5442da9512c05b9fa6e4
react-one <dev@reactone.digital>
2021-09-24 01:22:29 +0100

    fix: update checkAndGetInstaller to compute all data

--- packages/cli/src/utils/checkAndGetInstaller.ts
+++ packages/cli/src/utils/checkAndGetInstaller.ts
@@ -1,4 +1,4 @@
-import * as shell from 'shelljs';
+import shell from 'shelljs';
 import fs from 'fs';
 import path from 'path';
 import isExpoADependency from './isExpoADependency';
@@ -6,7 +6,7 @@ import isExpoADependency from './isExpoADependency';
 export interface ICheckAndGetInstallerReturn {
   installer: string | null;
   state: ICheckState;
-  rootDirectory: string | undefined;
+  rootDirectory: string | null;
 }
 
 export interface ICheckState {
@@ -36,8 +36,18 @@ function checkAndGetInstaller(dir: string): ICheckAndGetInstallerReturn {
       isYarnInstalled: false,
       isNpmInstalled: false,
     },
-    rootDirectory: undefined,
+    rootDirectory: null,
   };
+
+  // check if yarn command is installed
+  if (shell.which('yarn')) {
+    result.state.isYarnInstalled = true;
+  }
+
+  // check if yarn command is installed
+  if (shell.which('npm')) {
+    result.state.isNpmInstalled = true;
+  }
 
   /**
    * traversing
@@ -48,43 +58,33 @@ function checkAndGetInstaller(dir: string): ICheckAndGetInstallerReturn {
     if (fs.existsSync(packageJsonFile)) {
       const stat = fs.statSync(packageJsonFile);
       if (!stat.isDirectory()) {
-        result.rootDirectory = dir;
+        result.rootDirectory = dir || null;
         result.state.isPackageJsonFound = true;
         const packageJsonText = fs.readFileSync(packageJsonFile, 'utf8');
+
         // ---- expo is a dependency -----
         if (isExpoADependency(packageJsonText)) {
           result.state.isExpoFound = true;
           result.installer = userPickedInstaller || 'expo';
-          return result;
         }
         // ---- yarn lock -----
         if (fs.existsSync(path.resolve(dir, 'yarn.lock'))) {
           result.state.isYarnPackageLockFound = true;
-          result.installer = userPickedInstaller || 'yarn';
-          return result;
+          result.installer = result.installer || userPickedInstaller || 'yarn';
         }
         // ---- npm lock -----
         if (fs.existsSync(path.resolve(dir, 'package.lock'))) {
           result.state.isNpmPackageLockFound = true;
-          result.installer = userPickedInstaller || 'npm';
-          return result;
+          result.installer = result.installer || userPickedInstaller || 'npm';
         }
 
         // ---- no expo, no lock ----
-        // check if yarn command is installed
-        if (shell.which('yarn')) {
-          result.state.isYarnInstalled = true;
-        }
-
-        // check if yarn command is installed
-        if (shell.which('npm')) {
-          result.state.isNpmInstalled = true;
-        }
-
-        if (result.state.isYarnInstalled) {
-          result.installer = userPickedInstaller || 'yarn';
-        } else if (result.state.isNpmInstalled) {
-          result.installer = userPickedInstaller || 'npm';
+        if (!result.installer) {
+          if (result.state.isYarnInstalled) {
+            result.installer = userPickedInstaller || 'yarn';
+          } else if (result.state.isNpmInstalled) {
+            result.installer = userPickedInstaller || 'npm';
+          }
         }
 
         return result;
