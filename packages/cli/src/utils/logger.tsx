22c99b54281940b2456736042718ba30dbe7fdde
react-one <dev@reactone.digital>
2021-10-13 07:17:54 +0100

    style(cli): remove _ for private properties in logger.tsx

--- packages/cli/src/utils/logger.tsx
+++ packages/cli/src/utils/logger.tsx
@@ -1,58 +1,58 @@
 export class Logger {
-  private _isVerbose = false;
-  private _isDebug = false;
-  private _isLogEnabled = true;
+  private isVerbose = false;
+  private isDebug = false;
+  private isLogEnabled = true;
 
   public setIsVerbose(enable: boolean): this {
-    this._isVerbose = enable;
+    this.isVerbose = enable;
     return this;
   }
 
   public setIsDebug(enable: boolean): this {
-    this._isDebug = enable;
+    this.isDebug = enable;
     return this;
   }
 
   public enable(): this {
-    this._isLogEnabled = true;
+    this.isLogEnabled = true;
     return this;
   }
 
   public disable(): this {
-    this._isLogEnabled = false;
+    this.isLogEnabled = false;
     return this;
   }
 
   public log(msg: any): this {
-    if (this._isLogEnabled) {
+    if (this.isLogEnabled) {
       console.log(msg);
     }
     return this;
   }
 
   public warn(msg: any): this {
-    if (this._isLogEnabled) {
+    if (this.isLogEnabled) {
       console.warn(msg);
     }
     return this;
   }
 
   public error(msg: any): this {
-    if (this._isLogEnabled) {
+    if (this.isLogEnabled) {
       console.error(msg);
     }
     return this;
   }
 
   public verbose(msg: any): this {
-    if (this._isLogEnabled && (this._isVerbose || this._isDebug)) {
+    if (this.isLogEnabled && (this.isVerbose || this.isDebug)) {
       console.log(msg);
     }
     return this;
   }
 
   public debug(msg: any): this {
-    if (this._isLogEnabled && this._isDebug) {
+    if (this.isLogEnabled && this.isDebug) {
       console.log(msg);
     }
     return this;
