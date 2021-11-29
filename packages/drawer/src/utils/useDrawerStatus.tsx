fa1017e47655b3ee60392f0f19f9da91e3aa56ab
Rony <ronyrabak@gmail.com>
2021-11-29 07:27:26 +0000

    Fixed linting
    Fixed linting issue with imports sorting

--- packages/drawer/src/utils/useDrawerStatus.tsx
+++ packages/drawer/src/utils/useDrawerStatus.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
 import type { DrawerStatus } from '@react-navigation/native';
+import * as React from 'react';
 
 import DrawerStatusContext from './DrawerStatusContext';
 
