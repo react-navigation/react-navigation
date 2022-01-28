37d03ca484b1b9457f8d76d69a3763db113499b3
Sherif <sherif.mohamed@mcledger.co>
2022-01-28 23:30:41 +0200

    fix(dawer): fix drawer when changing drawertype value from front to be permanent to prevent white stretched block

--- packages/drawer/src/views/legacy/Drawer.tsx
+++ packages/drawer/src/views/legacy/Drawer.tsx
@@ -558,7 +558,7 @@ export default class DrawerView extends React.Component<DrawerProps> {
                     drawerType === 'permanent'
                       ? // Reanimated needs the property to be present, but it results in Browser bug
                         // https://bugs.chromium.org/p/chromium/issues/detail?id=20574
-                        undefined
+                        [{ translateX: 0 }]
                       : [{ translateX: contentTranslateX }],
                 },
               ]}
@@ -619,7 +619,7 @@ export default class DrawerView extends React.Component<DrawerProps> {
                     drawerType === 'permanent'
                       ? // Reanimated needs the property to be present, but it results in Browser bug
                         // https://bugs.chromium.org/p/chromium/issues/detail?id=20574
-                        undefined
+                        [{ translateX: 0 }]
                       : [{ translateX: drawerTranslateX }],
                   opacity: this.drawerOpacity,
                 },
