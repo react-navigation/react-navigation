5b5c96085c0c8b0e47ee13ac33d481f1be4999eb
Satyajit Sahoo <satyajit.happy@gmail.com>
2022-01-31 12:52:03 +0100

    chore: upgrade dependencies

--- packages/react-native-tab-view/example/metro.config.js
+++ packages/react-native-tab-view/example/metro.config.js
@@ -2,13 +2,16 @@
 
 const path = require('path');
 const fs = require('fs');
-const blacklist = require('metro-config/src/defaults/blacklist');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const { getDefaultConfig } = require('@expo/metro-config');
 const escape = require('escape-string-regexp');
 
 const root = path.resolve(__dirname, '..');
 const pak = JSON.parse(
   fs.readFileSync(path.join(root, 'package.json'), 'utf8')
 );
+
+const defaultConfig = getDefaultConfig(__dirname);
 
 const modules = [
   '@babel/runtime',
@@ -20,11 +23,15 @@ const modules = [
 ];
 
 module.exports = {
+  ...defaultConfig,
+
   projectRoot: __dirname,
   watchFolders: [root],
 
   resolver: {
-    blacklistRE: blacklist([
+    ...defaultConfig.resolver,
+
+    blacklistRE: exclusionList([
       new RegExp(`^${escape(path.join(root, 'node_modules'))}\\/.*$`),
     ]),
 
@@ -32,14 +39,5 @@ module.exports = {
       acc[name] = path.join(__dirname, 'node_modules', name);
       return acc;
     }, {}),
-  },
-
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
   },
 };
