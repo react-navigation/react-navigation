fdff9607c298fd0e35f982146f25fd21b3f1c020
Emin <eminsr1905@gmail.com>
2022-04-15 10:12:36 +0300

    change error to a suppressible warning

--- packages/bottom-tabs/src/utils/useBottomTabBarHeight.tsx
+++ packages/bottom-tabs/src/utils/useBottomTabBarHeight.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
+import warnOnce from 'warn-once';
 
 import BottomTabBarHeightContext from './BottomTabBarHeightContext';
 
-export default function useFloatingBottomTabBarHeight() {
+export default function useFloatingBottomTabBarHeight(ignore_warning = false) {
   const height = React.useContext(BottomTabBarHeightContext);
 
   if (height === undefined) {
-    throw new Error(
-      "Couldn't find the bottom tab bar height. Are you inside a screen in Bottom Tab Navigator?"
-    );
+    if(!ignore_warning)
+      warnOnce(
+        true,
+        "Couldn't find the bottom tab bar height (will return 0). Are you inside a screen in Bottom Tab Navigator?\n\nYou can ignore this warning by passing true to the useBottomTabBarHeight hook.\nuseBottomTabBarHeight(true)\n"
+      );
+    return 0;
   }
 
   return height;
