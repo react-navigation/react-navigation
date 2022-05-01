a9bd2b5f2b2c777f4633f2f4739571b7dd4e14b3
Aneeque Ahmad <aneeque65@gmail.com>
2022-05-01 20:33:02 +0500

    fix: navigation outside app when history delta is -1

--- packages/native/src/useLinking.tsx
+++ packages/native/src/useLinking.tsx
@@ -592,7 +592,8 @@ export default function useLinking(
               // We couldn't find an existing entry to go back to, so we'll go back by the delta
               // This won't be correct if multiple routes were pushed in one go before
               // Usually this shouldn't happen and this is a fallback for that
-              await history.go(historyDelta);
+              // Add Math.max to guard going to < 0 index
+              await history.go(Math.max(historyDelta, 0));
             }
 
             // Store the updated state as well as fix the path if incorrect
