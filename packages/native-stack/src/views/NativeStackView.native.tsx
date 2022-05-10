005180c430599f41a766bcfb93eeee7a86e862af
Hirbod Mirjavadi <hm@nightstomp.com>
2022-05-10 11:51:06 +0200

    feat: add logic to handle gestureDirection after review

--- packages/native-stack/src/views/NativeStackView.native.tsx
+++ packages/native-stack/src/views/NativeStackView.native.tsx
@@ -128,10 +128,8 @@ const SceneView = ({
 }: SceneViewProps) => {
   const { route, navigation, options, render } = descriptor;
   const {
-    animation,
+    animationDuration,
     animationTypeForReplace = 'push',
-    customAnimationOnGesture,
-    fullScreenGestureEnabled,
     gestureEnabled,
     header,
     headerShown,
@@ -144,11 +142,32 @@ const SceneView = ({
     statusBarStyle,
     statusBarTranslucent,
     statusBarColor,
-    swipeDirection = 'horizontal',
-    transitionDuration,
+    gestureDirection = 'horizontal',
   } = options;
 
-  let { presentation = 'card' } = options;
+  let {
+    animation,
+    customAnimationOnGesture,
+    fullScreenGestureEnabled,
+    presentation = 'card',
+  } = options;
+
+  if (gestureDirection === 'vertical') {
+    // for `vertical` direction to work, we need to set `fullScreenSwipeEnabled` to `true`
+    // so the screen can be dismissed from any point on screen.
+    // `customAnimationOnSwipe` needs to be set to `true` so the `stackAnimation` set by user can be used,
+    // otherwise `simple_push` will be used.
+    // Also, the default animation for this direction seems to be `slide_from_bottom`.
+    if (fullScreenGestureEnabled === undefined) {
+      fullScreenGestureEnabled = true;
+    }
+    if (customAnimationOnGesture === undefined) {
+      customAnimationOnGesture = true;
+    }
+    if (animation === undefined) {
+      animation = 'slide_from_bottom';
+    }
+  }
 
   if (index === 0) {
     // first screen should always be treated as `card`, it resolves problems with no header animation
@@ -209,8 +228,8 @@ const SceneView = ({
       statusBarStyle={statusBarStyle}
       statusBarColor={statusBarColor}
       statusBarTranslucent={statusBarTranslucent}
-      swipeDirection={swipeDirection}
-      transitionDuration={transitionDuration}
+      swipeDirection={gestureDirection}
+      transitionDuration={animationDuration}
       onWillDisappear={onWillDisappear}
       onAppear={onAppear}
       onDisappear={onDisappear}
