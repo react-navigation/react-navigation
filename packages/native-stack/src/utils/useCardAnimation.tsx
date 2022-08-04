8999aa1ee524b87fdcbd8af95e38e2c4b5b03c0c
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-08-04 13:13:38 +0200

    feat: PoC useCardAnimation for native-stack

--- packages/native-stack/src/utils/useCardAnimation.tsx
+++ packages/native-stack/src/utils/useCardAnimation.tsx
@@ -2,7 +2,33 @@ import React from 'react';
 import { Animated } from 'react-native';
 import { useTransitionProgress } from 'react-native-screens';
 
-export default function useCardAnimation() {
+export type NativeStackCardInterpolationProps = {
+  /**
+   * Values for the current screen.
+   */
+  current: {
+    /**
+     * Animated node representing the progress value of the current screen.
+     */
+    progress: Animated.AnimatedInterpolation;
+  };
+  /**
+   * Values for the screen after this one in the stack.
+   * This can be `undefined` in case the screen animating is the last one.
+   */
+  next?: {
+    /**
+     * Animated node representing the progress value of the next screen.
+     */
+    progress: Animated.AnimatedInterpolation;
+  };
+  /**
+   * Animated node representing whether the card is closing (1 - closing, 0 - not closing).
+   */
+  closing: Animated.AnimatedInterpolation;
+};
+
+export default function useCardAnimation(): NativeStackCardInterpolationProps {
   const { progress, closing } = useTransitionProgress();
   const [closingValue, setClosingValue] = React.useState<number>(0);
   const [progressValue, setProgressValue] = React.useState<number>(0);
@@ -19,5 +45,6 @@ export default function useCardAnimation() {
     current: {
       progress: new Animated.Value(Math.abs(progressValue - closingValue)),
     },
+    closing,
   };
 }
