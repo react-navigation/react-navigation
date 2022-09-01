05cf8ab8491105b1f2e2c6f2f4bffe8c615d4a80
Szymon Rybczak <szymon.rybczak@gmail.com>
2022-09-01 14:46:02 +0200

    fix: move onIndexChange (#1384)

--- packages/react-native-tab-view/src/PanResponderAdapter.tsx
+++ packages/react-native-tab-view/src/PanResponderAdapter.tsx
@@ -81,6 +81,8 @@ export default function PanResponderAdapter<T extends Route>({
 
       const { timing, ...transitionConfig } = DefaultTransitionSpec;
 
+      onIndexChangeRef.current(index);
+
       Animated.parallel([
         timing(panX, {
           ...transitionConfig,
@@ -89,7 +91,6 @@ export default function PanResponderAdapter<T extends Route>({
         }),
       ]).start(({ finished }) => {
         if (finished) {
-          onIndexChangeRef.current(index);
           pendingIndexRef.current = undefined;
         }
       });
