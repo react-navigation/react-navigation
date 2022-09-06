0cd7d7121082c24423639eb3268b53fc7fbf3147
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-09-06 15:36:41 +0200

    feat: remove 'next' property

--- packages/native-stack/src/utils/useCardAnimation.tsx
+++ packages/native-stack/src/utils/useCardAnimation.tsx
@@ -18,12 +18,6 @@ export type NativeStackCardInterpolationProps = {
    * Values for the screen after this one in the stack.
    * This can be `undefined` in case the screen animating is the last one.
    */
-  next?: {
-    /**
-     * Animated node representing the progress value of the next screen.
-     */
-    progress: Animated.AnimatedInterpolation;
-  };
   /**
    * Animated node representing whether the card is closing (1 - closing, 0 - not closing).
    */
@@ -34,38 +28,20 @@ export default function useCardAnimation(): NativeStackCardInterpolationProps {
   const { progress: transitionProgress, closing } = useTransitionProgress();
   const focused = useIsFocused();
 
-  const animatedFocused = new Animated.Value(focused ? 1 : 0);
-  const one = new Animated.Value(1);
-
   // RNScreens implement transtion progress as always from 0 to 1 in both directions
   // but useCardAnimation from JS Stack uses 0 to 1 on opening and 1 to 0 on closing
   const current = {
-    progress: conditional(
-      animatedFocused,
-      conditional(
-        closing,
-        Animated.subtract(closing, transitionProgress),
-        transitionProgress
-      ),
-      one
-    ),
-  };
-
-  const next = {
-    progress: conditional(
-      animatedFocused,
-      one,
-      conditional(
-        closing,
-        transitionProgress,
-        Animated.subtract(one, transitionProgress)
-      )
-    ),
+    progress: focused
+      ? conditional(
+          closing,
+          Animated.subtract(closing, transitionProgress),
+          transitionProgress
+        )
+      : new Animated.Value(1),
   };
 
   return {
     current,
-    next,
     closing,
   };
 }
