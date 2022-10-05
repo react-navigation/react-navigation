b9bd2f5c5a7f0f99e6cad1188353b785c01219de
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-10-05 17:06:32 +0200

    refactor!: remove deprecated options & warnings

--- packages/drawer/src/navigators/createDrawerNavigator.tsx
+++ packages/drawer/src/navigators/createDrawerNavigator.tsx
@@ -5,12 +5,10 @@ import {
   DrawerNavigationState,
   DrawerRouter,
   DrawerRouterOptions,
-  DrawerStatus,
   ParamListBase,
   useNavigationBuilder,
 } from '@react-navigation/native';
 import * as React from 'react';
-import warnOnce from 'warn-once';
 
 import type {
   DrawerNavigationConfig,
@@ -31,80 +29,13 @@ type Props = DefaultNavigatorOptions<
 function DrawerNavigator({
   id,
   initialRouteName,
-  defaultStatus: customDefaultStatus,
+  defaultStatus = 'closed',
   backBehavior,
   children,
   screenListeners,
   screenOptions,
-  ...restWithDeprecated
+  ...rest
 }: Props) {
-  const {
-    // @ts-expect-error: openByDefault is deprecated
-    openByDefault,
-    // @ts-expect-error: lazy is deprecated
-    lazy,
-    // @ts-expect-error: drawerContentOptions is deprecated
-    drawerContentOptions,
-    ...rest
-  } = restWithDeprecated;
-
-  let defaultScreenOptions: DrawerNavigationOptions = {};
-
-  if (drawerContentOptions) {
-    Object.assign(defaultScreenOptions, {
-      drawerPosition: drawerContentOptions.drawerPosition,
-      drawerType: drawerContentOptions.drawerType,
-      swipeEdgeWidth: drawerContentOptions.edgeWidth,
-      drawerHideStatusBarOnOpen: drawerContentOptions.hideStatusBar,
-      keyboardDismissMode: drawerContentOptions.keyboardDismissMode,
-      swipeMinDistance: drawerContentOptions.minSwipeDistance,
-      overlayColor: drawerContentOptions.overlayColor,
-      drawerStatusBarAnimation: drawerContentOptions.statusBarAnimation,
-      gestureHandlerProps: drawerContentOptions.gestureHandlerProps,
-    });
-
-    (
-      Object.keys(defaultScreenOptions) as (keyof DrawerNavigationOptions)[]
-    ).forEach((key) => {
-      if (defaultScreenOptions[key] === undefined) {
-        // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-        delete defaultScreenOptions[key];
-      }
-    });
-
-    warnOnce(
-      drawerContentOptions,
-      `Drawer Navigator: 'drawerContentOptions' is deprecated. Migrate the options to 'screenOptions' instead.\n\nPlace the following in 'screenOptions' in your code to keep current behavior:\n\n${JSON.stringify(
-        defaultScreenOptions,
-        null,
-        2
-      )}\n\nSee https://reactnavigation.org/docs/drawer-navigator#options for more details.`
-    );
-  }
-
-  if (typeof lazy === 'boolean') {
-    defaultScreenOptions.lazy = lazy;
-
-    warnOnce(
-      true,
-      `Drawer Navigator: 'lazy' in props is deprecated. Move it to 'screenOptions' instead.\n\nSee https://reactnavigation.org/docs/drawer-navigator/#lazy for more details.`
-    );
-  }
-
-  if (typeof openByDefault === 'boolean') {
-    warnOnce(
-      true,
-      `Drawer Navigator: 'openByDefault' is deprecated. Use 'defaultStatus' and set it to 'open' or 'closed' instead.\n\nSee https://reactnavigation.org/docs/drawer-navigator/#defaultstatus for more details.`
-    );
-  }
-
-  const defaultStatus: DrawerStatus =
-    customDefaultStatus !== undefined
-      ? customDefaultStatus
-      : openByDefault
-      ? 'open'
-      : 'closed';
-
   const { state, descriptors, navigation, NavigationContent } =
     useNavigationBuilder<
       DrawerNavigationState<ParamListBase>,
@@ -120,7 +51,6 @@ function DrawerNavigator({
       children,
       screenListeners,
       screenOptions,
-      defaultScreenOptions,
     });
 
   return (
