7338d4e9462d54fd132e852443e6c19e20ab5738
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2022-10-08 15:39:45 +0200

    refactor!: remove deprecated options & warnings (#10912)
    PR removes deprecation warnings introduced in https://github.com/react-navigation/react-navigation/commit/8e0ae5fc79dc7de1fe39e867184233958e3f82bd

--- packages/bottom-tabs/src/navigators/createBottomTabNavigator.tsx
+++ packages/bottom-tabs/src/navigators/createBottomTabNavigator.tsx
@@ -9,7 +9,6 @@ import {
   useNavigationBuilder,
 } from '@react-navigation/native';
 import * as React from 'react';
-import warnOnce from 'warn-once';
 
 import type {
   BottomTabNavigationConfig,
@@ -35,67 +34,8 @@ function BottomTabNavigator({
   screenListeners,
   screenOptions,
   sceneContainerStyle,
-  ...restWithDeprecated
+  ...rest
 }: Props) {
-  const {
-    // @ts-expect-error: lazy is deprecated
-    lazy,
-    // @ts-expect-error: tabBarOptions is deprecated
-    tabBarOptions,
-    ...rest
-  } = restWithDeprecated;
-
-  let defaultScreenOptions: BottomTabNavigationOptions = {};
-
-  if (tabBarOptions) {
-    Object.assign(defaultScreenOptions, {
-      tabBarHideOnKeyboard: tabBarOptions.keyboardHidesTabBar,
-      tabBarActiveTintColor: tabBarOptions.activeTintColor,
-      tabBarInactiveTintColor: tabBarOptions.inactiveTintColor,
-      tabBarActiveBackgroundColor: tabBarOptions.activeBackgroundColor,
-      tabBarInactiveBackgroundColor: tabBarOptions.inactiveBackgroundColor,
-      tabBarAllowFontScaling: tabBarOptions.allowFontScaling,
-      tabBarShowLabel: tabBarOptions.showLabel,
-      tabBarLabelStyle: tabBarOptions.labelStyle,
-      tabBarIconStyle: tabBarOptions.iconStyle,
-      tabBarItemStyle: tabBarOptions.tabStyle,
-      tabBarLabelPosition:
-        tabBarOptions.labelPosition ??
-        (tabBarOptions.adaptive === false ? 'below-icon' : undefined),
-      tabBarStyle: [
-        { display: tabBarOptions.tabBarVisible ? 'none' : 'flex' },
-        defaultScreenOptions.tabBarStyle,
-      ],
-    });
-
-    (
-      Object.keys(defaultScreenOptions) as (keyof BottomTabNavigationOptions)[]
-    ).forEach((key) => {
-      if (defaultScreenOptions[key] === undefined) {
-        // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-        delete defaultScreenOptions[key];
-      }
-    });
-
-    warnOnce(
-      tabBarOptions,
-      `Bottom Tab Navigator: 'tabBarOptions' is deprecated. Migrate the options to 'screenOptions' instead.\n\nPlace the following in 'screenOptions' in your code to keep current behavior:\n\n${JSON.stringify(
-        defaultScreenOptions,
-        null,
-        2
-      )}\n\nSee https://reactnavigation.org/docs/bottom-tab-navigator#options for more details.`
-    );
-  }
-
-  if (typeof lazy === 'boolean') {
-    defaultScreenOptions.lazy = lazy;
-
-    warnOnce(
-      true,
-      `Bottom Tab Navigator: 'lazy' in props is deprecated. Move it to 'screenOptions' instead.\n\nSee https://reactnavigation.org/docs/bottom-tab-navigator/#lazy for more details.`
-    );
-  }
-
   const { state, descriptors, navigation, NavigationContent } =
     useNavigationBuilder<
       TabNavigationState<ParamListBase>,
@@ -110,7 +50,6 @@ function BottomTabNavigator({
       children,
       screenListeners,
       screenOptions,
-      defaultScreenOptions,
     });
 
   return (
--- packages/material-top-tabs/src/navigators/createMaterialTopTabNavigator.tsx
+++ packages/material-top-tabs/src/navigators/createMaterialTopTabNavigator.tsx
@@ -9,7 +9,6 @@ import {
   useNavigationBuilder,
 } from '@react-navigation/native';
 import * as React from 'react';
-import warnOnce from 'warn-once';
 
 import type {
   MaterialTopTabNavigationConfig,
@@ -34,86 +33,8 @@ function MaterialTopTabNavigator({
   children,
   screenListeners,
   screenOptions,
-  ...restWithDeprecated
+  ...rest
 }: Props) {
-  const {
-    // @ts-expect-error: swipeEnabled is deprecated
-    swipeEnabled,
-    // @ts-expect-error: lazy is deprecated
-    lazy,
-    // @ts-expect-error: lazyPlaceholder is deprecated
-    lazyPlaceholder,
-    // @ts-expect-error: lazyPreloadDistance is deprecated
-    lazyPreloadDistance,
-    // @ts-expect-error: tabBarOptions is deprecated
-    tabBarOptions,
-    ...rest
-  } = restWithDeprecated;
-
-  let defaultScreenOptions: MaterialTopTabNavigationOptions = {};
-
-  if (tabBarOptions) {
-    Object.assign(defaultScreenOptions, {
-      tabBarActiveTintColor: tabBarOptions.activeTintColor,
-      tabBarInactiveTintColor: tabBarOptions.inactiveTintColor,
-      tabBarPressColor: tabBarOptions.pressColor,
-      tabBarPressOpacity: tabBarOptions.pressOpacity,
-      tabBarShowLabel: tabBarOptions.showLabel,
-      tabBarShowIcon: tabBarOptions.showIcon,
-      tabBarAllowFontScaling: tabBarOptions.allowFontScaling,
-      tabBarBounces: tabBarOptions.bounces,
-      tabBarScrollEnabled: tabBarOptions.scrollEnabled,
-      tabBarIconStyle: tabBarOptions.iconStyle,
-      tabBarLabelStyle: tabBarOptions.labelStyle,
-      tabBarItemStyle: tabBarOptions.tabStyle,
-      tabBarBadge: tabBarOptions.renderBadge,
-      tabBarIndicator: tabBarOptions.renderIndicator,
-      tabBarIndicatorStyle: tabBarOptions.indicatorStyle,
-      tabBarIndicatorContainerStyle: tabBarOptions.indicatorContainerStyle,
-      tabBarContentContainerStyle: tabBarOptions.contentContainerStyle,
-      tabBarStyle: tabBarOptions.style,
-    });
-
-    (
-      Object.keys(
-        defaultScreenOptions
-      ) as (keyof MaterialTopTabNavigationOptions)[]
-    ).forEach((key) => {
-      if (defaultScreenOptions[key] === undefined) {
-        // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-        delete defaultScreenOptions[key];
-      }
-    });
-
-    warnOnce(
-      tabBarOptions,
-      `Material Top Tab Navigator: 'tabBarOptions' is deprecated. Migrate the options to 'screenOptions' instead.\n\nPlace the following in 'screenOptions' in your code to keep current behavior:\n\n${JSON.stringify(
-        defaultScreenOptions,
-        null,
-        2
-      )}\n\nSee https://reactnavigation.org/docs/material-top-tab-navigator#options for more details.`
-    );
-  }
-
-  const deprecatedProps = {
-    swipeEnabled,
-    lazy,
-    lazyPlaceholder,
-    lazyPreloadDistance,
-  } as const;
-
-  Object.entries(deprecatedProps).forEach(([propName, propValue]) => {
-    if (propValue !== undefined) {
-      // @ts-expect-error: Object.entries doesn't return strict types
-      defaultScreenOptions[propName] = propValue;
-
-      warnOnce(
-        true,
-        `Material Top Tab Navigator: '${propName}' in props is deprecated. Move it to 'screenOptions' instead.\n\nSee https://reactnavigation.org/docs/material-top-tab-navigator#${propName.toLowerCase()} for more details.`
-      );
-    }
-  });
-
   const { state, descriptors, navigation, NavigationContent } =
     useNavigationBuilder<
       TabNavigationState<ParamListBase>,
@@ -128,7 +49,6 @@ function MaterialTopTabNavigator({
       children,
       screenListeners,
       screenOptions,
-      defaultScreenOptions,
     });
 
   return (
