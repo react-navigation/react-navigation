00f38fbaa801fdd0dfce6393b79da5bab0223d33
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-10-20 15:02:32 +0200

    chore: add nested stack test

--- packages/native/src/__tests__/useLinkingOnConditionalRendering.test.tsx
+++ packages/native/src/__tests__/useLinkingOnConditionalRendering.test.tsx
@@ -121,3 +121,128 @@ it('should schedule a state to be handled on conditional linking', async () => {
 
   consoleWarnMock.mockRestore();
 });
+
+it('should schedule a state to be handled on conditional linking in nested stack', async () => {
+  const createStackNavigator = createNavigatorFactory((props: any) => {
+    const { state, descriptors, NavigationContent } = useNavigationBuilder(
+      StackRouter,
+      props
+    );
+
+    return (
+      <NavigationContent>
+        {state.routes.map((route) => (
+          <div key={route.key}>{descriptors[route.key].render()}</div>
+        ))}
+      </NavigationContent>
+    );
+  });
+
+  const Stack = createStackNavigator();
+  const NestedStack = createStackNavigator();
+
+  const TestScreen = ({ route, signOut }: any): any => (
+    <>
+      <Text>{route.name}</Text>
+      <Button title="sign out" onPress={signOut} />
+    </>
+  );
+  const SignInScreen = ({ route, signIn }: any): any => {
+    const fn = useLinkingOnConditionalRender();
+    return (
+      <>
+        <Text>{route.name}</Text>
+        <Button
+          title="sign in"
+          onPress={() => {
+            fn();
+            signIn();
+          }}
+        />
+      </>
+    );
+  };
+
+  const linking = {
+    prefixes: ['rn://'],
+    config: {
+      screens: {
+        Home: {
+          path: 'home',
+          screens: {
+            Profile: 'profile',
+          },
+        },
+      },
+    },
+    async getInitialURL() {
+      return 'rn://home/profile';
+    },
+  };
+
+  const navigation = createNavigationContainerRef();
+
+  const App = () => {
+    const [isSignedIn, setSignedIn] = React.useState(false);
+
+    return (
+      <NavigationContainer ref={navigation} linking={linking}>
+        <Stack.Navigator>
+          {isSignedIn ? (
+            <Stack.Screen name="Home">
+              {() => (
+                <NestedStack.Navigator>
+                  <NestedStack.Screen name="Details">
+                    {(props) => (
+                      <TestScreen
+                        signOut={() => setSignedIn(false)}
+                        {...props}
+                      />
+                    )}
+                  </NestedStack.Screen>
+                  <NestedStack.Screen name="Profile">
+                    {(props) => (
+                      <TestScreen
+                        signOut={() => setSignedIn(false)}
+                        {...props}
+                      />
+                    )}
+                  </NestedStack.Screen>
+                </NestedStack.Navigator>
+              )}
+            </Stack.Screen>
+          ) : (
+            <Stack.Screen name="SignIn">
+              {(props) => (
+                <SignInScreen signIn={() => setSignedIn(true)} {...props} />
+              )}
+            </Stack.Screen>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  };
+
+  const consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation();
+  const { queryByText } = await waitFor(() => render(<App />));
+
+  expect(console.warn).toHaveBeenCalledTimes(1);
+  expect(queryByText('SignIn')).not.toBeNull();
+
+  fireEvent.press(queryByText(/sign in/i));
+
+  expect(queryByText('SignIn')).toBeNull();
+  expect(queryByText('Profile')).not.toBeNull();
+
+  fireEvent.press(queryByText(/sign out/i));
+
+  expect(queryByText('SignIn')).not.toBeNull();
+  expect(queryByText('Details')).toBeNull();
+  expect(queryByText('Profile')).toBeNull();
+
+  fireEvent.press(queryByText(/sign in/i));
+
+  expect(queryByText('Details')).not.toBeNull();
+
+  consoleWarnMock.mockRestore();
+});
