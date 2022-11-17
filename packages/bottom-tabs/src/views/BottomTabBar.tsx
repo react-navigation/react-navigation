8fffae91db6964e90b3205d62be2c518d503e512
josemak25 <amakirij@gmail.com>
2022-11-17 12:12:59 +0100

    feat: remove static style and used styled sheets

--- packages/bottom-tabs/src/views/BottomTabBar.tsx
+++ packages/bottom-tabs/src/views/BottomTabBar.tsx
@@ -324,9 +324,6 @@ export default function BottomTabBar({
         styles.layout,
         tabBarPositionMap[tabBarPosition].tabBar,
         {
-          left: 0,
-          right: 0,
-          bottom: 0,
           backgroundColor:
             tabBarBackgroundElement != null ? 'transparent' : colors.card,
         },
