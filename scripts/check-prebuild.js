c06a4c621b369ada479499f3684887a248cfb9c4
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-11-21 16:18:51 +0100

    chore: only patch podfile when necessary

--- scripts/check-prebuild.js
+++ scripts/check-prebuild.js
@@ -20,17 +20,24 @@ if (!prebuildFoldersExist) {
     )} does not exist.\n\n[react-navigation] Running 'npx expo prebuild'...\n\n`
   );
 
-  cp.execSync('npx expo prebuild', {
-    stdio: 'inherit',
-  });
+  cp.execSync(
+    'npx expo prebuild --no-install --skip-dependency-update react-native,expo',
+    {
+      stdio: 'inherit',
+    }
+  );
 }
 
 const podfilePath = path.join(__dirname, '../example/ios/Podfile');
-
 const podfile = fs.readFileSync(podfilePath, 'utf8');
-const podfileWithFlipperEnabled = podfile.replace(
-  /#\s*use_flipper!/gm,
-  'use_flipper!'
-);
 
-fs.writeFileSync(podfilePath, podfileWithFlipperEnabled);
+if (podfile.match(/#\s*use_flipper!/)) {
+  const podfileWithFlipperEnabled = podfile.replace(
+    /#\s*use_flipper!/gm,
+    'use_flipper!'
+  );
+
+  console.log('\n[react-navigation] Patching Podfile...\n');
+
+  fs.writeFileSync(podfilePath, podfileWithFlipperEnabled);
+}
