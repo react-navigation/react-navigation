4e4f4e22fa6a5ccd194d2b6d35b0c6a18427500a
Satyajit Sahoo <satyajit.happy@gmail.com>
2022-12-04 23:42:51 +0100

    Update packages/native/src/useScrollToTop.tsx

--- packages/native/src/useScrollToTop.tsx
+++ packages/native/src/useScrollToTop.tsx
@@ -109,7 +109,7 @@ export default function useScrollToTop(
     });
 
     return () => {
-      unsubscribers.map((unsubscribe) => unsubscribe());
+      unsubscribers.forEach(unsubscribe => unsubscribe());
     };
   }, [navigation, ref, route.key]);
 }
