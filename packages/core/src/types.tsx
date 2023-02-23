7302e307c94f58af3cf2c43ace449b056f4c5729
Anton Yushkevich <yushkevichm@gmail.com>
2023-02-23 22:24:25 +0300

    fix: PathConfigMap type error: Type 'T' does not satisfy the constraint '{}'

--- packages/core/src/types.tsx
+++ packages/core/src/types.tsx
@@ -784,7 +784,7 @@ export type PathConfig<ParamList extends {}> = {
 export type PathConfigMap<ParamList extends {}> = {
   [RouteName in keyof ParamList]?: NonNullable<
     ParamList[RouteName]
-  > extends NavigatorScreenParams<infer T>
+  > extends NavigatorScreenParams<infer T extends {}>
     ? string | PathConfig<T>
     : string | Omit<PathConfig<{}>, 'screens' | 'initialRouteName'>;
 };
