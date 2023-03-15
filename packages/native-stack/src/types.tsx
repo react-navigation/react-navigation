e0356b16cae06eac1e98fa7a874504da88c62a35
nikmantulenko <45350605+nikmantulenko@users.noreply.github.com>
2023-03-16 00:13:53 +0200

    Update types.tsx
    making NativeStackHeaderProps type generic

--- packages/native-stack/src/types.tsx
+++ packages/native-stack/src/types.tsx
@@ -64,7 +64,10 @@ export type NativeStackNavigationHelpers = NavigationHelpers<
 // We want it to be an empty object because navigator does not have any additional props
 export type NativeStackNavigationConfig = {};
 
-export type NativeStackHeaderProps = {
+export type NativeStackHeaderProps<
+  ParamList extends ParamListBase,
+  RouteName extends keyof ParamList = string,
+> = {
   /**
    * Options for the back button.
    */
@@ -81,11 +84,11 @@ export type NativeStackHeaderProps = {
   /**
    * Route object for the current screen.
    */
-  route: Route<string>;
+  route: RouteProp<ParamList, RouteName>;
   /**
    * Navigation prop for the header.
    */
-  navigation: NativeStackNavigationProp<ParamListBase>;
+  navigation: NativeStackNavigationProp<ParamList, RouteName>;
 };
 
 export type HeaderButtonProps = {
