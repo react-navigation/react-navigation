45c1c830faef4b954f5f1cecc385fca9e1dce42e
Dylan Companjen <dylancompanjen@gmail.com>
2023-04-20 10:57:11 +0200

    fix: headerHeight on phones with dynamic island

--- packages/native-stack/src/views/NativeStackView.native.tsx
+++ packages/native-stack/src/views/NativeStackView.native.tsx
@@ -220,9 +220,13 @@ const SceneView = ({
       ? 0
       : insets.top;
 
+  // On models with Dynamic Island the status bar height is smaller than the safe area top inset.
+  const hasDynamicIsland = Platform.OS === 'ios' && topInset === 59;
+  const statusBarHeight = hasDynamicIsland ? 54 : topInset;
+
   const { preventedRoutes } = usePreventRemoveContext();
 
-  const defaultHeaderHeight = getDefaultHeaderHeight(frame, isModal, topInset);
+  const defaultHeaderHeight = getDefaultHeaderHeight(frame, isModal, statusBarHeight);
 
   const [customHeaderHeight, setCustomHeaderHeight] =
     React.useState(defaultHeaderHeight);
