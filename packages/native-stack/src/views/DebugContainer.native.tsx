e86baaae6a5b9d02c5f00f8592e9010bb0bff711
Satyajit Sahoo <satyajit.happy@gmail.com>
2023-07-31 14:15:24 +0200

    Update packages/native-stack/src/views/DebugContainer.native.tsx

--- packages/native-stack/src/views/DebugContainer.native.tsx
+++ packages/native-stack/src/views/DebugContainer.native.tsx
@@ -9,10 +9,12 @@ type ContainerProps = ViewProps & {
   children: React.ReactNode;
 };
 
-// This view must *not* be flattened.
-// See https://github.com/software-mansion/react-native-screens/pull/1825
-// for detailed explanation.
-let Container = function (props: ContainerProps): JSX.Element {
+/**
+ * This view must *not* be flattened.
+ * See https://github.com/software-mansion/react-native-screens/pull/1825
+ * for detailed explanation.
+ */
+export let DebugContainer = (props: ContainerProps) => {
   return <View {...props} collapsable={false} />;
 };
 
