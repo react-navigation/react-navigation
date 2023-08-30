2f05071f7ae95919c29e34dd2f03b03c733b61c1
Adam Grzybowski <adam.grzybowski@swmansion.com>
2023-08-30 15:15:06 +0200

    feat: add useTabAnimation

--- packages/material-top-tabs/src/views/MaterialTopTabView.tsx
+++ packages/material-top-tabs/src/views/MaterialTopTabView.tsx
@@ -14,6 +14,7 @@ import type {
   MaterialTopTabNavigationConfig,
   MaterialTopTabNavigationHelpers,
 } from '../types';
+import { TabAnimationContext } from '../utils/TabAnimationContext';
 import { MaterialTopTabBar } from './MaterialTopTabBar';
 
 type Props = MaterialTopTabNavigationConfig & {
@@ -54,7 +55,11 @@ export function MaterialTopTabView({
           target: state.key,
         });
       }}
-      renderScene={({ route }) => descriptors[route.key].render()}
+      renderScene={({ route, position }) => (
+        <TabAnimationContext.Provider value={position}>
+          {descriptors[route.key].render()}
+        </TabAnimationContext.Provider>
+      )}
       navigationState={state}
       renderTabBar={renderTabBar}
       renderLazyPlaceholder={({ route }) =>
