f8241dba4997bde1d082a1ed8475498130818133
Satyajit Sahoo <satyajit.happy@gmail.com>
2023-09-25 09:19:44 +0200

    fix: don't use .web extensions

--- packages/native/src/useBackButton.tsx
+++ packages/native/src/useBackButton.tsx
@@ -2,32 +2,10 @@ import type {
   NavigationContainerRef,
   ParamListBase,
 } from '@react-navigation/core';
-import * as React from 'react';
-import { BackHandler } from 'react-native';
 
 export default function useBackButton(
-  ref: React.RefObject<NavigationContainerRef<ParamListBase>>
+  _: React.RefObject<NavigationContainerRef<ParamListBase>>
 ) {
-  React.useEffect(() => {
-    const subscription = BackHandler.addEventListener(
-      'hardwareBackPress',
-      () => {
-        const navigation = ref.current;
-
-        if (navigation == null) {
-          return false;
-        }
-
-        if (navigation.canGoBack()) {
-          navigation.goBack();
-
-          return true;
-        }
-
-        return false;
-      }
-    );
-
-    return () => subscription.remove();
-  }, [ref]);
+  // No-op
+  // BackHandler is not available on web
 }
