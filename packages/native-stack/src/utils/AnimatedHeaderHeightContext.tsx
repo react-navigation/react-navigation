e107aa10f940ba57a31f1a2806d3b1ebddccbf3d
tboba <tymoteusz.boba@gmail.com>
2023-11-02 13:29:45 +0100

    chore: remove AnimatedHeaderHeightContext.tsx

--- packages/native-stack/src/utils/AnimatedHeaderHeightContext.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import type { Animated } from 'react-native';
-
-export const AnimatedHeaderHeightContext = React.createContext<
-  Animated.Value | undefined
->(undefined);
