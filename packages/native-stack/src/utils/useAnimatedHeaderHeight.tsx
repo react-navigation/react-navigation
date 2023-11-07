02ad6eb0028d1bdd83ecf520eb293c20b9fc392e
tboba <tymoteusz.boba@gmail.com>
2023-11-07 09:52:58 +0100

    chore: correct error in useAnimatedHeaderHeight

--- packages/native-stack/src/utils/useAnimatedHeaderHeight.tsx
+++ packages/native-stack/src/utils/useAnimatedHeaderHeight.tsx
@@ -10,7 +10,7 @@ export function useAnimatedHeaderHeight() {
 
   if (animatedValue === undefined) {
     throw new Error(
-      "Couldn't find the header height. Are you inside a screen in a navigator with a header?"
+      "Couldn't find the header height. Are you inside a screen in a native stack navigator?"
     );
   }
 
