e01cf130c9b1b834ceaf824b30dea318bb6554dd
Uktam Khamidov <uktam4ik.khamidov@gmail.com>
2024-02-29 16:01:38 +0500

    remove throwing error from useBottomTabBarHeight

--- packages/bottom-tabs/src/utils/useBottomTabBarHeight.tsx
+++ packages/bottom-tabs/src/utils/useBottomTabBarHeight.tsx
@@ -6,9 +6,8 @@ export function useBottomTabBarHeight() {
   const height = React.useContext(BottomTabBarHeightContext);
 
   if (height === undefined) {
-    throw new Error(
-      "Couldn't find the bottom tab bar height. Are you inside a screen in Bottom Tab Navigator?"
-    );
+    console.warn("Couldn't find the bottom tab bar height. Are you inside a screen in Bottom Tab Navigator?");
+    return 0;
   }
 
   return height;
