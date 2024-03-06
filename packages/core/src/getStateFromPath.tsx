b07f0e2d02c2ca56232aa87afd4826bc513f3120
Mykola Stanislavchuk <kola.stani@gmail.com>
2024-03-07 00:41:19 +0100

    fix: decodes encoded params in path when parsing it to state  (#11869)
    **Motivation**
    
    params was not decoded before put to the state - it can lead to
    different path
    
    ---------
    
    Co-authored-by: Satyajit Sahoo <satyajit.happy@gmail.com>

--- packages/core/src/getStateFromPath.tsx
+++ packages/core/src/getStateFromPath.tsx
@@ -275,11 +275,14 @@ const matchAgainstConfigs = (remaining: string, configs: RouteConfig[]) => {
         ?.split('/')
         .filter((p) => p.startsWith(':'))
         .reduce<Record<string, any>>(
-          (acc, p, i) =>
-            Object.assign(acc, {
-              // The param segments appear every second item starting from 2 in the regex match result
-              [p]: match![(i + 1) * 2].replace(/\//, ''),
-            }),
+          (acc, p, i) => {
+            // The param segments appear every second item starting from 2 in the regex match result
+            const decodedParamSegment = decodeURIComponent(match![(i + 1) * 2]);
+            return Object.assign(acc, {
+              [p]: decodedParamSegment.replace(/\//, ''),
+            });
+          },
+
           {}
         );
 
