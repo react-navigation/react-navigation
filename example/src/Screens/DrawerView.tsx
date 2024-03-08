f6e9836b6a8ea99e896934383953c80715e59898
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-03-08 13:55:46 +0100

    chore: add buttons to customize drawer options in example

--- example/src/Screens/DrawerView.tsx
+++ example/src/Screens/DrawerView.tsx
@@ -1,4 +1,6 @@
+import { useActionSheet } from '@expo/react-native-action-sheet';
 import { Button } from '@react-navigation/elements';
+import { useTheme } from '@react-navigation/native';
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Drawer, useDrawerProgress } from 'react-native-drawer-layout';
@@ -28,28 +30,77 @@ const RealDrawer = () => {
   );
 };
 
+const DRAWER_TYPES = ['front', 'back', 'slide'] as const;
+
 export function DrawerView() {
+  const { showActionSheetWithOptions } = useActionSheet();
+  const { colors } = useTheme();
+
   const [open, setOpen] = React.useState(false);
+  const [drawerType, setDrawerType] =
+    React.useState<(typeof DRAWER_TYPES)[number]>('front');
+  const [drawerPosition, setDrawerPosition] = React.useState<'left' | 'right'>(
+    'left'
+  );
 
   return (
     <Drawer
       open={open}
       onOpen={() => setOpen(true)}
       onClose={() => setOpen(false)}
-      drawerType="front"
+      drawerType={drawerType}
+      drawerPosition={drawerPosition}
       renderDrawerContent={() => {
         return (
           <View style={styles.container}>
-            <Button onPress={() => setOpen(false)}>Close drawer</Button>
+            <Button color="tomato" onPress={() => setOpen(false)}>
+              Close drawer
+            </Button>
           </View>
         );
       }}
     >
-      <View style={styles.container}>
+      <View style={[styles.container, { backgroundColor: colors.background }]}>
         <RealDrawer />
-        <Button onPress={() => setOpen((prevOpen) => !prevOpen)}>
-          {open ? 'Close' : 'Open'} drawer
-        </Button>
+        <View style={styles.buttons}>
+          <Button
+            variant="filled"
+            onPress={() => setOpen((prevOpen) => !prevOpen)}
+          >
+            {open ? 'Close' : 'Open'} drawer
+          </Button>
+          <Button
+            onPress={() =>
+              setDrawerPosition((prevPosition) =>
+                prevPosition === 'left' ? 'right' : 'left'
+              )
+            }
+          >
+            Change position ({drawerPosition})
+          </Button>
+          <Button
+            onPress={() =>
+              showActionSheetWithOptions(
+                {
+                  options: DRAWER_TYPES.map((option) => {
+                    if (option === drawerType) {
+                      return `${option} (current)`;
+                    }
+
+                    return option;
+                  }),
+                },
+                (index) => {
+                  if (index != null) {
+                    setDrawerType(DRAWER_TYPES[index]);
+                  }
+                }
+              )
+            }
+          >
+            Change type ({drawerType})
+          </Button>
+        </View>
       </View>
     </Drawer>
   );
@@ -67,6 +118,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 8,
+  },
+  buttons: {
+    gap: 8,
   },
   realDrawerWrapper: {
     width: 64,
