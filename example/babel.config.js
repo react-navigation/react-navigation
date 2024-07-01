281a508563f7d582221a193bbc5b81042026c952
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-07-01 11:34:01 +0200

    chore: fix aliasing monorepo packages for react-native

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,7 +1,38 @@
+const path = require('path');
+const fs = require('fs');
+
+const packages = path.resolve(__dirname, '..', 'packages');
+
 module.exports = function (api) {
   api.cache(true);
+
+  const alias = Object.fromEntries(
+    fs
+      .readdirSync(packages)
+      .filter((name) => !name.startsWith('.'))
+      .map((name) => {
+        const pak = require(`../packages/${name}/package.json`);
+
+        if (pak.source == null) {
+          return null;
+        }
+
+        return [pak.name, path.resolve(packages, name, pak.source)];
+      })
+      .filter(Boolean)
+  );
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+          alias,
+        },
+      ],
+    ],
   };
 };
