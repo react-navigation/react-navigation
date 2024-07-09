0b51deef3bfc7d56f684b4344c624e77433c0940
tboba <tymoteusz.boba@gmail.com>
2024-07-09 14:55:21 +0200

    chore: revert screens to 3.31.1

--- packages/native-stack/src/views/NativeStackView.native.tsx
+++ packages/native-stack/src/views/NativeStackView.native.tsx
@@ -361,6 +361,7 @@ const SceneView = ({
       homeIndicatorHidden={autoHideHomeIndicator}
       hideKeyboardOnSwipe={keyboardHandlingEnabled}
       navigationBarColor={navigationBarColor}
+      // @ts-expect-error prop supported from react-native-screens 3.32.0 onwards
       navigationBarTranslucent={navigationBarTranslucent}
       navigationBarHidden={navigationBarHidden}
       replaceAnimation={animationTypeForReplace}
