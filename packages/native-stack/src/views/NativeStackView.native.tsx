3115a224354a2f8040beb538696c3d5a4bdac40e
Maksymilian Galas <maksymilian.galas@swmansion.com>
2024-07-22 14:35:05 +0200

    chore: remove @ts-expect-error causing CI fails

--- packages/native-stack/src/views/NativeStackView.native.tsx
+++ packages/native-stack/src/views/NativeStackView.native.tsx
@@ -344,7 +344,6 @@ const SceneView = ({
       homeIndicatorHidden={autoHideHomeIndicator}
       hideKeyboardOnSwipe={keyboardHandlingEnabled}
       navigationBarColor={navigationBarColor}
-      // @ts-expect-error prop supported from react-native-screens 3.32.0 onwards
       navigationBarTranslucent={navigationBarTranslucent}
       navigationBarHidden={navigationBarHidden}
       replaceAnimation={animationTypeForReplace}
