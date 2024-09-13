befe47d7752e3cb69d426eb4814f8d63b06aa8f8
Mark Lawlor <mwlawlor@gmail.com>
2024-09-13 11:42:00 +1000

    feat: export LocaleDirContext and UNSTABLE_UnhandledLinkingContext

--- packages/native/src/index.tsx
+++ packages/native/src/index.tsx
@@ -1,11 +1,13 @@
 export { createStaticNavigation } from './createStaticNavigation';
 export { Link } from './Link';
 export { LinkingContext } from './LinkingContext';
+export { LocaleDirContext } from './LocaleDirContext';
 export { NavigationContainer } from './NavigationContainer';
 export { ServerContainer } from './ServerContainer';
 export { DarkTheme } from './theming/DarkTheme';
 export { DefaultTheme } from './theming/DefaultTheme';
 export * from './types';
+export { UnhandledLinkingContext as UNSTABLE_UnhandledLinkingContext } from './UnhandledLinkingContext';
 export { useLinkBuilder } from './useLinkBuilder';
 export { type LinkProps, useLinkProps } from './useLinkProps';
 export { useLinkTo } from './useLinkTo';
