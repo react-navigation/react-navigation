26e974f5185aa9b8bd014737f897430a35bffc86
Lem <ulna-draftee-0z@icloud.com>
2024-09-21 23:47:08 -0600

    test: web condition was not being called by tests

--- packages/react-native-tab-view/src/__tests__/PlatformPressable.test.tsx
+++ packages/react-native-tab-view/src/__tests__/PlatformPressable.test.tsx
@@ -1,49 +1,69 @@
-import { describe, expect, jest, test } from '@jest/globals';
+import { beforeAll, describe, expect, jest, test } from '@jest/globals';
 import { fireEvent, render, userEvent } from '@testing-library/react-native';
 import { Platform, View } from 'react-native';
 
 import { PlatformPressable } from '../PlatformPressable';
 
 describe('PlatformPressable', () => {
-  const renderUI = (onPress: jest.Mock) => {
-    return render(
-      <PlatformPressable onPress={onPress} testID={'Pressable'}>
-        <View />
-      </PlatformPressable>
-    );
-  };
-
-  describe('tests using Fire Event API', () => {
-    test('should be pressable using simply fireEvent.press', async () => {
+  describe('on native', () => {
+    test('should be pressable using simply fireEvent.press', () => {
       const onPress = jest.fn();
-      const { getByTestId } = renderUI(onPress);
+      const { getByTestId } = render(
+        <PlatformPressable onPress={onPress} testID={'Pressable'}>
+          <View />
+        </PlatformPressable>
+      );
 
       fireEvent.press(getByTestId('Pressable'));
 
       expect(onPress).toHaveBeenCalled();
     });
-  });
 
-  describe('tests using User Event API', () => {
-    jest.useFakeTimers();
-
-    test('should be pressable on web', async () => {
-      Platform.OS = 'web';
+    test('should be pressable using with UserEvent', async () => {
       const onPress = jest.fn();
-      const { getByTestId } = renderUI(onPress);
+      const { getByTestId } = render(
+        <PlatformPressable onPress={onPress} testID={'Pressable'}>
+          <View />
+        </PlatformPressable>
+      );
 
       const user = userEvent.setup();
       await user.press(getByTestId('Pressable'));
 
       expect(onPress).toHaveBeenCalled();
     });
+  });
 
-    test('should be pressable using the User Event API', async () => {
+  describe('on web', () => {
+    const renderWebUI = (onPress: jest.Mock) => {
+      return render(
+        <PlatformPressable onPress={onPress} testID={'Pressable'} href={'/'}>
+          <View />
+        </PlatformPressable>
+      );
+    };
+
+    beforeAll(() => {
+      Platform.OS = 'web';
+    });
+
+    test('should ignore non-left clicks', () => {
       const onPress = jest.fn();
-      const { getByTestId } = renderUI(onPress);
+      const { getByTestId } = renderWebUI(onPress);
 
-      const user = userEvent.setup();
-      await user.press(getByTestId('Pressable'));
+      fireEvent.press(getByTestId('Pressable'), { button: 1 });
+
+      expect(onPress).not.toHaveBeenCalled();
+    });
+
+    test('should be pressable with a left click', () => {
+      const onPress = jest.fn();
+      const { getByTestId } = renderWebUI(onPress);
+
+      fireEvent.press(getByTestId('Pressable'), {
+        button: 0,
+        preventDefault: jest.fn(),
+      });
 
       expect(onPress).toHaveBeenCalled();
     });
