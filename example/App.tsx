2a865b4b357588fc46d65df8a66945ff49aeb5b1
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-10-22 13:46:37 +0200

    chore: setup expo dev client

--- example/App.tsx
+++ example/App.tsx
@@ -5,8 +5,14 @@ import { Assets } from '@react-navigation/elements';
 import { registerRootComponent } from 'expo';
 import { Asset } from 'expo-asset';
 import * as React from 'react';
+import { LogBox } from 'react-native';
 
 import { App } from './src/index';
+
+LogBox.ignoreLogs([
+  'Warning: findHostInstance_DEPRECATED is deprecated in StrictMode',
+  'Warning: findNodeHandle is deprecated in StrictMode',
+]);
 
 Asset.loadAsync(Assets);
 
