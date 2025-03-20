f24734d31d248aad130cd85e8db6fbfc567be2cf
Maciej Stosio <maciekstosio@users.noreply.github.com>
2025-03-20 10:53:59 +0100

    fix: jumping header back button

--- packages/native-stack/src/views/useHeaderConfigProps.tsx
+++ packages/native-stack/src/views/useHeaderConfigProps.tsx
@@ -187,7 +187,7 @@ export function useHeaderConfigProps({
     parseInt(Platform.Version, 10) >= 14 &&
     // Doesn't have custom back title
     headerBackTitle == null &&
-    // Doesn't have custom styling
+    // Doesn't have custom styling, by default System, see: https://github.com/software-mansion/react-native-screens/pull/2105#discussion_r1565222738
     backTitleFontFamily === 'System' &&
     backTitleFontSize == null &&
     // Back button menu is not disabled
