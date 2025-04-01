61e356100678f43874fb11e64de11c0da22c4695
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-04-01 20:17:04 +0200

    feat: add a useRoutePath hook

---
+++ packages/native/src/useRoutePath.tsx
@@ -0,0 +1,30 @@
+import { getPathFromState, useStateForPath } from '@react-navigation/core';
+import * as React from 'react';
+
+import { LinkingContext } from './LinkingContext';
+
+/**
+ * Hook to get the path for the current route based on linking options.
+ *
+ * @returns Path for the current route.
+ */
+export function useRoutePath() {
+  const { options } = React.useContext(LinkingContext);
+  const state = useStateForPath();
+
+  if (state === undefined) {
+    throw new Error(
+      "Couldn't find a state for the route object. Is your component inside a screen in a navigator?"
+    );
+  }
+
+  const getPathFromStateHelper = options?.getPathFromState ?? getPathFromState;
+
+  const path = React.useMemo(() => {
+    const path = getPathFromStateHelper(state, options?.config);
+
+    return path;
+  }, [state, options?.config, getPathFromStateHelper]);
+
+  return path;
+}
