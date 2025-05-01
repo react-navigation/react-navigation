645d23112675cb532245b4459d10dc0991f3095a
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-05-01 17:05:26 +0200

    refactor: move history tests to 1 file and add more assertions

--- example/e2e/tests/history.test.ts
+++ example/e2e/tests/history.test.ts
@@ -1,5 +1,36 @@
 import { expect, test } from '@playwright/test';
 
+test('pops to the proper screen after going back and forward in history', async ({
+  page,
+}) => {
+  await page.goto('/native-stack/article/gandalf');
+
+  const buttonToFeed = page.getByRole('button', {
+    name: 'Navigate to feed',
+  });
+
+  await buttonToFeed.click();
+
+  await page.goBack();
+
+  await page.goForward();
+
+  const buttonGoBack = page.getByRole('button', {
+    name: 'Go back',
+  });
+
+  // After click, we have [not-found, home] on history stack
+  await buttonGoBack.click();
+
+  await expect(page).toHaveTitle(
+    'Article by Gandalf - React Navigation Example'
+  );
+
+  await expect(
+    page.getByRole('heading', { name: 'Article by Gandalf' })
+  ).toBeVisible();
+});
+
 test('go back to the proper history entry after popping nested stack', async ({
   page,
 }) => {
@@ -36,7 +67,11 @@ test('go back to the proper history entry after popping nested stack', async ({
 
   await expect(page).toHaveTitle('Examples - React Navigation Example');
 
+  await expect(page.getByRole('heading', { name: 'Examples' })).toBeVisible();
+
   await page.goBack();
 
   await expect(page).toHaveTitle('Oops! - React Navigation Example');
+
+  await expect(page.getByRole('heading', { name: 'Oops!' })).toBeVisible();
 });
