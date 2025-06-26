25e643c3690bfd48c0d9b61cf3e461512662e332
Johan Kasperi <johan@kasperi.se>
2025-06-26 13:40:11 +0200

    feat: titleStyle color

--- packages/native-stack/src/utils/prepareHeaderBarButtonItems.ts
+++ packages/native-stack/src/utils/prepareHeaderBarButtonItems.ts
@@ -7,12 +7,36 @@ export const prepareHeaderBarButtonItems = (
   routeKey: string
 ) => {
   return barButtonItems?.map((item, index) => {
+    if ('spacing' in item) {
+      return item;
+    }
+    const image = item.image ? Image.resolveAssetSource(item.image) : undefined;
+    const titleStyle = item.titleStyle
+      ? { ...item.titleStyle, color: processColor(item.titleStyle.color) }
+      : undefined;
+    const tintColor = item.tintColor ? processColor(item.tintColor) : undefined;
+    const processedItem = {
+      ...item,
+      image,
+      titleStyle,
+      tintColor,
+    };
     if ('onPress' in item) {
       return {
-        ...item,
-        ...(item.image ? { image: Image.resolveAssetSource(item.image) } : {}),
-        ...(item.tintColor ? { tintColor: processColor(item.tintColor) } : {}),
+        ...processedItem,
         buttonId: `${item.title ?? item.image}-${index}-${routeKey}`,
+      };
+    }
+    if ('menu' in item) {
+      return {
+        ...processedItem,
+        menu: item.menu.map((menuItem, menuIndex) => ({
+          ...menuItem,
+          // ...(menuItem.image ? { image: Image.resolveAssetSource(menuItem.image) } : {}),
+          // ...(menuItem.tintColor ? { tintColor: processColor(menuItem.tintColor) } : {}),
+          // buttonId: `${menuItem.title ?? menuItem.image}-${menuIndex}-${routeKey}`,
+          menuId: `${menuItem.title}-${menuIndex}-${routeKey}`,
+        })),
       };
     }
     return item;
