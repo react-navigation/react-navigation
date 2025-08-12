cec23be7fbf509c5f55dcb744f9455b64a486f46
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-08-12 03:30:11 +0200

    chore: add vite config to run example on web

---
+++ example/src/theme.native.tsx
@@ -0,0 +1,27 @@
+import { DefaultTheme, type Theme } from '@react-navigation/native';
+import { Platform, PlatformColor } from 'react-native';
+
+export const PlatformTheme: Theme = {
+  ...DefaultTheme,
+  colors: Platform.select<() => Theme['colors']>({
+    ios: () => ({
+      primary: PlatformColor('systemRed'),
+      background: PlatformColor('systemGroupedBackground'),
+      card: PlatformColor('tertiarySystemBackground'),
+      text: PlatformColor('label'),
+      border: PlatformColor('separator'),
+      notification: PlatformColor('systemRed'),
+    }),
+    android: () => ({
+      primary: PlatformColor('@android:color/system_primary_light'),
+      background: PlatformColor(
+        '@android:color/system_surface_container_light'
+      ),
+      card: PlatformColor('@android:color/system_background_light'),
+      text: PlatformColor('@android:color/system_on_surface_light'),
+      border: PlatformColor('@android:color/system_outline_variant_light'),
+      notification: PlatformColor('@android:color/holo_red_light'),
+    }),
+    default: () => DefaultTheme.colors,
+  })(),
+};
