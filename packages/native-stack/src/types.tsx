78cad172d5347e25b5da1694129dee1cdb224dd6
Konrad Michalik <konrad.michalik@swmansion.com>
2025-09-09 11:13:19 +0200

    docs(iOS): update docs for animationDuration

--- packages/native-stack/src/types.tsx
+++ packages/native-stack/src/types.tsx
@@ -540,7 +540,7 @@ export type NativeStackNavigationOptions = {
   animation?: ScreenProps['stackAnimation'];
   /**
    * Changes the duration (in milliseconds) of `slide_from_bottom`, `fade_from_bottom`, `fade` and `simple_push` transitions on iOS. Defaults to `500`.
-   * The duration of `default` and `flip` transitions isn't customizable.
+   * For screens with `default` and `flip` transitions, and, as of now, for screens with `presentation` set to `modal`, `formSheet`, `pageSheet` (regardless of transition), the duration isn't customizable.
    *
    * @platform ios
    */
