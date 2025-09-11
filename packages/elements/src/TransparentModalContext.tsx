f76f68caaabf7085d2a1f4d2bd35ca074bae00a6
Anthony Wentzel <anthony.wentzel@appfolio.com>
2025-09-11 16:34:55 -0400

    fix(context): leverage getNamedContext util

--- packages/elements/src/TransparentModalContext.tsx
+++ packages/elements/src/TransparentModalContext.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
+import { getNamedContext } from './getNamedContext';
 
 /**
  * Context to track whether a transparent modal is currently being displayed.
  * When true, other navigation elements (headers, tab bars) should be hidden from screen readers.
  */
-export const TransparentModalContext = React.createContext(false);
+export const TransparentModalContext = getNamedContext('TransparentModalContext', false);
