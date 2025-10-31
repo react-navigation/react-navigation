1afef6053d4686acf5e3dd94ad4f413ceab1952f
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-10-31 15:50:15 +0100

    feat: accept name argument for useRoute (#12820)
    **Motivation**
    
    This updates `useRoute` to use a type-safe API.
    
    - The `useRoute` hook now accepts an argument - the name of the route
    - The argument to `useRoute` provides auto-completion based on all
    routes in the project (using `ReactNavigation.RootParamList`)
    - The returned type of `useRoute` is based on the route name argument
    - When no argument is passed, `useRoute` returns a union of all routes
    in the project
    
    BREAKING CHANGE: `useRoute` no longer accepts a generic to override the
    returned type since it's not type-safe. User can do `const route =
    useRoute() as SomeType` to make it explicit.
    
    
    https://github.com/user-attachments/assets/512ab42e-af24-452e-8ef1-99276a3ba17c
    
    NOTE: This doesn't check parent routes. It's being worked on in
    https://github.com/react-navigation/react-navigation/pull/12790
    
    **Test plan**
    
    - Added unit tests and type tests

--- packages/core/src/useRoute.tsx
+++ packages/core/src/useRoute.tsx
@@ -1,15 +1,31 @@
-import type { ParamListBase } from '@react-navigation/routers';
+import type { ParamListBase, Route } from '@react-navigation/routers';
 import * as React from 'react';
 
 import { NavigationRouteContext } from './NavigationRouteContext';
-import type { RouteProp } from './types';
+import type { ParamListRoute, ParamsForRoute, RouteProp } from './types';
 
 /**
  * Hook to access the route prop of the parent screen anywhere.
  *
  * @returns Route prop of the parent screen.
  */
-export function useRoute<T extends RouteProp<ParamListBase>>(): T {
+export function useRoute<
+  RootParamList extends {} = ReactNavigation.RootParamList,
+  RouteName extends AllRouteNames<RootParamList> = AllRouteNames<RootParamList>,
+>(
+  name: RouteName
+): Route<
+  RouteName,
+  RootParamList extends ParamListBase
+    ? ParamsForRoute<RootParamList, RouteName>
+    : undefined
+>;
+export function useRoute<
+  RootParamList extends {} = ReactNavigation.RootParamList,
+>(): RootParamList extends ParamListBase
+  ? ParamListRoute<RootParamList>
+  : RouteProp<ParamListBase>;
+export function useRoute(name?: string) {
   const route = React.useContext(NavigationRouteContext);
 
   if (route === undefined) {
@@ -18,5 +34,19 @@ export function useRoute<T extends RouteProp<ParamListBase>>(): T {
     );
   }
 
-  return route as T;
+  if (name !== undefined && route.name !== name) {
+    throw new Error(
+      `The provided route name ('${name}') doesn't match the current route's name ('${route.name}'). It must be used in the '${name}' screen.`
+    );
+  }
+
+  return route;
 }
+
+/**
+ * Get all possible route names from a param list and its nested navigators.
+ */
+type AllRouteNames<RootParamList extends {}> =
+  RootParamList extends ParamListBase
+    ? ParamListRoute<RootParamList>['name']
+    : string;
