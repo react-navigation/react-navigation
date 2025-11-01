484b221b315ebf7f46da24c67b0b18ff56c697b6
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-11-01 14:03:53 +0100

    refactor: tweak how lazy screens are handled

--- packages/elements/src/Lazy.tsx
+++ packages/elements/src/Lazy.tsx
@@ -1,14 +1,49 @@
 import * as React from 'react';
 
 type Props = {
+  /**
+   * Whether lazy rendering is enabled.
+   */
   enabled: boolean;
+  /**
+   * Whether the component is visible.
+   */
+  visible: boolean;
+  /**
+   * Function that returns the content to render.
+   */
   render: () => React.ReactElement;
 };
 
-export function Lazy({ enabled, render }: Props) {
-  const [rendered, setRendered] = React.useState(enabled);
+/**
+ * Render content lazily based on visibility.
+ *
+ * When enabled:
+ * - If content is visible, it will render immediately
+ * - If content is not visible, it won't render until it becomes visible
+ *
+ * Otherwise:
+ * - If content is visible, it will render immediately
+ * - If content is not visible, it will defer rendering until idle
+ *
+ * Once rendered, the content remains rendered.
+ */
+export function Lazy({ enabled, visible, render }: Props) {
+  const [rendered, setRendered] = React.useState(enabled ? visible : false);
 
-  if (enabled === true && rendered === false) {
+  React.useEffect(() => {
+    if (enabled || visible || rendered) {
+      return;
+    }
+
+    const id = requestIdleCallback(() => {
+      setRendered(true);
+    });
+
+    return () => cancelIdleCallback(id);
+  }, [enabled, rendered, visible]);
+
+  if (visible && rendered === false) {
     setRendered(true);
 
     return render();
