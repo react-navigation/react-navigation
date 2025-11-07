1af31c9eb4195fb89ae6162ef248757fe7e12ac4
osdnk <micosa97@gmail.com>
2025-11-07 18:04:57 +0200

    fix: resolve header jumping on iOS with Fabric #2

--- packages/stack/src/views/Header/HeaderContainer.tsx
+++ packages/stack/src/views/Header/HeaderContainer.tsx
@@ -8,7 +8,6 @@ import {
 import * as React from 'react';
 import {
   Animated,
-  Platform,
   type StyleProp,
   StyleSheet,
   View,
@@ -41,35 +40,6 @@ export type Props = {
   style?: Animated.WithAnimatedValue<StyleProp<ViewStyle>>;
 };
 
-// This is a workaround for https://github.com/react-navigation/react-navigation/issues/12456
-// It seems that the animated values don't get properly updated
-// when the position is set on the Animated.View directly.
-// As a result, on iOS the header happens to be positioned outside of the screen.
-// To fix this, we extract the position and set it on a wrapping View.
-function flattenStyleAndExtractPosition(
-  style: Animated.WithAnimatedValue<StyleProp<ViewStyle>>
-): {
-  flattenedStyle: Animated.WithAnimatedValue<StyleProp<ViewStyle>>;
-  position: ViewStyle['position'] | undefined;
-} {
-  const flattenedStyle = StyleSheet.flatten(style) || {};
-
-  if (
-    Platform.OS === 'ios' &&
-    typeof flattenedStyle === 'object' &&
-    'position' in flattenedStyle &&
-    typeof flattenedStyle.position === 'string'
-  ) {
-    const position = flattenedStyle.position;
-    delete flattenedStyle.position;
-    return { flattenedStyle, position };
-  }
-
-  return { flattenedStyle, position: undefined };
-}
-
-const Container = Platform.OS === 'ios' ? View : React.Fragment;
-
 export function HeaderContainer({
   mode,
   scenes,
@@ -82,138 +52,134 @@ export function HeaderContainer({
   const parentHeaderBack = React.useContext(HeaderBackContext);
   const { buildHref } = useLinkBuilder();
 
-  const { flattenedStyle, position } = flattenStyleAndExtractPosition(style);
-
   return (
-    <Container style={{ position }}>
-      <Animated.View style={[styles.container, flattenedStyle]}>
-        {scenes.slice(-3).map((scene, i, self) => {
-          if ((mode === 'screen' && i !== self.length - 1) || !scene) {
-            return null;
-          }
+    <Animated.View style={[styles.container, style]}>
+      {/* We render header only on two top-most headers as
+         a workaround for https://github.com/react-navigation/react-navigation/issues/12456.
+         If the header is persisted, it might be placed incorrectly when navigating back. */}
+      {scenes.slice(-2).map((scene, i, self) => {
+        if ((mode === 'screen' && i !== self.length - 1) || !scene) {
+          return null;
+        }
 
+        const {
+          header,
+          headerMode,
+          headerShown = true,
+          headerTransparent,
+          headerStyleInterpolator,
+        } = scene.descriptor.options;
+
+        if (headerMode !== mode || !headerShown) {
+          return null;
+        }
+
+        const isFocused = focusedRoute.key === scene.descriptor.route.key;
+        const previousScene = getPreviousScene({
+          route: scene.descriptor.route,
+        });
+
+        let headerBack = parentHeaderBack;
+
+        if (previousScene) {
+          const { options, route } = previousScene.descriptor;
+
+          headerBack = previousScene
+            ? {
+                title: getHeaderTitle(options, route.name),
+                href: buildHref(route.name, route.params),
+              }
+            : parentHeaderBack;
+        }
+
+        // If the screen is next to a headerless screen, we need to make the header appear static
+        // This makes the header look like it's moving with the screen
+        const previousDescriptor = self[i - 1]?.descriptor;
+        const nextDescriptor = self[i + 1]?.descriptor;
+
+        const {
+          headerShown: previousHeaderShown = true,
+          headerMode: previousHeaderMode,
+        } = previousDescriptor?.options || {};
+
+        // If any of the next screens don't have a header or header is part of the screen
+        // Then we need to move this header offscreen so that it doesn't cover it
+        const nextHeaderlessScene = self.slice(i + 1).find((scene) => {
           const {
-            header,
-            headerMode,
-            headerShown = true,
-            headerTransparent,
-            headerStyleInterpolator,
-          } = scene.descriptor.options;
+            headerShown: currentHeaderShown = true,
+            headerMode: currentHeaderMode,
+          } = scene?.descriptor.options || {};
 
-          if (headerMode !== mode || !headerShown) {
-            return null;
-          }
+          return currentHeaderShown === false || currentHeaderMode === 'screen';
+        });
 
-          const isFocused = focusedRoute.key === scene.descriptor.route.key;
-          const previousScene = getPreviousScene({
-            route: scene.descriptor.route,
-          });
+        const { gestureDirection: nextHeaderlessGestureDirection } =
+          nextHeaderlessScene?.descriptor.options || {};
 
-          let headerBack = parentHeaderBack;
+        const isHeaderStatic =
+          ((previousHeaderShown === false || previousHeaderMode === 'screen') &&
+            // We still need to animate when coming back from next scene
+            // A hacky way to check this is if the next scene exists
+            !nextDescriptor) ||
+          nextHeaderlessScene;
 
-          if (previousScene) {
-            const { options, route } = previousScene.descriptor;
+        const props: StackHeaderProps = {
+          back: headerBack,
+          progress: scene.progress,
+          options: scene.descriptor.options,
+          route: scene.descriptor.route,
+          navigation: scene.descriptor
+            .navigation as StackNavigationProp<ParamListBase>,
+          styleInterpolator:
+            mode === 'float'
+              ? isHeaderStatic
+                ? nextHeaderlessGestureDirection === 'vertical' ||
+                  nextHeaderlessGestureDirection === 'vertical-inverted'
+                  ? forSlideUp
+                  : nextHeaderlessGestureDirection === 'horizontal-inverted'
+                    ? forSlideRight
+                    : forSlideLeft
+                : headerStyleInterpolator
+              : forNoAnimation,
+        };
 
-            headerBack = previousScene
-              ? {
-                  title: getHeaderTitle(options, route.name),
-                  href: buildHref(route.name, route.params),
-                }
-              : parentHeaderBack;
-          }
+        return (
+          <NavigationProvider
+            key={scene.descriptor.route.key}
+            navigation={scene.descriptor.navigation}
+            route={scene.descriptor.route}
+          >
+            <View
+              onLayout={
+                onContentHeightChange
+                  ? (e) => {
+                      const { height } = e.nativeEvent.layout;
 
-          // If the screen is next to a headerless screen, we need to make the header appear static
-          // This makes the header look like it's moving with the screen
-          const previousDescriptor = self[i - 1]?.descriptor;
-          const nextDescriptor = self[i + 1]?.descriptor;
-
-          const {
-            headerShown: previousHeaderShown = true,
-            headerMode: previousHeaderMode,
-          } = previousDescriptor?.options || {};
-
-          // If any of the next screens don't have a header or header is part of the screen
-          // Then we need to move this header offscreen so that it doesn't cover it
-          const nextHeaderlessScene = self.slice(i + 1).find((scene) => {
-            const {
-              headerShown: currentHeaderShown = true,
-              headerMode: currentHeaderMode,
-            } = scene?.descriptor.options || {};
-
-            return (
-              currentHeaderShown === false || currentHeaderMode === 'screen'
-            );
-          });
-
-          const { gestureDirection: nextHeaderlessGestureDirection } =
-            nextHeaderlessScene?.descriptor.options || {};
-
-          const isHeaderStatic =
-            ((previousHeaderShown === false ||
-              previousHeaderMode === 'screen') &&
-              // We still need to animate when coming back from next scene
-              // A hacky way to check this is if the next scene exists
-              !nextDescriptor) ||
-            nextHeaderlessScene;
-
-          const props: StackHeaderProps = {
-            back: headerBack,
-            progress: scene.progress,
-            options: scene.descriptor.options,
-            route: scene.descriptor.route,
-            navigation: scene.descriptor
-              .navigation as StackNavigationProp<ParamListBase>,
-            styleInterpolator:
-              mode === 'float'
-                ? isHeaderStatic
-                  ? nextHeaderlessGestureDirection === 'vertical' ||
-                    nextHeaderlessGestureDirection === 'vertical-inverted'
-                    ? forSlideUp
-                    : nextHeaderlessGestureDirection === 'horizontal-inverted'
-                      ? forSlideRight
-                      : forSlideLeft
-                  : headerStyleInterpolator
-                : forNoAnimation,
-          };
-
-          return (
-            <NavigationProvider
-              key={scene.descriptor.route.key}
-              navigation={scene.descriptor.navigation}
-              route={scene.descriptor.route}
+                      onContentHeightChange({
+                        route: scene.descriptor.route,
+                        height,
+                      });
+                    }
+                  : undefined
+              }
+              aria-hidden={!isFocused}
+              style={[
+                // Avoid positioning the focused header absolutely
+                // Otherwise accessibility tools don't seem to be able to find it
+                (mode === 'float' && !isFocused) || headerTransparent
+                  ? styles.header
+                  : null,
+                {
+                  pointerEvents: isFocused ? 'box-none' : 'none',
+                },
+              ]}
             >
-              <View
-                onLayout={
-                  onContentHeightChange
-                    ? (e) => {
-                        const { height } = e.nativeEvent.layout;
-
-                        onContentHeightChange({
-                          route: scene.descriptor.route,
-                          height,
-                        });
-                      }
-                    : undefined
-                }
-                aria-hidden={!isFocused}
-                style={[
-                  // Avoid positioning the focused header absolutely
-                  // Otherwise accessibility tools don't seem to be able to find it
-                  (mode === 'float' && !isFocused) || headerTransparent
-                    ? styles.header
-                    : null,
-                  {
-                    pointerEvents: isFocused ? 'box-none' : 'none',
-                  },
-                ]}
-              >
-                {header !== undefined ? header(props) : <Header {...props} />}
-              </View>
-            </NavigationProvider>
-          );
-        })}
-      </Animated.View>
-    </Container>
+              {header !== undefined ? header(props) : <Header {...props} />}
+            </View>
+          </NavigationProvider>
+        );
+      })}
+    </Animated.View>
   );
 }
 
