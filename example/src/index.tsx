8cf1036cdfa961c98478f48b834d1f158b7dfaef
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-11-20 15:36:12 +0100

    chore: filter out internal dev client deep links

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -356,6 +356,9 @@ export function App() {
               // Android: npx uri-scheme@latest open "rne://simple-stack" --android
               // iOS: npx uri-scheme@latest open "rne://simple-stack" --ios
               prefixes: [createURL('/')],
+              // When using Dev Client, launching app from the server has an internal URL
+              // This is not a URL we can handle, so we filter it out
+              filter: (url) => !url.includes('/expo-development-client/'),
               enabled: 'auto',
             }}
             fallback={<Text>Loading…</Text>}
