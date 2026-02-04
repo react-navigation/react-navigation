3bf1a8fe21ab9e22c52cd411ae7b195ab664ecbb
Satyajit Sahoo <satyajit.happy@gmail.com>
2026-02-04 18:45:27 +0100

    refactor: don't use IIFE for theme colors

--- example/src/theme.native.tsx
+++ example/src/theme.native.tsx
@@ -3,16 +3,16 @@ import { Platform, PlatformColor } from 'react-native';
 
 export const PlatformTheme: Theme = {
   ...DefaultTheme,
-  colors: Platform.select<() => Theme['colors']>({
-    ios: () => ({
+  colors: Platform.select<Theme['colors']>({
+    ios: {
       primary: PlatformColor('systemRed'),
       background: PlatformColor('systemGroupedBackground'),
       card: PlatformColor('tertiarySystemBackground'),
       text: PlatformColor('label'),
       border: PlatformColor('separator'),
       notification: PlatformColor('systemRed'),
-    }),
-    android: () => ({
+    },
+    android: {
       primary: PlatformColor('@android:color/system_primary_light'),
       background: PlatformColor(
         '@android:color/system_surface_container_light'
@@ -21,7 +21,7 @@ export const PlatformTheme: Theme = {
       text: PlatformColor('@android:color/system_on_surface_light'),
       border: PlatformColor('@android:color/system_outline_variant_light'),
       notification: PlatformColor('@android:color/system_error_light'),
-    }),
-    default: () => DefaultTheme.colors,
-  })(),
+    },
+    default: DefaultTheme.colors,
+  }),
 };
