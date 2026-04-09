9cef84ad46647279aec044766d3c1c3c8c99ad33
Satyajit Sahoo <satyajit.happy@gmail.com>
2026-04-09 17:24:29 +0200

    chore: update bob and vite config

--- example/vite.config.mjs
+++ example/vite.config.mjs
@@ -1,58 +1,30 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig, transformWithEsbuild } from 'vite';
-import commonjs from 'vite-plugin-commonjs';
+import fs from 'node:fs';
+import path from 'node:path';
 
-const extensions = [
-  '.web.tsx',
-  '.tsx',
-  '.web.ts',
-  '.ts',
-  '.web.jsx',
-  '.jsx',
-  '.web.js',
-  '.js',
-  '.web.mjs',
-  '.mjs',
-  '.json',
-];
+import config from 'react-native-builder-bob/vite-config';
+import { defineConfig, mergeConfig } from 'vite';
 
-const jsx = (regex) => ({
-  name: 'js-as-jsx',
-  enforce: 'pre',
-  async transform(code, id) {
-    if (id.endsWith('.js') && regex.test(id)) {
-      return transformWithEsbuild(code, id, {
-        loader: 'jsx',
-        jsx: 'automatic',
-      });
-    }
+import pack from '../package.json' with { type: 'json' };
 
-    return null;
-  },
-});
+const root = path.join(import.meta.dirname, '..');
+const packages = pack.workspaces.flatMap((workspace) =>
+  fs
+    .globSync(path.posix.join(workspace, 'package.json'), {
+      cwd: root,
+    })
+    .map((packageJsonPath) =>
+      JSON.parse(fs.readFileSync(path.join(root, packageJsonPath), 'utf8'))
+    )
+);
 
-/** @type {import('vite').UserConfig} */
-export default defineConfig(({ mode }) => ({
-  plugins: [jsx(/\/(@expo|expo-.+)\//), commonjs(), react()],
-  define: {
-    __DEV__: JSON.stringify(mode !== 'production'),
-    'process.env.EXPO_OS': JSON.stringify('web'),
-    global: 'globalThis',
-  },
-  resolve: {
-    extensions: extensions,
-    conditions: ['source', 'module', 'browser', mode],
-    alias: {
-      'react-native': 'react-native-web',
+export default defineConfig((env) =>
+  mergeConfig(config(env), {
+    resolve: {
+      dedupe: [
+        ...new Set(
+          packages.flatMap((pack) => Object.keys(pack.peerDependencies ?? {}))
+        ),
+      ],
     },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      resolveExtensions: extensions,
-      jsx: 'automatic',
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
-}));
+  })
+);
