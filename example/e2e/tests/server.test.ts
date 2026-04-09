d9ff7158d99ed2825f5e77cdacb12c8f7cc119cb
Satyajit Sahoo <satyajit.happy@gmail.com>
2026-04-09 17:53:59 +0200

    chore: upgrade dependencies

--- example/e2e/tests/server.test.ts
+++ example/e2e/tests/server.test.ts
@@ -1,5 +1,4 @@
 import { expect, test as it } from '@playwright/test';
-import { load } from 'cheerio';
 
 const server = 'http://localhost:3275';
 
@@ -7,7 +6,5 @@ it('renders the home page', async () => {
   const res = await fetch(server);
   const html = await res.text();
 
-  const $ = load(html);
-
-  expect($('title').text()).toBe('Examples');
+  expect(html).toMatch(/<title>\s*Examples\s*<\/title>/i);
 });
