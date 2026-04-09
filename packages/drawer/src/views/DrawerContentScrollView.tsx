5446185afd32ef696e960808e7c6ab944661b863
Satyajit Sahoo <satyajit.happy@gmail.com>
2026-04-09 14:28:01 +0200

    feat: rework safe area handling in drawer content scroll view
    - Use `contentInsetAdjustmentBehavior` instead of manual insets on iOS
    - Remove corner insets if `contentInsetAdjustmentBehavior` is set to
      `never`

--- packages/drawer/src/views/DrawerContentScrollView.tsx
+++ packages/drawer/src/views/DrawerContentScrollView.tsx
@@ -1,6 +1,11 @@
 import { UNSTABLE_CornerInset, useLocale } from '@react-navigation/native';
 import * as React from 'react';
-import { ScrollView, type ScrollViewProps, StyleSheet } from 'react-native';
+import {
+  Platform,
+  ScrollView,
+  type ScrollViewProps,
+  StyleSheet,
+} from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { DrawerPositionContext } from '../utils/DrawerPositionContext';
@@ -12,7 +17,13 @@ type Props = ScrollViewProps & {
 const SPACING = 12;
 
 function DrawerContentScrollViewInner(
-  { contentContainerStyle, style, children, ...rest }: Props,
+  {
+    contentInsetAdjustmentBehavior = 'automatic',
+    contentContainerStyle,
+    style,
+    children,
+    ...rest
+  }: Props,
   ref?: React.Ref<ScrollView>
 ) {
   const drawerPosition = React.use(DrawerPositionContext);
@@ -25,32 +36,32 @@ function DrawerContentScrollViewInner(
       : drawerPosition === 'right';
 
   return (
-    <>
-      <ScrollView
-        {...rest}
-        ref={ref}
-        contentContainerStyle={[
-          {
-            paddingBottom: SPACING + insets.bottom,
-            paddingStart: SPACING + (!isRight ? insets.left : 0),
-            paddingEnd: SPACING + (isRight ? insets.right : 0),
-          },
-          contentContainerStyle,
-        ]}
-        style={[styles.container, style]}
-      >
-        <UNSTABLE_CornerInset
-          direction="vertical"
-          edge="top"
-          style={{
-            // We don't want to apply both safe area and corner insets
-            minHeight: insets.top,
-            marginBottom: SPACING,
-          }}
-        />
-        {children}
-      </ScrollView>
-    </>
+    <ScrollView
+      {...rest}
+      ref={ref}
+      contentInsetAdjustmentBehavior={contentInsetAdjustmentBehavior}
+      contentContainerStyle={[
+        {
+          paddingTop:
+            SPACING + Platform.select({ ios: 0, default: insets.top }),
+          paddingBottom:
+            SPACING + Platform.select({ ios: 0, default: insets.bottom }),
+          paddingStart:
+            SPACING +
+            (!isRight ? Platform.select({ ios: 0, default: insets.left }) : 0),
+          paddingEnd:
+            SPACING +
+            (isRight ? Platform.select({ ios: 0, default: insets.right }) : 0),
+        },
+        contentContainerStyle,
+      ]}
+      style={[styles.container, style]}
+    >
+      {Platform.OS === 'ios' && contentInsetAdjustmentBehavior !== 'never' && (
+        <UNSTABLE_CornerInset direction="vertical" edge="top" />
+      )}
+      {children}
+    </ScrollView>
   );
 }
 
