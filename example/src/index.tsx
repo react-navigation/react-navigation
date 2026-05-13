462b5da33fe3e02c7afba30b5d2dde8985f64517
Satyajit Sahoo <satyajit.happy@gmail.com>
2026-05-13 12:16:17 +0200

    chore: add version suffix to url scheme in example app
    this avoids launching example app for the wrong version

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -66,10 +66,8 @@ const SCREEN_NAMES = Object.keys(SCREENS) as (keyof typeof SCREENS)[];
 
 const linking: LinkingOptions<RootStackParamList> = {
   // To test deep linking on, run the following in the Terminal:
-  // Android: adb shell am start -a android.intent.action.VIEW -d "exp://127.0.0.1:19000/--/simple-stack"
-  // iOS: xcrun simctl openurl booted exp://127.0.0.1:19000/--/simple-stack
-  // Android (bare): adb shell am start -a android.intent.action.VIEW -d "rne://127.0.0.1:19000/--/simple-stack"
-  // iOS (bare): xcrun simctl openurl booted rne://127.0.0.1:19000/--/simple-stack
+  // Android (bare): adb shell am start -a android.intent.action.VIEW -d "rne+7://127.0.0.1:19000/--/simple-stack"
+  // iOS (bare): xcrun simctl openurl booted rne+7://127.0.0.1:19000/--/simple-stack
   // The first segment of the link is the the scheme + host (returned by `Linking.makeUrl`)
   prefixes: [createURL('/')],
   // When not using Dev Client, launching app from the server has an internal URL
