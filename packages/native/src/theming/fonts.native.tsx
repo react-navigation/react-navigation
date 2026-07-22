308cf985584a1363422f9e1a759f44cb9299e4c7
Satyajit Sahoo <satyajit.happy@gmail.com>
2026-07-22 22:41:23 +0200

    feat: make native package work without react-native-web

---
+++ packages/native/src/theming/fonts.native.tsx
@@ -0,0 +1,41 @@
+import type { Theme } from '@react-navigation/core';
+import { Platform } from 'react-native';
+
+export const fonts = Platform.select({
+  ios: {
+    regular: {
+      fontFamily: 'System',
+      fontWeight: '400',
+    },
+    medium: {
+      fontFamily: 'System',
+      fontWeight: '500',
+    },
+    bold: {
+      fontFamily: 'System',
+      fontWeight: '600',
+    },
+    heavy: {
+      fontFamily: 'System',
+      fontWeight: '700',
+    },
+  },
+  default: {
+    regular: {
+      fontFamily: 'sans-serif',
+      fontWeight: 'normal',
+    },
+    medium: {
+      fontFamily: 'sans-serif-medium',
+      fontWeight: 'normal',
+    },
+    bold: {
+      fontFamily: 'sans-serif',
+      fontWeight: '600',
+    },
+    heavy: {
+      fontFamily: 'sans-serif',
+      fontWeight: '700',
+    },
+  },
+} as const satisfies Record<string, Theme['fonts']>);
