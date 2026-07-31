ad0ebae96f6972f23925f6b0cb205fa1d4e94eef
delekta <kamil.delekta@discordapp.com>
2026-07-31 14:02:38 +0200

    Restoring .tsx files, intorducing .native.tsx

--- packages/stack/src/views/InteractionManager.tsx
+++ packages/stack/src/views/InteractionManager.tsx
@@ -1,4 +1,5 @@
-import { Platform } from 'react-native';
+/* eslint-disable no-restricted-imports */
+import * as ReactNative from 'react-native';
 
 type InteractionManagerType = {
   createInteractionHandle(): number;
@@ -7,13 +8,13 @@ type InteractionManagerType = {
 
 let InteractionManager: InteractionManagerType | undefined;
 
-const version = Platform.constants?.reactNativeVersion;
+const version = ReactNative.Platform.constants?.reactNativeVersion;
 
 try {
   InteractionManager =
     version?.major === 0 && version.minor >= 82
       ? undefined
-      : require('react-native').InteractionManager;
+      : ReactNative.InteractionManager;
 } catch (e) {
   // On newer React Native versions, accessing InteractionManager throws an error
   // https://github.com/react/react-native/pull/57026
